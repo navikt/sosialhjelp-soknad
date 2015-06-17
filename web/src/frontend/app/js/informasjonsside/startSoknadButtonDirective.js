542a52f6e1715be70205f535ac6a13d838b996ad
Steffen Hageland <steffen.hageland@nav.no>
2015-06-17 15:04:27 +0200

    dro inn modal less for å få styling på popups ved feilmeldinger

--- web/src/frontend/app/js/informasjonsside/startSoknadButtonDirective.js
+++ web/src/frontend/app/js/informasjonsside/startSoknadButtonDirective.js
@@ -5,20 +5,18 @@ angular.module('nav.kravdialogbp.informasjonsside')
             templateUrl: 'js/informasjonsside/templates/startSoknadButtonTemplate.html',
             link: function (scope) {
                 scope.startSoknad = function () {
-                    if (feilliste.length === 0) {
-                        scope.fremdriftsindikator = {
-                            laster: true
-                        };
+                    scope.fremdriftsindikator = {
+                        laster: true
+                    };
 
-                        var skjemanummer = "NAV 04-16.01";
+                    var skjemanummer = "NAV 04-16.01";
 
-                        soknadService.create({soknadType: skjemanummer}).$promise.then(function (soknad) {
-                                console.log("STARTET SØKNAD!", soknad);
-                            })
-                            .catch(function () {
-                                scope.fremdriftsindikator.laster = false;
-                            });
-                    }
+                    soknadService.create({soknadType: skjemanummer}).$promise.then(function (soknad) {
+                            console.log("STARTET SØKNAD!", soknad);
+                        })
+                        .catch(function () {
+                            scope.fremdriftsindikator.laster = false;
+                        });
                 };
             }
         };
