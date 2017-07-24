4f2397bfedf2ca8271dc34026997431cd69bdd3f
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-07-24 15:11:10 +0200

    bruk skjemanummer til tiltakspenger
    rename fra kravdialog til soknadsosialhjelp

--- web/src/frontend/app/js/felles/constants/constantsModule.js
+++ web/src/frontend/app/js/felles/constants/constantsModule.js
@@ -1,4 +1,4 @@
 angular.module('nav.constant', [])
-    .constant('cmsprefix', 'foreldresoknad.')
-    .constant('CmsBundleName', 'foreldrepenger')
+    .constant('cmsprefix', 'soknadsosialhjelp.')
+    .constant('CmsBundleName', 'soknadsosialhjelp')
     .constant("ApiBaseUrl", window.Globals.apiUrl);
--- web/src/frontend/app/js/opprett/startSoknadButton/startSoknadButtonDirective.js
+++ web/src/frontend/app/js/opprett/startSoknadButton/startSoknadButtonDirective.js
@@ -11,7 +11,7 @@ angular.module('nav.soknadsosialhjelp.opprett')
                         laster: true
                     };
 
-                    var skjemanummer = "NAV 04-01.03";
+                    var skjemanummer = "NAV 76-13.45";
 
                     soknadService.create({soknadType: skjemanummer}).then(function (soknad) {
                         UtilService.redirectTilUnderside("/app/" + soknad.brukerBehandlingId + '#/informasjonsside');
