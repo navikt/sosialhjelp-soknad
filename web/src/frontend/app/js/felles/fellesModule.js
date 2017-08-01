83ded285c60477d24c78bb4658283405f217ce03
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-08-01 15:34:30 +0200

    opprader innholdshenter til v5 for å se om det fisker problemet med commonslogging

--- web/src/frontend/app/js/felles/fellesModule.js
+++ web/src/frontend/app/js/felles/fellesModule.js
@@ -1,7 +1,7 @@
 angular.module('nav.soknadsosialhjelp.felles', [
     'nav.constant',
     'lodash',
-    'nav.soknadsosialhjelp.googleanalytics',
+    // 'nav.soknadsosialhjelp.googleanalytics',
     'nav.soknadsosialhjelp.httpProvider',
     'nav.soknadsosialhjelp.loader',
     'nav.soknadsosialhjelp.routes'
@@ -9,7 +9,7 @@ angular.module('nav.soknadsosialhjelp.felles', [
 
 require('./constants/constantsModule');
 require('./lodash/lodash-module');
-require('./googleanalytics/googleAnalyticsModule');
+// require('./googleanalytics/googleAnalyticsModule');
 require('./httpprovider/httpProviderModule');
 require('./loader/loaderModule');
 require('./routes/routesModule');
