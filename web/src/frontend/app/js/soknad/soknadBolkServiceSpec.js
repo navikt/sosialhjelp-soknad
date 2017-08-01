d60c06721631a433f845017fd4687feb21144208
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-08-01 10:39:51 +0200

    Fiks testene

--- web/src/frontend/app/js/soknad/soknadBolkServiceSpec.js
+++ web/src/frontend/app/js/soknad/soknadBolkServiceSpec.js
@@ -6,40 +6,40 @@ describe('soknadBolkService', function () {
     }));
 
     it('getBolkMedNavn() skal returere bolken med ID som gitt navn', function () {
-        expect(this.soknadBolkService.getBolkMedNavn('dummybolk1').id).toBe('dummybolk1');
+        expect(this.soknadBolkService.getBolkMedNavn('arbeidbolk').id).toBe('arbeidbolk');
     });
 
     it('apneTab() skal sette gitt bolk til åpen', function () {
-        this.soknadBolkService.getBolkMedNavn('dummybolk1').apen = false;
-        this.soknadBolkService.apneTab('dummybolk1');
-        expect(this.soknadBolkService.getBolkMedNavn('dummybolk1').apen).toBeTruthy();
+        this.soknadBolkService.getBolkMedNavn('arbeidbolk').apen = false;
+        this.soknadBolkService.apneTab('arbeidbolk');
+        expect(this.soknadBolkService.getBolkMedNavn('arbeidbolk').apen).toBeTruthy();
     });
 
     it('apneTab() skal sette flere bolker til åpen', function () {
-        this.soknadBolkService.getBolkMedNavn('dummybolk1').apen = false;
-        this.soknadBolkService.getBolkMedNavn('dummybolk2').apen = false;
-        this.soknadBolkService.apneTab(['dummybolk1', 'dummybolk2']);
-        expect(this.soknadBolkService.getBolkMedNavn('dummybolk1').apen).toBeTruthy();
-        expect(this.soknadBolkService.getBolkMedNavn('dummybolk2').apen).toBeTruthy();
+        this.soknadBolkService.getBolkMedNavn('arbeidbolk').apen = false;
+        this.soknadBolkService.getBolkMedNavn('familiebolk').apen = false;
+        this.soknadBolkService.apneTab(['arbeidbolk', 'familiebolk']);
+        expect(this.soknadBolkService.getBolkMedNavn('arbeidbolk').apen).toBeTruthy();
+        expect(this.soknadBolkService.getBolkMedNavn('familiebolk').apen).toBeTruthy();
     });
 
     it('lukkTab() skal sette gitt bolk til ikke åpen', function () {
-        this.soknadBolkService.getBolkMedNavn('dummybolk1').apen = true;
-        this.soknadBolkService.lukkTab('dummybolk1');
-        expect(this.soknadBolkService.getBolkMedNavn('dummybolk1').apen).toBeFalsy();
+        this.soknadBolkService.getBolkMedNavn('arbeidbolk').apen = true;
+        this.soknadBolkService.lukkTab('arbeidbolk');
+        expect(this.soknadBolkService.getBolkMedNavn('arbeidbolk').apen).toBeFalsy();
     });
 
     it('lukkTab() skal sette flere bolker til ikke åpen', function () {
-        this.soknadBolkService.getBolkMedNavn('dummybolk1').apen = true;
-        this.soknadBolkService.getBolkMedNavn('dummybolk2').apen = true;
-        this.soknadBolkService.lukkTab(['dummybolk1', 'dummybolk2']);
-        expect(this.soknadBolkService.getBolkMedNavn('dummybolk1').apen).toBeFalsy();
-        expect(this.soknadBolkService.getBolkMedNavn('dummybolk2').apen).toBeFalsy();
+        this.soknadBolkService.getBolkMedNavn('arbeidbolk').apen = true;
+        this.soknadBolkService.getBolkMedNavn('familiebolk').apen = true;
+        this.soknadBolkService.lukkTab(['arbeidbolk', 'familiebolk']);
+        expect(this.soknadBolkService.getBolkMedNavn('arbeidbolk').apen).toBeFalsy();
+        expect(this.soknadBolkService.getBolkMedNavn('familiebolk').apen).toBeFalsy();
     });
 
     it('settValidert() skal sette bolken til validert', function () {
-        this.soknadBolkService.getBolkMedNavn('dummybolk1').validering = true;
-        this.soknadBolkService.settValidert('dummybolk1');
-        expect(this.soknadBolkService.getBolkMedNavn('dummybolk1').validering).toBeFalsy();
+        this.soknadBolkService.getBolkMedNavn('arbeidbolk').validering = true;
+        this.soknadBolkService.settValidert('arbeidbolk');
+        expect(this.soknadBolkService.getBolkMedNavn('arbeidbolk').validering).toBeFalsy();
     });
 });
