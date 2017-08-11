fd8d563b9899d5405ce77203ac77f1b055c0d40b
Hansen <H151343@ADEO.NO>
2017-08-11 07:43:49 +0200

    Rydde i folderstruktur

---
+++ web/src/frontend/src/app/containers/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "../styles/app.css";
+
+class App extends React.Component<{}, {}> {
+	render() {
+		return (
+			<div>
+				ReactSkall
+			</div>
+		);
+	}
+}
+
+export default App;
