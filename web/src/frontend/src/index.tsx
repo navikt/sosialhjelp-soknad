a1ed8efdf78ca84ea448f0d68e99df835d92f127
Hansen <H151343@ADEO.NO>
2017-08-11 07:47:22 +0200

    Korrigere path til index.css

--- web/src/frontend/src/index.tsx
+++ web/src/frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
-import "./styles/index.css";
+import "./index.css";
 
 import App from "./app/containers/App";
 
