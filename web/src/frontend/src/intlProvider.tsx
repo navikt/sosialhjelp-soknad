cca9ca5da94fb32103763d9a7754a2410594d290
Flemming, Thomas <Thomas.Flemming@nav.no>
2017-08-11 15:23:01 +0200

    work in progress

--- web/src/frontend/src/intlProvider.tsx
+++ web/src/frontend/src/intlProvider.tsx
@@ -4,14 +4,16 @@ import * as nb from "react-intl/locale-data/nb";
 
 addLocaleData(nb);
 
-function IntlProvider({children}: { children: React.ReactNode }) {
-	const ledetekster = {data: {nb: {"skjema.tittel": "Søknad for økonomisk sosialhjelp"}}};
-	const locale = "nb";
-	return (
-		<Provider messages={ ledetekster.data[locale] } defaultLocale="nb" locale={ locale }>
-			{ children }
-		</Provider>
-	);
+class IntlProvider extends React.Component<{ children: React.ReactNode }> {
+	render() {
+		const ledetekster = {data: {nb: {"skjema.tittel": "Søknad for økonomisk sosialhjelp"}}};
+		const locale = "nb";
+		return (
+			<Provider messages={ ledetekster.data[locale] } defaultLocale="nb" locale={ locale }>
+				{ this.props.children }
+			</Provider>
+		);
+	}
 }
 
 export default IntlProvider;
