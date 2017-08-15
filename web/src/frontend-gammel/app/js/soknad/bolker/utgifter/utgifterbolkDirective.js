da48a8e2ee9c1bb54a33832985b40bfd9059b3ef
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-08-15 08:12:29 +0200

    legg til ny bolk for utgifter

---
+++ web/src/frontend-gammel/app/js/soknad/bolker/utgifter/utgifterbolkDirective.js
@@ -0,0 +1,6 @@
+angular.module('nav.soknadsosialhjelp.soknad.utgifterbolk')
+    .directive('utgifterbolk', function () {
+        return {
+            templateUrl: 'js/soknad/bolker/utgifter/utgifterbolk.html'
+        };
+    });
