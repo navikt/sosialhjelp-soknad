b922a9668faf4f75beea624bf43a9cf38c10fa1c
Flemming, Thomas <thomas.flemming@gmail.com>
2017-08-17 16:22:07 +0200

    Underskjema

--- web/src/frontend/src/app/soknadsskjema/Steg1.tsx
+++ web/src/frontend/src/app/soknadsskjema/Steg1.tsx
@@ -11,6 +11,7 @@ import { injectIntl, InjectedIntlProps } from "react-intl";
 import FaktumCheckbox from "../faktum/components/FaktumCheckbox";
 import FaktumRadio from "../faktum/components/FaktumRadio";
 import FaktumSkjemagruppe from "../faktum/components/FaktumSkjemagruppe";
+import Underskjema from "../components/underskjema";
 
 interface StateProps {
 	faktum: FaktumMap;
@@ -33,21 +34,23 @@ class Steg1 extends React.Component<
 						<FaktumCheckbox faktumKey="arbeid.dinsituasjon.arbeidsledig" />
 						<FaktumCheckbox faktumKey="arbeid.dinsituasjon.jobb" />
 						<FaktumCheckbox faktumKey="arbeid.dinsituasjon.student" />
-						<FaktumSkjemagruppe
-							visible={faktum.get("arbeid.dinsituasjon.student") === "true"}
-							title={intl.formatMessage({
-								id: "arbeid.dinsituasjon.student.true.heltid.sporsmal"
-							})}
-						>
-							<FaktumRadio
-								faktumKey="arbeid.dinsituasjon.student.true.heltid"
-								value="true"
-							/>
-							<FaktumRadio
-								faktumKey="arbeid.dinsituasjon.student.true.heltid"
-								value="false"
-							/>
-						</FaktumSkjemagruppe>
+						<Underskjema visible={faktum.get("arbeid.dinsituasjon.student")}>
+							<FaktumSkjemagruppe
+								visible={faktum.get("arbeid.dinsituasjon.student") === "true"}
+								title={intl.formatMessage({
+									id: "arbeid.dinsituasjon.student.true.heltid.sporsmal"
+								})}
+							>
+								<FaktumRadio
+									faktumKey="arbeid.dinsituasjon.student.true.heltid"
+									value="true"
+								/>
+								<FaktumRadio
+									faktumKey="arbeid.dinsituasjon.student.true.heltid"
+									value="false"
+								/>
+							</FaktumSkjemagruppe>
+						</Underskjema>
 						<FaktumCheckbox faktumKey="arbeid.dinsituasjon.annensituasjon" />
 						<FaktumSkjemagruppe
 							visible={
