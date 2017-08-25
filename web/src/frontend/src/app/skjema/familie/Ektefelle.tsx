aba12ef42be6a384ea317872cffdc1d5d77f5065
frode hansen <frode.hansen@bekk.no>
2017-08-25 13:09:56 +0200

    Justeringer nivaTreSkjema - brukt i ektefelle

--- web/src/frontend/src/app/skjema/familie/Ektefelle.tsx
+++ web/src/frontend/src/app/skjema/familie/Ektefelle.tsx
@@ -6,6 +6,7 @@ import { radioCheckKeys } from "../../../skjema/utils";
 import FaktumRadio from "../../../skjema/faktum/FaktumRadio";
 import FaktumSkjemagruppe from "../../../skjema/faktum/FaktumSkjemagruppe";
 import FaktumTextarea from "../../../skjema/faktum/FaktumTextarea";
+import NivaTreSkjema from "../../../skjema/components/nivaTreSkjema";
 
 class Ektefelle extends React.Component<FaktumComponentProps, {}> {
 	render() {
@@ -17,9 +18,9 @@ class Ektefelle extends React.Component<FaktumComponentProps, {}> {
 				<FaktumSkjemagruppe tittelId={borsammen.sporsmal}>
 					<FaktumRadio faktumKey={borsammen.faktum} option="true" />
 					<FaktumRadio faktumKey={borsammen.faktum} option="false" />
-					{faktum.get(borsammen.faktum) === "false"
-						? <FaktumTextarea faktumKey={`${borsammen.faktum}.beskrivelse`} />
-						: null}
+					<NivaTreSkjema visible={faktum.get(borsammen.faktum) === "false"}>
+						<FaktumTextarea faktumKey={`${borsammen.faktum}.beskrivelse`} />
+					</NivaTreSkjema>
 				</FaktumSkjemagruppe>
 			</div>
 		);
