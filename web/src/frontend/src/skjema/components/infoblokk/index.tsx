bef0d3fa17b7973a8d04eeb18dd60b06315bb04c
frode hansen <frode.hansen@bekk.no>
2017-08-28 13:09:45 +0200

    Info ikon

--- web/src/frontend/src/skjema/components/infoblokk/index.tsx
+++ web/src/frontend/src/skjema/components/infoblokk/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import "./infoblokk.css";
-
+import Info from "../svg/Info";
 interface Props {
 	tittel?: string;
 }
@@ -8,6 +8,7 @@ interface Props {
 const Infoblokk: React.StatelessComponent<Props> = ({ children, tittel }) => {
 	return (
 		<div className="skjema-infoblokk">
+			<Info />
 			<h1>Tittel</h1>
 			{children}
 		</div>
