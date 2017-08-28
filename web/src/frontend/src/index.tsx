7d52db41b5ffe0913aa0848bbf77ffd8baf1de88
Flemming, Thomas <thomas.flemming@gmail.com>
2017-08-28 18:38:45 +0200

    Midlertidig fjerne saga

--- web/src/frontend/src/index.tsx
+++ web/src/frontend/src/index.tsx
@@ -5,13 +5,8 @@ import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
-
-// import { composeWithDevTools } from "redux-devtools-extension";
 import { createStore, combineReducers, applyMiddleware } from "redux";
-// import createSagaMiddleware from "redux-saga";
-
 import App from "./app";
-// import rootSaga from "./sagas";
 import thunk from "redux-thunk";
 import SoknadReducer from "./redux/soknad/reducer";
 import FaktumReducer from "./skjema/reducer";
@@ -24,8 +19,6 @@ const rootReducer = combineReducers({
 	faktum: FaktumReducer
 });
 
-// const sagaMiddleware = createSagaMiddleware();
-
 function configureStore() {
 	/* tslint:disable-next-line */
 	const devtools: any = (window['devToolsExtension'] && erDev()) ? window['devToolsExtension']() : (f:any)=>f;
@@ -34,14 +27,6 @@ function configureStore() {
 }
 
 const store = configureStore();
-
-// const store = createStore(
-// 	rootReducer,
-// 	composeWithDevTools(applyMiddleware(sagaMiddleware))
-// );
-//
-// sagaMiddleware.run(rootSaga);
-
 ReactDOM.render(
 	<IntlProvider>
 		<Provider store={store}>
