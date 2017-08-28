d4e3382cc63f90b18728a56fd5d3070a0dc42ef0
Hansen <H151343@ADEO.NO>
2017-08-28 09:41:09 +0200

    Arrow svg komponent

---
+++ web/src/frontend/src/app/components/svg/Arrow.tsx
@@ -0,0 +1,16 @@
+/* tslint:disable*/
+import * as React from "react";
+
+interface Props {
+	prop?: any;
+}
+
+const Arrow: React.StatelessComponent<Props> = (props: Props) =>
+	<svg
+		className="bostedBydelArrow"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 100 125"
+	>
+		<path d="M67.4 57.3L53.5 71.3V19.7c0-2-1.6-3.6-3.6-3.6s-3.6 1.6-3.6 3.6v51.6L32.3 57.3c-1.4-1.4-3.7-1.4-5.1 0 -1.4 1.4-1.4 3.7 0 5.1l20.1 20.1c0.2 0.2 0.4 0.3 0.5 0.4 0 0 0.1 0.1 0.2 0.1 0.2 0.1 0.3 0.2 0.5 0.2 0.1 0 0.1 0 0.2 0.1 0.2 0.1 0.3 0.1 0.5 0.1 0.2 0 0.5 0.1 0.7 0.1h0c0.2 0 0.5 0 0.7-0.1 0.2 0 0.3-0.1 0.4-0.1 0.1 0 0.2 0 0.2-0.1 0.2-0.1 0.3-0.2 0.5-0.3 0 0 0.1 0 0.1-0.1 0.2-0.1 0.4-0.3 0.5-0.4l20.1-20.1c1.4-1.4 1.4-3.7 0-5.1C71.1 55.9 68.8 55.9 67.4 57.3z" />
+	</svg>;
+export default Arrow;
--- web/src/frontend/src/app/start/Bosted.tsx
+++ web/src/frontend/src/app/start/Bosted.tsx
@@ -6,6 +6,7 @@ import { FaktumState, FaktumMap } from "../../skjema/reducer";
 import { Kommuner } from "./kommuner";
 import { Kommune, Bydel } from "./types";
 import { Collapse } from "react-collapse";
+import Arrow from "../components/svg/Arrow";
 
 interface StateProps {
 	faktum: FaktumMap;
@@ -51,14 +52,7 @@ class Bosted extends React.Component<StateProps> {
 
 					{valgtKommune && valgtKommune.bydeler
 						? <div className="blokk-l">
-								<svg
-									className="bostedBydelArrow"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 100 125"
-								>
-									<path d="M67.4 57.3L53.5 71.3V19.7c0-2-1.6-3.6-3.6-3.6s-3.6 1.6-3.6 3.6v51.6L32.3 57.3c-1.4-1.4-3.7-1.4-5.1 0 -1.4 1.4-1.4 3.7 0 5.1l20.1 20.1c0.2 0.2 0.4 0.3 0.5 0.4 0 0 0.1 0.1 0.2 0.1 0.2 0.1 0.3 0.2 0.5 0.2 0.1 0 0.1 0 0.2 0.1 0.2 0.1 0.3 0.1 0.5 0.1 0.2 0 0.5 0.1 0.7 0.1h0c0.2 0 0.5 0 0.7-0.1 0.2 0 0.3-0.1 0.4-0.1 0.1 0 0.2 0 0.2-0.1 0.2-0.1 0.3-0.2 0.5-0.3 0 0 0.1 0 0.1-0.1 0.2-0.1 0.4-0.3 0.5-0.4l20.1-20.1c1.4-1.4 1.4-3.7 0-5.1C71.1 55.9 68.8 55.9 67.4 57.3z" />
-								</svg>
-
+								<Arrow />
 								<FaktumSelect
 									faktumKey="personalia.bydel"
 									bredde="m"
