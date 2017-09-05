c0a6a96d40d18f3ba2b503c3d3bc5978af5da2a3
h151343 <frode.hansen@nav.no>
2017-09-05 10:52:52 +0200

    Justere bredde og tekst for fødselsnummer

--- web/src/frontend/src/nav-soknad/components/person/index.tsx
+++ web/src/frontend/src/nav-soknad/components/person/index.tsx
@@ -22,10 +22,10 @@ const Personskjema: React.StatelessComponent<Props> = ({
 			</Row>
 			<Row>
 				<Column sm="6" xs="3">
-					<InputFaktum faktumKey={fnrFaktumKey} maxLength={6} />
+					<InputFaktum faktumKey={fnrFaktumKey} maxLength={6} bredde="s" />
 				</Column>
 				<Column sm="6" xs="3">
-					<InputFaktum faktumKey={pnrFaktumKey} maxLength={5} />
+					<InputFaktum faktumKey={pnrFaktumKey} maxLength={5} bredde="s" />
 				</Column>
 			</Row>
 		</Container>
