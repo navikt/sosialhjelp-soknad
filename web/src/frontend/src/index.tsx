5fe73b25a6699e93ed06fdf62e2ba0223a4d8596
h151343 <frode.hansen@nav.no>
2017-09-05 10:23:43 +0200

    Fjerne editorconfig fra git

--- web/src/frontend/src/index.tsx
+++ web/src/frontend/src/index.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import registerServiceWorker from "./registerServiceWorker";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import App from "./digisos";
 import thunk from "redux-thunk";
@@ -15,33 +14,33 @@ import IntlProvider from "./intlProvider";
 import "./index.css";
 
 const rootReducer = combineReducers({
-  soknad: SoknadReducer,
-  faktumStore: FaktumReducer
+	soknad: SoknadReducer,
+	faktumStore: FaktumReducer
 });
 
 function configureStore() {
-  /* tslint:disable */
-  const devtools: any =
-    /* tslint:disable-next-line */
-    window["devToolsExtension"] && erDev()
-      ? /* tslint:disable-next-line */
-        window["devToolsExtension"]()
-      : (f: any) => f;
-  /* tslint:enable */
-  const middleware = applyMiddleware(thunk);
-  return middleware(devtools(createStore))(rootReducer);
+	/* tslint:disable */
+	const devtools: any =
+		/* tslint:disable-next-line */
+		window["devToolsExtension"] && erDev()
+			? /* tslint:disable-next-line */
+				window["devToolsExtension"]()
+			: (f: any) => f;
+	/* tslint:enable */
+	const middleware = applyMiddleware(thunk);
+	return middleware(devtools(createStore))(rootReducer);
 }
 
 const store = configureStore();
 ReactDOM.render(
-  <IntlProvider>
-    <Provider store={store}>
-      <BrowserRouter basename="soknadsosialhjelp">
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </IntlProvider>,
-  document.getElementById("root") as HTMLElement
+	<IntlProvider>
+		<Provider store={store}>
+			<BrowserRouter basename="soknadsosialhjelp">
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</IntlProvider>,
+	document.getElementById("root") as HTMLElement
 );
 
 export { store };
