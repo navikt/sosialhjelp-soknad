52a7926e3bd5fbcc9d2df0b9711dd83abe7fbec5
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-09-07 14:49:29 +0200

    bytt ut public med build

--- web/src/frontend/herokubackend.js
+++ web/src/frontend/herokubackend.js
@@ -35,7 +35,7 @@ router.get("/sendsoknad/soknader/:brukerBehandlingId/oppsummering", function(req
 app.use(utils.allowCrossDomain);
 app.use('/', router);
 app.get("/*", function (req, res) {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/build/index.html");
 });
 
 app.listen(port);
