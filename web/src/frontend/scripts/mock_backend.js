5967eba8035f43dadf4ab548a3db0fa9985cc4a5
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-09-14 13:54:24 +0200

    oppdaterer fakta.json basert på det bruker svarer
    * Alle endringer i faktastrukturen må gjøres i opprinneligFakta da denne er basen for faktastrukturen som blir brukt ved oppdatering av faktumverdier
    
    * Ignore fakta.json da denne blir lik som opprinneligFaktafilen ved start og endres ved lagring
    
    * Trekk ut fakta fra soknad.json da denne må returnere riktig state av faktaene etter lagring av bruker

--- web/src/frontend/scripts/mock_backend.js
+++ web/src/frontend/scripts/mock_backend.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 const utils = require("./utils.js");
+const fs = require('fs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -10,6 +11,22 @@ app.use(utils.allowCrossDomain);
 
 var port = process.env.PORT || 3001;
 var router = express.Router();
+
+
+function settOpprineligeFaktaTilFakta() {
+	const opprinneligeFakta = utils.lesMockDataFil("opprinneligFakta.json");
+	var fakta = utils.lesMockDataFil("fakta.json");
+
+	fakta = JSON.parse(JSON.stringify(opprinneligeFakta));
+
+	const fileName = utils.getFilePath("fakta.json");
+	fs.writeFile(fileName, JSON.stringify(fakta), function (err) {
+		if(err) return console.log(err);
+		console.log("Setter opprinneligeFakta til fakta");
+	});
+};
+settOpprineligeFaktaTilFakta();
+
 
 // Språkdata tjeneste /api/tekster
 router.get('/informasjon/tekster', function (req, res) {
@@ -27,7 +44,11 @@ router.post("/soknader", function (req, res) {
 
 router.get("/soknader/:brukerBehandlingId", function (req, res) {
     if(req.accepts('application/json') ) {
-        res.json(utils.lesMockDataFil("soknad.json"));
+    	const soknad = utils.lesMockDataFil("soknad.json");
+    	soknad.fakta = utils.lesMockDataFil("fakta.json");
+
+    	console.log("get soknader");
+        res.json(soknad);
     } else {
         res.status(406);
         res.json({feil: "Forventer Accept: application/json i header"});
@@ -38,26 +59,42 @@ router.get("/soknader/:brukerBehandlingId/oppsummering", function (req, res) {
     res.send(utils.lesMockHtmlFil("oppsummering.html"));
 });
 
-// Faktaressurser
-var fakta = utils.lesMockDataFil("fakta.json");
 
 router.get("/soknader/:brukerBehandlingId/fakta", function (req, res) {
-    res.json(fakta);
+	console.log("get fakta");
+	var fakta = utils.lesMockDataFil("fakta.json");
+	res.json(fakta);
 });
 
 router.get("/fakta/:faktumId", function (req, res) {
-    res.json(utils.hentFaktum(req.params.faktumId, fakta));
+	console.log("get faktum");
+	const fakta = utils.lesMockDataFil("fakta.json");
+	res.json(utils.hentFaktum(req.params.faktumId, fakta));
 });
 
 router.put("/fakta/:faktumId", function (req, res) {
-    fakta[utils.finnFaktaIndex(req.params.faktumId, fakta)] = req.body;
-    res.json(utils.hentFaktum(req.params.faktumId, fakta));
+	const fakta = utils.lesMockDataFil("fakta.json");
+	const faktum = req.body;
+	fakta[utils.finnFaktaIndex(faktum.faktumId, fakta)] = faktum;
+	return oppdaterFaktum(faktum, res, fakta, "put");
 });
 
 router.post("/fakta/:faktumId", function (req, res) {
-    fakta.push(req.body);
-    res.json(utils.hentFaktum(req.params.faktumId, fakta));
+	const fakta = utils.lesMockDataFil("fakta.json");
+	const faktum = req.body;
+	fakta.push(faktum);
+	return oppdaterFaktum(faktum, res, fakta, "post");
 });
+
+function oppdaterFaktum(faktum, res, fakta, metode) {
+	const fileName = utils.getFilePath("fakta.json");
+	fs.writeFile(fileName, JSON.stringify(fakta), function (err) {
+		if(err) return console.log(err);
+
+		console.log(metode +  " faktum ", faktum.key, faktum.value)
+	});
+	return res.json(utils.hentFaktum(faktum.faktumId, fakta));
+}
 
 app.use('/', router);
 
