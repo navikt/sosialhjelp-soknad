b6bb7553c7e6dc82aca1804a6330fac3eaa54a33
Frode Hansen <frode.hansen@bekk.no>
2017-09-14 11:13:50 +0200

    Gjøre bekreft oppsummering til locale state

--- web/src/frontend/src/digisos/skjema/oppsummering/index.tsx
+++ web/src/frontend/src/digisos/skjema/oppsummering/index.tsx
@@ -3,23 +3,32 @@ import { connect } from "react-redux";
 import StegFaktum from "../../../nav-soknad/faktum/StegFaktum";
 import { DispatchProps } from "../../../nav-soknad/redux/faktaTypes";
 import { FaktumComponentProps } from "../../../nav-soknad/redux/faktaReducer";
-import CheckboxFaktum, {
-	createCheckboxFaktumKey
-} from "../../../nav-soknad/faktum/CheckboxFaktum";
+import { Checkbox } from "nav-frontend-skjema";
 import { hentOppsummering } from "../../redux/oppsummering/oppsummeringActions";
 import { State } from "../../redux/reducers";
+import { injectIntl, InjectedIntlProps } from "react-intl";
 import "./oppsummering.css";
 
 interface StateProps {
 	oppsummering?: string;
 }
 
-type Props = FaktumComponentProps & DispatchProps & StateProps;
+interface LocalState {
+	bekreftet: boolean;
+}
 
-class Oppsummering extends React.Component<Props> {
+type Props = FaktumComponentProps &
+	DispatchProps &
+	StateProps &
+	InjectedIntlProps;
+
+class Oppsummering extends React.Component<Props, LocalState> {
 	constructor(props: Props) {
 		super(props);
 		this.getOppsummering = this.getOppsummering.bind(this);
+		this.state = {
+			bekreftet: false
+		};
 	}
 	componentDidMount() {
 		this.props.dispatch(hentOppsummering("1000B8FNi"));
@@ -39,8 +48,13 @@ class Oppsummering extends React.Component<Props> {
 					}
 				/>
 				<div className="skjema-oppsummering__bekreft">
-					<CheckboxFaktum
-						faktumKey={createCheckboxFaktumKey("oppsummering.bekreft", "true")}
+					<Checkbox
+						label={this.props.intl.formatMessage({
+							id: "oppsummering.bekreft.true"
+						})}
+						checked={this.state.bekreftet}
+						onChange={evt =>
+							this.setState({ bekreftet: (evt as any).target.checked })}
 					/>
 				</div>
 			</StegFaktum>
@@ -52,4 +66,4 @@ export default connect((state: State, props: any) => {
 	return {
 		oppsummering: state.oppsummering.oppsummering
 	};
-})(Oppsummering);
+})(injectIntl(Oppsummering));
