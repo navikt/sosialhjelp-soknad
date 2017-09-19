af944c122da772aed4454209f5998f4fa5222c01
frode hansen <frode.hansen@bekk.no>
2017-09-19 20:16:20 +0200

    merge

--- web/src/frontend/src/digisos/index.tsx
+++ web/src/frontend/src/digisos/index.tsx
@@ -1,25 +1,20 @@
 import * as React from "react";
 import "./styles/app.css";
-import { FormattedMessage } from "react-intl";
 import { Route, Switch } from "react-router";
 import Start from "./start";
-import Skjema from "./skjema";
+import SkjemaRouter from "./skjema/";
 import Kvittering from "./kvittering";
-import { Sidetittel } from "nav-frontend-typografi";
+import FeilSide from "../nav-soknad/components/feilmeldinger/feilSide";
 
 class App extends React.Component<{}, {}> {
 	render() {
 		return (
 			<div className="app-digisos">
-				<div className="app-digisos__title">
-					<Sidetittel>
-						<FormattedMessage id="skjema.tittel" />
-					</Sidetittel>
-				</div>
 				<Switch>
-					<Route path={`/informasjon`} exact={true} component={Start} />
-					<Route path={`/skjema/:brukerBehandlingId`} component={Skjema} />
-					<Route path={`/kvittering`} component={Kvittering} />
+					<Route path={`/informasjon`} exact={true} component={Start}/>
+					<Route path={`/skjema/:brukerBehandlingId`} component={SkjemaRouter}/>
+					<Route path={`/kvittering`} component={Kvittering}/>
+					<Route component={FeilSide} />
 				</Switch>
 				{this.props.children}
 			</div>
