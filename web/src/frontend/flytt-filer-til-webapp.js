28c002d74fabb2cf1a7adb6993bead4c608c270e
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-09-20 14:41:23 +0200

    bruker path til filstier for å ta hensyn til linux og windows

--- web/src/frontend/flytt-filer-til-webapp.js
+++ web/src/frontend/flytt-filer-til-webapp.js
@@ -1,15 +1,21 @@
 var fsextra = require('fs-extra');
 var fs = require('fs');
+const path = require("path");
 
-fs.readdir('build/static', function (err, folders)  {
+const mainFolder = [".", "build", "static"];
+fs.readdir(mainFolder.join(path.sep), function (err, folders)  {
 	folders.forEach(function(folder) {
-		fs.readdir('build/static/' + folder, function (err, files) {
+		var dir = [".", "build", "static", folder];
+
+		fs.readdir(dir.join(path.sep), function (err, files) {
 			var mainFile = files[0];
 
 			if(mainFile) {
 				var fileending = mainFile.split('.')[2];
 				var filename =  mainFile.split('.')[0];
-				fsextra.copy('build/static/' + folder + '/' + mainFile, '../main/webapp/statisk/' + folder + '/' +  filename +'.'+ fileending, function (err) {
+				var srcFile = [".", "build", "static", folder, mainFile];
+				var outFile = ["..", "main", "webapp", "statisk", folder, filename +'.'+ fileending];
+				fsextra.copy(srcFile.join(path.sep), outFile.join(path.sep), function (err) {
 					if(err){
 						console.log('build/static/' + folder + '/' + mainFile);
 
