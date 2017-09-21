6a19e99c9c7cb1dadb6c1ffdadaf20a961556170
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-09-21 17:16:33 +0200

    utvid faktautils med FaktumId

--- web/src/frontend/src/nav-soknad/redux/faktaUtils.ts
+++ web/src/frontend/src/nav-soknad/redux/faktaUtils.ts
@@ -1,13 +1,26 @@
 import { Faktum } from "./faktaTypes";
+import { erDev } from "../../digisos/redux/rest-utils";
 
-export function finnFaktum(faktumKey: string, fakta: Faktum[]): Faktum {
+export function finnFaktum(faktumKey: string, fakta: Faktum[], faktumId?: number): Faktum {
+	if (faktumId) {
+		return finnFaktumMedId(faktumKey, fakta, faktumId);
+	}
 	const faktum = fakta.filter((f: Faktum) => {
 		return f.key === faktumKey;
 	});
 	if (faktum.length === 0) {
-		throw new Error("Faktum ikke funnet: " + faktumKey);
+		if (erDev()) {
+			// tslint:disable-next-line
+			console.log("Faktum ikke funnet: " + faktumKey);
+		}
 	}
 	return faktum[0];
+}
+
+export function finnFaktumMedId(faktumKey: string, fakta: Faktum[], faktumId: number): Faktum {
+	return fakta.filter((f: Faktum) => {
+		return f.faktumId === faktumId;
+	})[0];
 }
 
 export function finnFakta(faktumKey: string, fakta: Faktum[]): Faktum[] {
