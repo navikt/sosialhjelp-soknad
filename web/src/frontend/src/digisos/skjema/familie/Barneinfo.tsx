92f1d9cf227d300f332012e265667f4a32e391df
frode hansen <frode.hansen@bekk.no>
2017-09-22 11:05:39 +0200

    Legge til role=button på lenke

--- web/src/frontend/src/digisos/skjema/familie/Barneinfo.tsx
+++ web/src/frontend/src/digisos/skjema/familie/Barneinfo.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
-import { FaktumComponentProps, SoknadAppState } from "../../../nav-soknad/redux/faktaReducer";
+import {
+	FaktumComponentProps,
+	SoknadAppState
+} from "../../../nav-soknad/redux/faktaReducer";
 import { finnFakta, finnFaktum } from "../../../nav-soknad/redux/faktaUtils";
 import { connect } from "react-redux";
 import { DispatchProps } from "../../../nav-soknad/redux/faktaTypes";
@@ -14,29 +17,38 @@ interface OwnProps {
 	parentFaktumKey: string;
 }
 
-class Barneinfo extends React.Component<OwnProps & FaktumComponentProps & DispatchProps, {}> {
+class Barneinfo extends React.Component<
+	OwnProps & FaktumComponentProps & DispatchProps,
+	{}
+> {
 	componentDidMount() {
-		const {fakta, faktumKey, parentFaktumKey} = this.props;
+		const { fakta, faktumKey, parentFaktumKey } = this.props;
 		const parentFaktum = finnFaktum(parentFaktumKey, fakta);
 		const faktum = finnFaktum(faktumKey, fakta);
 		if (!faktum) {
-			this.props.dispatch(opprettFaktum({key: faktumKey, parrentFaktum: parentFaktum.faktumId}));
+			this.props.dispatch(
+				opprettFaktum({ key: faktumKey, parrentFaktum: parentFaktum.faktumId })
+			);
 		}
 	}
 
 	render() {
-		const {fakta, faktumKey, parentFaktumKey} = this.props;
+		const { fakta, faktumKey, parentFaktumKey } = this.props;
 		const alleBarn = finnFakta(faktumKey, fakta);
 		const parrentFaktum = finnFaktum(parentFaktumKey, fakta);
-		const leggTilBarn = (): any => this.props.dispatch(opprettFaktum({
-				key: faktumKey,
-				parrentFaktum: parrentFaktum.faktumId
-			}));
+		const leggTilBarn = (): any =>
+			this.props.dispatch(
+				opprettFaktum({
+					key: faktumKey,
+					parrentFaktum: parrentFaktum.faktumId
+				})
+			);
 		return (
 			<div>
 				{alleBarn.map((barnFaktum: Faktum) => (
-					<Barn fakta={fakta} faktum={barnFaktum} key={barnFaktum.faktumId}/>))}
-				<a href="javascript:void(0);" onClick={leggTilBarn}>
+					<Barn fakta={fakta} faktum={barnFaktum} key={barnFaktum.faktumId} />
+				))}
+				<a href="javascript:void(0);" onClick={leggTilBarn} role="button">
 					<FormattedMessage id="familie.barn.true.barn.leggtil" />
 				</a>
 			</div>
@@ -48,7 +60,11 @@ interface StateFromProps {
 	fakta: Faktum[];
 }
 
-export default connect<StateFromProps, {}, OwnProps>((state: SoknadAppState) => {
+export default connect<
+	StateFromProps,
+	{},
+	OwnProps
+>((state: SoknadAppState) => {
 	return {
 		fakta: state.fakta.data
 	};
