ac1b1a9ee0d48762f550af73d0c27b935103ec8f
frode hansen <frode.hansen@bekk.no>
2017-09-25 17:36:51 +0200

    Oppdatere mock

--- web/src/frontend/scripts/mock_backend.js
+++ web/src/frontend/scripts/mock_backend.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 const utils = require("./utils.js");
-const fs = require("fs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,26 +11,14 @@ app.use(utils.allowCrossDomain);
 var port = process.env.PORT || 3001;
 var router = express.Router();
 
-function settOpprineligeFaktaTilFakta() {
-	const opprinneligeFakta = utils.lesMockDataFil("opprinneligFakta.json");
-	var fakta = utils.lesMockDataFil("fakta.json");
-
-	fakta = JSON.parse(JSON.stringify(opprinneligeFakta));
-
-	const fileName = utils.getFilePath("fakta.json");
-	fs.writeFile(fileName, JSON.stringify(fakta), function(err) {
-		if (err) return console.log(err);
-		console.log("Setter opprinneligeFakta til fakta");
-	});
-}
-settOpprineligeFaktaTilFakta();
-
 // Språkdata tjeneste /api/tekster
 router.get("/informasjon/tekster", function(req, res) {
 	res.json(utils.lesSpraakfil());
 });
 
 const brukerBehandlingId = "1000B7FGM";
+const soknad = utils.lesMockDataFil("soknad.json");
+const fakta = soknad.fakta;
 
 // Søknadsressurser
 router.post("/soknader", function(req, res) {
@@ -41,15 +28,16 @@ router.post("/soknader", function(req, res) {
 });
 
 router.get("/soknader/:brukerBehandlingId", function(req, res) {
-	if (req.accepts("application/json")) {
-		const soknad = utils.lesMockDataFil("soknad.json");
-		soknad.fakta = utils.lesMockDataFil("fakta.json");
-
-		console.log("get soknader");
-		res.json(soknad);
+	console.log("get soknader");
+	if (req.headers["content-type"] === "text/html") {
+		res.send(utils.lesMockHtmlFil("oppsummering.html"));
 	} else {
-		res.status(406);
-		res.json({ feil: "Forventer Accept: application/json i header" });
+		if (req.accepts("application/json")) {
+			res.json(utils.lesMockDataFil("soknad.json"));
+		} else {
+			res.status(406);
+			res.json({ feil: "Forventer Accept: application/json i header" });
+		}
 	}
 });
 
@@ -59,28 +47,24 @@ router.get("/soknader/:brukerBehandlingId/oppsummering", function(req, res) {
 
 router.get("/soknader/:brukerBehandlingId/fakta", function(req, res) {
 	console.log("get fakta");
-	var fakta = utils.lesMockDataFil("fakta.json");
 	res.json(fakta);
 });
 
 router.get("/fakta/:faktumId", function(req, res) {
 	console.log("get faktum");
-	const fakta = utils.lesMockDataFil("fakta.json");
-	res.json(utils.hentFaktum(req.params.faktumId, fakta));
+	res.json(utils.hentFaktum(req.params.faktumId));
 });
 
 router.put("/fakta/:faktumId", function(req, res) {
-	const fakta = utils.lesMockDataFil("fakta.json");
 	const faktum = req.body;
 	fakta[utils.finnFaktaIndex(faktum.faktumId, fakta)] = faktum;
-	return oppdaterFaktum(faktum, res, fakta, "put");
+	return res.json(utils.hentFaktum(faktum.faktumId, fakta));
 });
 
 router.post("/fakta/:faktumId", function(req, res) {
-	const fakta = utils.lesMockDataFil("fakta.json");
 	const faktum = req.body;
 	fakta.push(faktum);
-	return oppdaterFaktum(faktum, res, fakta, "post");
+	return res.json(utils.hentFaktum(faktum.faktumId, fakta));
 });
 
 var faktumid = 1000;
