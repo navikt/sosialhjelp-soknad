d29e6effa78a49f70199a32e69271167b0c7a7ef
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-09-27 12:15:27 +0200

    endre spørsmålformulering for bostøtte samt fjerne underspørsmålene

--- web/src/frontend/src/digisos/skjema/inntektFormue/Bostotte.tsx
+++ web/src/frontend/src/digisos/skjema/inntektFormue/Bostotte.tsx
@@ -1,34 +1,16 @@
 import * as React from "react";
 import SporsmalFaktum from "../../../nav-soknad/faktum/SporsmalFaktum";
 import { FaktumComponentProps } from "../../../nav-soknad/redux/reducer";
-import { radioCheckKeys, faktumIsSelected, getFaktumVerdi } from "../../../nav-soknad/utils";
+import { radioCheckKeys } from "../../../nav-soknad/utils";
 
 import RadioFaktum from "../../../nav-soknad/faktum/RadioFaktum";
-import CheckboxFaktum from "../../../nav-soknad/faktum/CheckboxFaktum";
-import SkjemagruppeFaktum from "../../../nav-soknad/faktum/SkjemagruppeFaktum";
-import Underskjema from "../../../nav-soknad/components/underskjema";
 
 class Bostotte extends React.Component<FaktumComponentProps, {}> {
 	render() {
-		const { fakta } = this.props;
 		const bostotte = radioCheckKeys("inntekt.bostotte");
-		const hvilkenStotte = radioCheckKeys("inntekt.bostotte.true.type");
 		return (
 			<SporsmalFaktum faktumId={bostotte.faktum}>
 				<RadioFaktum faktumKey={bostotte.faktum} option="true" />
-				<Underskjema visible={faktumIsSelected(getFaktumVerdi(fakta, "inntekt.bostotte"))}>
-					<SkjemagruppeFaktum faktumId={hvilkenStotte.faktum}>
-						{/*TODO legge til checkboxgroup-faktum*/}
-						<CheckboxFaktum
-							faktumKey={hvilkenStotte.faktum}
-							option="husbanken"
-						/>
-						<CheckboxFaktum
-							faktumKey={hvilkenStotte.faktum}
-							option="kommunal"
-						/>
-					</SkjemagruppeFaktum>
-				</Underskjema>
 				<RadioFaktum faktumKey={bostotte.faktum} option="false" />
 			</SporsmalFaktum>
 		);
