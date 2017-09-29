d3cee7d5a693cd8d41ca3a544286eea5e908fd1d
Frode Hansen <frode.hansen@bekk.no>
2017-09-29 15:00:30 +0200

    Vise feilside når det mangler info i url om steg og brukerbehanldingId

---
+++ web/src/frontend/src/nav-soknad/components/loadContainer/LoadContainer.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { FormattedMessage } from "react-intl";
+import NavFrontendSpinner from "nav-frontend-spinner";
+
+import { REST_STATUS } from "../../types";
+
+interface Props {
+	restStatus: string;
+}
+
+const LoadContainer: React.StatelessComponent<Props> = ({
+	restStatus,
+	children
+}) => {
+	if (
+		restStatus === REST_STATUS.INITIALISERT ||
+		restStatus === REST_STATUS.PENDING
+	) {
+		return (
+			<div className="application-spinner">
+				<NavFrontendSpinner storrelse="xxl" />
+			</div>
+		);
+	} else if (restStatus === REST_STATUS.FEILET || true) {
+		return (
+			<p>
+				<FormattedMessage id="applikasjon.ukjentFeilUnderHentData" />
+			</p>
+		);
+	}
+	// return <div>{children}</div>;
+};
+
+export default LoadContainer;
