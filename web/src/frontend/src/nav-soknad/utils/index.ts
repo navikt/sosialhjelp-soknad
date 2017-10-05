dfe5703d0436d7263162182fb6e68a77e2276e95
Ingvild Indrebø <ingvild.indrebo@nav.no>
2017-10-05 09:26:01 +0200

    sporsmalfaktum vil aldri være activeElement så fjerner unødvendig sjekk

--- web/src/frontend/src/nav-soknad/utils/index.ts
+++ web/src/frontend/src/nav-soknad/utils/index.ts
@@ -7,10 +7,6 @@ export function boolToString(flag: boolean) {
 	return flag ? "true" : "false";
 }
 
-export function is(node: Element, child: Element) {
-	return node === child;
-}
-
 export function erDev(): boolean {
 	const url = window.location.href;
 	return url.indexOf("localhost:3000") > 0;
