9e00de7a77c58fa7906be5fee8c4a7315cdb0265
Ørjan Helstrøm <Orjan.Breimo.Helstrm@nav.no>
2017-10-16 12:26:28 +0200

    DIGISOS-150-endre-visning-inntekt-formue * Fjern Ja-Nei valg før man velger Eiendeler og Bankinnskudd * Oppdater tekster

--- web/src/frontend/src/digisos/skjema/inntektFormue/Bankinnskudd.tsx
+++ web/src/frontend/src/digisos/skjema/inntektFormue/Bankinnskudd.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 
 import SporsmalFaktum from "../../../nav-soknad/faktum/SporsmalFaktum";
-import RadioFaktum from "../../../nav-soknad/faktum/RadioFaktum";
 import CheckboxFaktum, {
 	createCheckboxFaktumKey
 } from "../../../nav-soknad/faktum/CheckboxFaktum";
 import TextareaFaktum from "../../../nav-soknad/faktum/TextareaFaktum";
-import Underskjema from "../../../nav-soknad/components/underskjema";
 import { FaktumComponentProps } from "../../../nav-soknad/redux/faktaReducer";
 import {
 	radioCheckKeys,
@@ -19,56 +17,54 @@ class Bankinnskudd extends React.Component<FaktumComponentProps, {}> {
 	render() {
 		const { fakta } = this.props;
 		const innskudd = radioCheckKeys("inntekt.bankinnskudd");
-		const hvilkeInnskudd = radioCheckKeys("inntekt.bankinnskudd.true.type");
-		const hvilkeInnskuddAnnet = "inntekt.bankinnskudd.true.type.annet";
+		const hvilkeInnskudd = radioCheckKeys("inntekt.bankinnskudd.type");
+		const hvilkeInnskuddAnnet = "inntekt.bankinnskudd.type.annet";
 		return (
 			<SporsmalFaktum faktumKey={innskudd.faktum}>
-				<RadioFaktum faktumKey={innskudd.faktum} value="true" />
-				<Underskjema
-					visible={faktumIsSelected(getFaktumVerdi(fakta, innskudd.faktum))}
-				>
-					<SporsmalFaktum faktumKey={hvilkeInnskudd.faktum}>
-						{/*TODO checkboxbgroup-faktum*/}
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeInnskudd.faktum,
-								"brukskonto"
-							)}
-						/>
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeInnskudd.faktum,
-								"sparekonto"
-							)}
-						/>
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeInnskudd.faktum,
-								"livsforsikring"
-							)}
-						/>
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeInnskudd.faktum,
-								"aksjer"
-							)}
-						/>
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeInnskudd.faktum,
-								"annet"
-							)}
-						/>
-						{faktumIsSelected(getFaktumVerdi(fakta, hvilkeInnskuddAnnet)) ? (
-							<TextareaFaktum
-								faktumKey={`${hvilkeInnskuddAnnet}.true.beskrivelse`}
-								maxLength={400}
-								validerFunc={[getMaksLengdeFunc(400)]}
-							/>
-						) : null}
-					</SporsmalFaktum>
-				</Underskjema>
-				<RadioFaktum faktumKey={innskudd.faktum} value="false" />
+				{/*TODO checkboxbgroup-faktum*/}
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeInnskudd.faktum,
+						"brukskonto"
+					)}
+				/>
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeInnskudd.faktum,
+						"sparekonto"
+					)}
+				/>
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeInnskudd.faktum,
+						"bsu"
+					)}
+				/>
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeInnskudd.faktum,
+						"livsforsikring"
+					)}
+				/>
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeInnskudd.faktum,
+						"aksjer"
+					)}
+				/>
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeInnskudd.faktum,
+						"annet"
+					)}
+				/>
+				{faktumIsSelected(getFaktumVerdi(fakta, hvilkeInnskuddAnnet)) ? (
+					<TextareaFaktum
+						faktumKey={`${hvilkeInnskuddAnnet}.true.beskrivelse`}
+						maxLength={400}
+						validerFunc={[getMaksLengdeFunc(400)]}
+					/>
+				) : null}
 			</SporsmalFaktum>
 		);
 	}
--- web/src/frontend/src/digisos/skjema/inntektFormue/Eiendeler.tsx
+++ web/src/frontend/src/digisos/skjema/inntektFormue/Eiendeler.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
 
 import SporsmalFaktum from "../../../nav-soknad/faktum/SporsmalFaktum";
-import RadioFaktum from "../../../nav-soknad/faktum/RadioFaktum";
 import CheckboxFaktum, {
 	createCheckboxFaktumKey
 } from "../../../nav-soknad/faktum/CheckboxFaktum";
 import TextareaFaktum from "../../../nav-soknad/faktum/TextareaFaktum";
-import Underskjema from "../../../nav-soknad/components/underskjema";
 import { FaktumComponentProps } from "../../../nav-soknad/redux/faktaReducer";
 import {
 	radioCheckKeys,
@@ -19,55 +17,47 @@ class Eiendeler extends React.Component<FaktumComponentProps, {}> {
 	render() {
 		const { fakta } = this.props;
 		const eiendeler = radioCheckKeys("inntekt.eierandeler");
-		const hvilkeEiendeler = radioCheckKeys("inntekt.eierandeler.true.type");
-		const hvilkeEiendelerAnnet = "inntekt.eierandeler.true.type.annet";
+		const hvilkeEiendeler = radioCheckKeys("inntekt.eierandeler.type");
+		const hvilkeEiendelerAnnet = "inntekt.eierandeler.type.annet";
 		return (
 			<SporsmalFaktum faktumKey={eiendeler.faktum}>
-				<RadioFaktum faktumKey={eiendeler.faktum} value="true" />
-				<Underskjema
-					visible={faktumIsSelected(getFaktumVerdi(fakta, eiendeler.faktum))}
-				>
-					<SporsmalFaktum faktumKey={hvilkeEiendeler.faktum}>
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeEiendeler.faktum,
-								"bolig"
-							)}
-						/>
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeEiendeler.faktum,
-								"campingvogn"
-							)}
-						/>
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeEiendeler.faktum,
-								"kjoretoy"
-							)}
-						/>
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeEiendeler.faktum,
-								"fritidseiendom"
-							)}
-						/>
-						<CheckboxFaktum
-							faktumKey={createCheckboxFaktumKey(
-								hvilkeEiendeler.faktum,
-								"annet"
-							)}
-						/>
-						{faktumIsSelected(getFaktumVerdi(fakta, hvilkeEiendelerAnnet)) ? (
-							<TextareaFaktum
-								faktumKey={`${hvilkeEiendelerAnnet}.true.beskrivelse`}
-								maxLength={400}
-								validerFunc={[getMaksLengdeFunc(400)]}
-							/>
-						) : null}
-					</SporsmalFaktum>
-				</Underskjema>
-				<RadioFaktum faktumKey={eiendeler.faktum} value="false" />
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeEiendeler.faktum,
+						"bolig"
+					)}
+				/>
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeEiendeler.faktum,
+						"campingvogn"
+					)}
+				/>
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeEiendeler.faktum,
+						"kjoretoy"
+					)}
+				/>
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeEiendeler.faktum,
+						"fritidseiendom"
+					)}
+				/>
+				<CheckboxFaktum
+					faktumKey={createCheckboxFaktumKey(
+						hvilkeEiendeler.faktum,
+						"annet"
+					)}
+				/>
+				{faktumIsSelected(getFaktumVerdi(fakta, hvilkeEiendelerAnnet)) ? (
+					<TextareaFaktum
+						faktumKey={`${hvilkeEiendelerAnnet}.true.beskrivelse`}
+						maxLength={400}
+						validerFunc={[getMaksLengdeFunc(400)]}
+					/>
+				) : null}
 			</SporsmalFaktum>
 		);
 	}
