89ffcb0323601e06c1e07df5d4b6b95f4c712d83
Flemming, Thomas <thomas.flemming@gmail.com>
2017-10-31 10:52:49 +0100

    Formattering

--- web/src/frontend/src/rootSaga.ts
+++ web/src/frontend/src/rootSaga.ts
@@ -1,12 +1,18 @@
 import { all } from "redux-saga/effects";
 import ledeteksterSaga from "./nav-soknad/redux/ledetekster/ledeteksterSaga";
 import tilgangSaga from "./nav-soknad/redux/tilgang/tilgangSaga";
-
 import faktaSaga from "./nav-soknad/redux/fakta/faktaSaga";
 import navigasjonSaga from "./nav-soknad/redux/navigasjon/navigasjonSaga";
 import miljovariablerSaga from "./digisos/redux/miljovariabler/miljoVariablerSaga";
 import navloggerSaga from "./nav-soknad/redux/navlogger/navloggerSaga";
 
 export default function* rootSaga() {
-	yield all([tilgangSaga(), ledeteksterSaga(), faktaSaga(), navigasjonSaga()]);
+	yield all([
+		tilgangSaga(),
+		ledeteksterSaga(),
+		faktaSaga(),
+		navigasjonSaga(),
+		miljovariablerSaga(),
+		navloggerSaga()
+	]);
 }
