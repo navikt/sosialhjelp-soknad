d263756807a79088b8c940b36cab8777478fce32
frode hansen <frode.hansen@bekk.no>
2017-11-23 15:51:25 +0100

    Utvide underskjema-komponent

--- web/src/frontend/src/nav-soknad/components/underskjema/index.tsx
+++ web/src/frontend/src/nav-soknad/components/underskjema/index.tsx
@@ -1,34 +1,45 @@
 import * as React from "react";
+import * as classNames from "classnames";
 import { Collapse } from "react-collapse";
 
 interface UnderskjemaProps extends React.Props<any> {
 	tittel?: string;
 	visible: boolean;
+	arrow?: boolean;
+	collapsable?: boolean;
 	children: React.ReactNode;
 }
 
 const Underskjema: React.StatelessComponent<UnderskjemaProps> = ({
 	tittel,
 	visible,
+	arrow = true,
+	collapsable = true,
 	children
 }) => {
-	return (
-		<Collapse
-			isOpened={visible}
-			className="underskjema__wrapper"
-			hasNestedCollapse={true}
-		>
-			{visible ? (
-				<div className="underskjema">
-					<div className="underskjema__boks">
-						<div className="underskjema__innhold">{children}</div>
-					</div>
-				</div>
-			) : (
-				<div />
-			)}
-		</Collapse>
+	if (!visible && !collapsable) {
+		return null;
+	}
+	const cls = classNames("underskjema", { "underskjema--pil": arrow });
+	const renderContent = () => (
+		<div className={cls}>
+			<div className="underskjema__boks">
+				<div className="underskjema__innhold">{children}</div>
+			</div>
+		</div>
 	);
+	if (collapsable) {
+		return (
+			<Collapse
+				isOpened={visible}
+				className="underskjema__wrapper"
+				hasNestedCollapse={true}
+			>
+				{visible ? renderContent() : <div />}
+			</Collapse>
+		);
+	}
+	return <div className="underskjema__wrapper">{renderContent()}</div>;
 };
 
 export default Underskjema;
