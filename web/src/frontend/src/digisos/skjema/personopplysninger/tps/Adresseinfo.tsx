e778b9020c82c88639321ca0d53eaa920cd968f8
Frode Hansen <frode.hansen@bekk.no>
2017-12-20 12:58:58 +0100

    Forenkling av adresseinfovisning

--- web/src/frontend/src/digisos/skjema/personopplysninger/tps/Adresseinfo.tsx
+++ web/src/frontend/src/digisos/skjema/personopplysninger/tps/Adresseinfo.tsx
@@ -1,17 +1,12 @@
 import * as React from "react";
-import {
-	injectIntl,
-	InjectedIntlProps,
-	InjectedIntl
-} from "react-intl";
+import { injectIntl, InjectedIntlProps } from "react-intl";
 import { Faktum } from "../../../../nav-soknad/types";
-import {
-	finnFaktum
-} from "../../../../nav-soknad/utils";
+import { finnFaktum } from "../../../../nav-soknad/utils";
 import SysteminfoFaktum from "../../../../nav-soknad/faktum/SysteminfoFaktum";
 import InputFaktum from "../../../../nav-soknad/faktum/InputFaktum";
 import TallFaktum from "../../../../nav-soknad/faktum/typedInput/TallFaktum";
 import Detaljeliste, {
+	ElementProps,
 	DetaljelisteElement
 } from "../../../../nav-soknad/components/detaljeliste";
 
@@ -32,50 +27,18 @@ export const Skjema: React.StatelessComponent<{}> = () => (
 	</div>
 );
 
-class Adresserad {
-	tittel: string;
-	verdi: string;
-
-	constructor(tittel: string, verdi: string) {
-		this.tittel = tittel;
-		this.verdi = verdi;
-	}
-}
-
-export function adressePropertiesTilVisning(properties: object, intl: InjectedIntl): Adresserad[] {
-	const result = new Array<Adresserad>();
-	const propertyOrder: string[] = [
-		"adresse",
-		// Vises ikke til bruker etter krav fra interaksjonsdesigner:
-		// "kommunenummer"
-		// "bolignummer",
-		// "gatenavn",
-		// "husnummer",
-		"postnummer", "poststed",
-		"eiendomsnavn", "gaardsnummer", "bruksnummer", "festenummer", "seksjonsnummer", "undernummer"
-	];
-
-	for (const key of propertyOrder) {
-		const tittel = intl.formatMessage({id: "kontakt.system.adresse." + key + ".label"});
-		const value = properties[key];
-		if (value != null) {
-			result.push(new Adresserad(
-				tittel, properties[key]
-			));
-		}
-	}
-
-	return result;
-}
-
 const Kontaktinfo: React.StatelessComponent<Props & InjectedIntlProps> = ({
 	fakta,
 	intl
 }) => {
 	const adresseFaktum = finnFaktum("kontakt.system.adresse", fakta);
-	const adresseProperties = adressePropertiesTilVisning(adresseFaktum.properties, intl).map(
-		ap => <DetaljelisteElement key={ap.tittel} tittel={ap.tittel} verdi={ap.verdi} />
-	);
+
+	const getProperty = (key: string): ElementProps => ({
+		tittel: intl.formatMessage({
+			id: "kontakt.system.adresse." + key + ".label"
+		}),
+		verdi: adresseFaktum.properties[key]
+	});
 
 	return (
 		<SysteminfoFaktum
@@ -86,7 +49,14 @@ const Kontaktinfo: React.StatelessComponent<Props & InjectedIntlProps> = ({
 			})}
 		>
 			<Detaljeliste>
-				{adresseProperties}
+				<DetaljelisteElement {...getProperty("adresse")} />
+				<DetaljelisteElement {...getProperty("postnummer")} />
+				<DetaljelisteElement {...getProperty("eiendomsnavn")} />
+				<DetaljelisteElement {...getProperty("gaardsnummer")} />
+				<DetaljelisteElement {...getProperty("bruksnummer")} />
+				<DetaljelisteElement {...getProperty("festenummer")} />
+				<DetaljelisteElement {...getProperty("seksjonsnummer")} />
+				<DetaljelisteElement {...getProperty("undernummer")} />
 			</Detaljeliste>
 		</SysteminfoFaktum>
 	);
