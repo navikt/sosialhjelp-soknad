8897c7cc1e4f43e48c39e1661ddabecd3a00ec95
Stian Grenborgen <grenborgen.stian@nav.no>
2018-01-02 15:35:02 +0100

    [DIGISOS-188] [DIGISOS-189] Viser kun gjeldende verdi i oppsummering.

--- web/src/frontend/src/digisos/skjema/personopplysninger/index.tsx
+++ web/src/frontend/src/digisos/skjema/personopplysninger/index.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import { connect } from "react-redux";
 
 import { State } from "../../redux/reducers";
+import { DispatchProps } from "../../../nav-soknad/redux/reduxTypes";
 import { FaktumComponentProps } from "../../../nav-soknad/redux/fakta/faktaTypes";
 import SporsmalFaktum from "../../../nav-soknad/faktum/SporsmalFaktum";
 import JaNeiSporsmalFaktum from "../../../nav-soknad/faktum/JaNeiSporsmalFaktum";
 import TelefonFaktum from "../../../nav-soknad/faktum/typedInput/TelefonFaktum";
 import { Skjema as BankinformasjonSkjema } from "./tps/Bankinformasjon";
 import { FeatureToggles } from "../../../featureToggles";
-import { radioCheckKeys } from "../../../nav-soknad/utils";
+import { radioCheckKeys, harFaktumVerdi, finnFaktum, getPropertyVerdi } from "../../../nav-soknad/utils";
+import { lagreFaktum } from "../../../nav-soknad/redux/fakta/faktaActions";
 import Personalia from "./tps/Personalia";
 import Adresseinfo from "./tps/Adresseinfo";
 import Telefoninfo from "./tps/Telefoninfo";
@@ -24,9 +26,31 @@ interface StateProps {
 	hentVedleggsForventning?: (fakta: any) => void;
 }
 
-export type Props = StateProps & FaktumComponentProps;
+export type Props = StateProps & FaktumComponentProps & DispatchProps;
 
 class Personopplysninger extends React.Component<Props, StateProps> {
+	componentDidMount() {
+		/* XXX: Bør flyttes til backend: */
+		if (this.props.visPersonaliaFraTPSfeatureToggle) {
+			const oppdaterFaktumVerdi = (brukerendrettoggleFaktumKey: string, verdi: string) => {
+				const brukerEndret = finnFaktum(brukerendrettoggleFaktumKey, this.props.fakta);
+				brukerEndret.value = verdi;
+				this.props.dispatch(
+					lagreFaktum(brukerEndret)
+				);
+			};
+
+			if (!harFaktumVerdi(this.props.fakta, "kontakt.system.telefon")) {
+				oppdaterFaktumVerdi("kontakt.telefon.brukerendrettoggle", "true");
+			}
+			if (!harFaktumVerdi(this.props.fakta, "kontakt.kontonummer.telefon")) {
+				oppdaterFaktumVerdi("kontakt.kontonummer.brukerendrettoggle", "true");
+			}
+			if (getPropertyVerdi(this.props.fakta, "kontakt.system.adresse", "type") == null) {
+				oppdaterFaktumVerdi("kontakt.adresse.brukerendrettoggle", "true");
+			}
+		}
+	}
 
 	render() {
 		if (this.props.visPersonaliaFraTPSfeatureToggle) {
