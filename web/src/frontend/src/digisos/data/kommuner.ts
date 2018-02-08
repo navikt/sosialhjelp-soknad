84f29d71bda1e1175651bb6ac26d8cdce5ee2742
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2018-02-08 12:46:17 +0100

    [DIGISOS-423] Mulighet for å velge 2 bydeler i Bergen. Kommentert ut de som vil gjøre at det tryner i Oslo

--- web/src/frontend/src/digisos/data/kommuner.ts
+++ web/src/frontend/src/digisos/data/kommuner.ts
@@ -1,27 +1,37 @@
 import { Kommune, Bydel } from "../../nav-soknad/types";
 
-export const OsloBydeler: Bydel[] = [
-	{ id: "alna", navn: "Bydel Alna" },
-	{ id: "bjerke", navn: "Bydel Bjerke" },
+const OsloBydeler: Bydel[] = [
+	// { id: "alna", navn: "Bydel Alna" },
+	// { id: "bjerke", navn: "Bydel Bjerke" },
 	{ id: "frogner", navn: "Bydel Frogner" },
-	{ id: "gamleoslo", navn: "Bydel Gamle Oslo" },
+	// { id: "gamleoslo", navn: "Bydel Gamle Oslo" },
 	{ id: "grorud", navn: "Bydel Grorud" },
 	{ id: "grunerlokka", navn: "Bydel Grünerløkka" },
-	{ id: "nordreaker", navn: "Bydel Nordre Aker" },
-	{ id: "nordstrand", navn: "Bydel Nordstrand" },
-	{ id: "sagene", navn: "Bydel Sagene" },
-	{ id: "sthanshaugen", navn: "Bydel St. Hanshaugen" },
-	{ id: "stovner", navn: "Bydel Stovner" },
-	{ id: "sondrenordstrand", navn: "Bydel Søndre Nordstrand" },
-	{ id: "ullern", navn: "Bydel Ullern" },
-	{ id: "vestreaker", navn: "Bydel Vestre Aker" },
-	{ id: "ostonsjo", navn: "Bydel Østensjø" }
+	// { id: "nordreaker", navn: "Bydel Nordre Aker" },
+	// { id: "nordstrand", navn: "Bydel Nordstrand" },
+	// { id: "sagene", navn: "Bydel Sagene" },
+	// { id: "sthanshaugen", navn: "Bydel St. Hanshaugen" },
+	// { id: "stovner", navn: "Bydel Stovner" },
+	// { id: "sondrenordstrand", navn: "Bydel Søndre Nordstrand" },
+	// { id: "ullern", navn: "Bydel Ullern" },
+	// { id: "vestreaker", navn: "Bydel Vestre Aker" },
+	// { id: "ostonsjo", navn: "Bydel Østensjø" }
+];
+
+const BergenBydeler: Bydel[] = [
+	{ id: "bergenhus", navn: "Bydel Bergenhus"},
+	{ id: "ytrebygda", navn: "Bydel Ytebygda"}
 ];
 
 export const Kommuner: Kommune[] = [
 	{
 		id: "horten",
 		navn: "Horten"
+	},
+	{
+		id: "bergen",
+		navn: "Bergen",
+		bydeler: BergenBydeler
 	},
 	{
 		id: "oslo",
@@ -33,8 +43,10 @@ export const Kommuner: Kommune[] = [
 export function getBosted(kommuneId: string, bydelId?: string): string {
 	/** Denne koden fungerer kun så lenge kommune og bydel er hardkodet i frontend */
 	const kommune = Kommuner.find(k => k.id === kommuneId);
-	const bydel =
-		kommuneId === "oslo" ? OsloBydeler.find(b => b.id === bydelId) : undefined;
+
+	const bydel = kommune.bydeler ?
+		kommune.bydeler.find(b => b.id === bydelId)
+		: undefined;
 
 	if (!kommune) {
 		return "N/A";
