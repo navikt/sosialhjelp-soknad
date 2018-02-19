83f1a1efc2cbd0c1be1d9c37b231657a2f6017bd
Mats Krüger Svensson <mats.kruger.svensson@nav.no>
2018-02-19 15:05:38 +0100

    [kaizen] Fjerner tester

--- web/src/frontend/src/digisos/data/kommuner.test.ts
+++ web/src/frontend/src/digisos/data/kommuner.test.ts
@@ -3,23 +3,23 @@ import { Kommune } from "../../nav-soknad/types";
 
 describe("kommuner", () => {
 
-	it("should return list of 'bydeler'", () => {
-		let oslo: Kommune = Kommuner.find(k => k.id === "oslo");
-		const antallBydeler = oslo.bydeler.length;
-		expect(antallBydeler).toBeGreaterThanOrEqual(2);
-	});
-
-	it("should return name of 'kommune'", () => {
-		const navn = getKommune("oslo");
-		expect(navn).toEqual("Oslo");
-	});
-
-	it("should return a valid 'bosted 'string", () => {
-		let bosted = getBosted("oslo", "frogner");
-		expect(bosted).toEqual("Oslo, Bydel Frogner");
-
-		bosted = getBosted("oslo", "sondrenordstrand");
-		expect(bosted).toEqual("Oslo, Bydel Søndre Nordstrand");
-	});
+	// it("should return list of 'bydeler'", () => {
+	// 	let oslo: Kommune = Kommuner.find(k => k.id === "oslo");
+	// 	const antallBydeler = oslo.bydeler.length;
+	// 	expect(antallBydeler).toBeGreaterThanOrEqual(2);
+	// });
+	//
+	// it("should return name of 'kommune'", () => {
+	// 	const navn = getKommune("oslo");
+	// 	expect(navn).toEqual("Oslo");
+	// });
+	//
+	// it("should return a valid 'bosted 'string", () => {
+	// 	let bosted = getBosted("oslo", "frogner");
+	// 	expect(bosted).toEqual("Oslo, Bydel Frogner");
+	//
+	// 	bosted = getBosted("oslo", "sondrenordstrand");
+	// 	expect(bosted).toEqual("Oslo, Bydel Søndre Nordstrand");
+	// });
 
 });
