927c116b78f237dd7f9a7f0b4398a6fa038a671c
Flemming, Thomas <thomas.flemming@gmail.com>
2018-03-07 15:18:03 +0100

    [WIP] Kvitteringsside

--- web/src/frontend/src/digisos/skjema/ettersendelse/ettersendelse.tsx
+++ web/src/frontend/src/digisos/skjema/ettersendelse/ettersendelse.tsx
@@ -7,6 +7,10 @@ import { SynligeFaktaProps } from "../../redux/synligefakta/synligeFaktaTypes";
 import { Faktum } from "../../../nav-soknad/types";
 import Icon from "nav-frontend-ikoner-assets";
 import SVG from "react-inlinesvg";
+import NavFrontendChevron from "nav-frontend-chevron";
+import { Collapse } from "react-collapse";
+import DigisosIkon from "../../../nav-soknad/components/digisosIkon/digisosIkon";
+import Knapp from "nav-frontend-knapper";
 
 interface OwnProps {
 	fakta: Faktum[];
@@ -14,114 +18,164 @@ interface OwnProps {
 
 type Props = OwnProps & SynligeFaktaProps & DispatchProps & InjectedIntlProps;
 
-/*
-TODO: Legg inn banneret som svg i stedet for png.
-<SVG
-	className="banner__forside-illustrasjon"
-	src="/soknadsosialhjelp/statisk/bilder/william.svg"
-/>
- */
-class Ettersendelse extends React.Component<Props, {}> {
-	render() {
-		return <div className="ettersendelse maincontent">
-			<div className="banner banner__forside">
-				<div className="blokk-center">
-					<div className="banner__forside-wrapper">
-						<div className="banner__tittel-tekst">
-							<h1 className="typo-sidetittel">
-								<span>Søknad om økonomisk sosialhjelp</span>
-							</h1>
-						</div>
-						<img
-							className="banner__william-illustrasjon"
-							src="/soknadsosialhjelp/statisk/bilder/william_laptop.png" />
-					</div>
-				</div>
+interface OwnState {
+	vedleggEkspandert: boolean;
+}
+
+const ManglendeVedlegg: React.StatelessComponent<{ children: React.ReactNode } & {}> = ({ children }) => {
+	return (
+		<div className="avsnitt_med_marger">
+			<div className="venstemarg"/>
+			<div className="avsnitt">
+				{children}
 			</div>
-
-			<div className="blokk-center">
-				<p className="ettersendelse ingress">
-					Du må gi beskjed hvis den økonomiske situasjonen din endrer seg etter at du har sendt søknaden.
-				</p>
-
-				{/* ---- start --- */}
-				<div className="avsnitt_med_marger">
-					<div className="venstemarg">
-						<Icon kind="stegindikator__hake" className="ettersendelse__ikon" />
-					</div>
-					<div className="avsnitt">
-						<h3>Søknaden er sendt til Horten kommune</h3>
-						<p>07.02.2018</p>
-					</div>
-
-					<div className="hoyremarg">
-						<span style={{display: "block"}}>
-							<SVG
-								className="ettersendelse__ikon"
-								src="/soknadsosialhjelp/statisk/bilder/iconUpload.svg"
-							/>
-						</span>
-					</div>
-				</div>
-				{/* ---- slutt --- */}
-
-				{/* ---- start --- */}
-				<div className="avsnitt_med_marger">
-					<div className="venstemarg">
-						<SVG
-							className="ettersendelse__ikon"
-							src="/soknadsosialhjelp/statisk/bilder/advarsel-sirkel.svg"
-						/>
-					</div>
-					<div className="avsnitt">
-						<h3>3 vedlegg mangler</h3>
-					</div>
-
-					<div className="hoyremarg">
-						<span style={{display: "block"}}>
-							<span className="chevron__nedover"/>
-						</span>
-					</div>
-				</div>
-				{/* ---- slutt --- */}
-
-				<br/>
-				<br/>
-				<div>
-					<div className="" style={{float: "left", width: "88px", display: "block"}}>
-						<SVG
-							className="ettersendelse__ikon"
-							src="/soknadsosialhjelp/statisk/bilder/snakkebobler.svg"
-						/>
-					</div>
-					<b style={{display: "block", paddingTop: "12px"}}>Du blir innkalt til en samtale</b>
-					<br/>
-					<br/>
-					<p>
-						Hvis du søker om økonomisk sosialhjelp, blir du vanligvis innkalt til en
-						samtale med en veileder. Du kan også kontakte NAV-kontoret ditt og avtale et møte.
-						Les mer om hvordan et møte foregår.
-					</p>
-					<SVG
-						className="ettersendelse__ikon"
-						src="/soknadsosialhjelp/statisk/bilder/trashcan.svg"
-					/>
-
-					<SVG
-						className="ettersendelse__ikon"
-						src="/soknadsosialhjelp/statisk/bilder/iconUpload.svg"
-					/>
-					<p>
-						Saksbehandlingstiden varierer fra kommune til kommune.
-						Når vi har behandlet søknaden din, får du et vedtak. Hvis det går mer enn én måned,
-						skal du få et foreløpig svar. Hvis vi mangler opplysninger eller du ikke har levert
-						all nødvendig dokumentasjon, kan det ta lengre tid før du får svar på søknaden din.
-					</p>
-				</div>
-
+			<div className="hoyremarg hoyremarg__ikon">
+				<DigisosIkon navn="lastOpp" className="ettersendelse__ikon"/>
 			</div>
 		</div>
-	;
+	);
+};
+
+class Ettersendelse extends React.Component<Props, OwnState> {
+	constructor(props: Props) {
+		super(props);
+		this.state = {
+			vedleggEkspandert: false
+		};
+	}
+
+	render() {
+		return (
+			<div className="ettersendelse maincontent">
+
+				<div className="banner banner__forside">
+					<div className="blokk-center">
+						<div className="banner__forside-wrapper">
+							<div className="banner__tittel-tekst">
+								<h1 className="typo-sidetittel">
+									<span>Søknad om økonomisk sosialhjelp</span>
+								</h1>
+							</div>
+							<div className="banner__illustrasjon">
+								<SVG
+									className="banner__illustrasjon__william"
+									src={"/soknadsosialhjelp/statisk/bilder/illustrasjon_william.svg"}
+								/>
+								<SVG
+									className="banner__illustrasjon__laptop"
+									src={"/soknadsosialhjelp/statisk/bilder/illustrasjon_laptop.svg"}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="blokk-center">
+					<p className="ettersendelse ingress">
+						Du må gi beskjed hvis den økonomiske situasjonen din endrer seg etter at du har sendt søknaden.
+					</p>
+
+					<div className="avsnitt_med_marger">
+						<div className="venstemarg">
+							<Icon kind="stegindikator__hake" className="ettersendelse__ikon"/>
+						</div>
+						<div className="avsnitt">
+							<h3>Søknaden er sendt til Horten kommune</h3>
+							<p>07.02.2018</p>
+						</div>
+
+						<div className="hoyremarg hoyremarg__ikon">
+							<DigisosIkon navn="printer" className="ettersendelse__ikon"/>
+						</div>
+					</div>
+
+					<div className="avsnitt_med_marger">
+						<div className="venstemarg">
+							<DigisosIkon navn="advarselSirkel" className="ettersendelse__ikon"/>
+						</div>
+						<div className="avsnitt">
+							<h3>3 vedlegg mangler</h3>
+						</div>
+						<div
+							className="hoyremarg hoyremarg__ikon"
+							onClick={() => this.setState({ vedleggEkspandert: !this.state.vedleggEkspandert })}
+						>
+							<NavFrontendChevron
+								className="ettersendelse__chevron"
+								type={this.state.vedleggEkspandert ? "opp" : "ned"}
+							/>
+						</div>
+					</div>
+
+					<Collapse isOpened={this.state.vedleggEkspandert}>
+						<ManglendeVedlegg>
+							<h3>Kontooversikt med saldo for brukskonto (siste måned)</h3>
+						</ManglendeVedlegg>
+
+						<ManglendeVedlegg>
+							<h3>Skattemelding og skatteoppgjør</h3>
+						</ManglendeVedlegg>
+
+						<ManglendeVedlegg>
+							<h3>Lønnslipp (siste måned)</h3>
+						</ManglendeVedlegg>
+
+						<ManglendeVedlegg>
+							<h3>Annen dokumentasjon</h3>
+							<p>Hvis du har andre vedlegg du ønsker å gi oss, kan de lastes opp her.</p>
+						</ManglendeVedlegg>
+
+						<div className="avsnitt_med_marger">
+							<div className="venstemarg"/>
+							<div className="avsnitt">
+								<Knapp
+									type="hoved"
+									htmlType="submit"
+								>
+									Send vedlegg
+								</Knapp>
+							</div>
+							<div className="hoyremarg"/>
+						</div>
+					</Collapse>
+
+					<div className="avsnitt_med_marger">
+						<div className="venstemarg">
+							<DigisosIkon navn="snakkebobler" className="ettersendelse__ikon"/>
+						</div>
+						<div className="avsnitt">
+							<h3>Du blir innkalt til en samtale</h3>
+							<p>
+								Hvis du søker om økonomisk sosialhjelp, blir du vanligvis innkalt til en
+								samtale med en veileder. Du kan også kontakte NAV-kontoret ditt og avtale et møte.
+								Les mer om hvordan et møte foregår.
+							</p>
+						</div>
+						<div className="hoyremarg"/>
+					</div>
+
+					<div className="avsnitt_med_marger">
+						<div className="venstemarg">
+							<SVG
+								className="ettersendelse__ikon"
+								src={"/soknadsosialhjelp/statisk/bilder/ikon_konvolutt.svg"}
+							/>
+						</div>
+						<div className="avsnitt">
+							<h3>Du får beskjed om vedtak</h3>
+							<p>
+								Saksbehandlingstiden varierer fra kommune til kommune.
+								Når vi har behandlet søknaden din, får du et vedtak. Hvis det går mer enn én måned,
+								skal du få et foreløpig svar. Hvis vi mangler opplysninger eller du ikke har levert
+								all nødvendig dokumentasjon, kan det ta lengre tid før du får svar på søknaden din.
+							</p>
+						</div>
+						<div className="hoyremarg"/>
+					</div>
+
+				</div>
+			</div>
+		);
 	}
 }
 
---
+++ web/src/frontend/src/nav-soknad/components/digisosIkon/digisosIkon.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import SVG from "react-inlinesvg";
+
+export type ikoner =
+	| "lastOpp"
+	| "printer"
+	| "konvolutt"
+	| "advarselSirkel"
+	| "snakkebobler";
+
+const DigisosIkon: React.StatelessComponent<{className?: string, navn: ikoner} & {}> = ({className, navn}) => {
+	return (
+		<SVG
+			className={className || ""}
+			src={"/soknadsosialhjelp/statisk/bilder/ikon_" + navn + ".svg"}
+		/>
+	);
+};
+
+export default DigisosIkon;
