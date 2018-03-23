02fc5b427c8582491cee380e171b652c43384ab7
Flemming, Thomas <thomas.flemming@gmail.com>
2018-03-23 16:00:45 +0100

    [WIP] Animert underskjema på sjekkbokser 'Eier du noe? > Annet'

--- web/src/frontend/src/nav-soknad/components/nivaTreSkjema/index.tsx
+++ web/src/frontend/src/nav-soknad/components/nivaTreSkjema/index.tsx
@@ -1,22 +1,27 @@
 import * as React from "react";
 import { Collapse } from "react-collapse";
 
+type Sizes = "small" | "large";
+
 interface NivaTreSkjemaProps extends React.Props<any> {
 	tittel?: string;
 	visible: boolean;
 	children: React.ReactNode;
+	size?: Sizes;
 }
 
 const NivaTreSkjema: React.StatelessComponent<NivaTreSkjemaProps> = ({
 	tittel,
 	visible,
-	children
+	children,
+	size
 }) => {
 	if (!visible) {
 		return null;
 	}
+	const className = (size && size === "small") ? " nivaTreSkjema__small" : "";
 	const renderContent = () => (
-		<div className="nivaTreSkjema">
+		<div className={"nivaTreSkjema " + className}>
 			<div className="nivaTreSkjema__boks">
 				<div className="nivaTreSkjema__innhold">{children}</div>
 			</div>
