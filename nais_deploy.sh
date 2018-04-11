7b1caa7e7ed0d13d45b6cba8ed5d886487faaa11
Stian Grenborgen <grenborgen.stian@nav.no>
2018-04-11 17:29:05 +0200

    [DIGISOS-463] Flere rettelser.

--- nais_deploy.sh
+++ nais_deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-nais_deploy_environment=$1
-
 echo "Deploying version ${releaseVersion} on ${nais_deploy_environment} with user ${domenebrukernavn}";
 deploy_result=$(curl -s -S -k --output /dev/stderr --write-out "%{http_code}" -d '{"application": "'${application}'", "version": "'${releaseVersion}'", "fasitEnvironment": "'${nais_deploy_environment}'", "zone": "sbs", "namespace": "'${nais_deploy_environment}'", "fasitUsername": "'${domenebrukernavn}'", "fasitPassword": "'${domenepassord//\"/\\\"}'", "manifesturl": "https://repo.adeo.no/repository/raw/nais/'${application}'/'${releaseVersion}'/nais.yaml"}' https://daemon.nais.oera-q.local/deploy)
 if [[ "${deploy_result}" != "200" ]]
