0e1332f1abe2fb51a7c40b7bdd5b4dfbe5d8485c
André Wolden <andre.wolden@nav.no>
2018-08-29 08:42:58 +0200

    [WIP] ser ut til at stepbystepen virker nå

---
+++ web/src/frontend/src/nav-soknad/components/infopanel/index.tsx
@@ -0,0 +1,127 @@
+import * as React from "react";
+import { Collapse } from "react-collapse";
+import Panel, {PanelAction } from "./panel";
+
+
+interface OwnProps {
+	children?: any;
+	initialState: CollapseState;
+	open: boolean;
+}
+
+interface State {
+	collapseState: CollapseState;
+	vises: boolean;
+}
+
+export enum CollapseState {
+	COLLAPSED = "collapsed",
+	CONTENT_FADED_IN = "CONTENT_FADED_IN",
+	EXPANDED = "expanded",
+	CONTENT_FADED_OUT = "CONTENT_FADED_OUT",
+}
+
+
+class InfoPanel extends React.Component<OwnProps, State> {
+
+	constructor(props: OwnProps) {
+		super(props);
+		this.state = {
+			collapseState: CollapseState.COLLAPSED,
+			vises: false
+		};
+	}
+
+	contentFadedIn(){
+		this.setState({
+			collapseState: CollapseState.CONTENT_FADED_IN
+		})
+	}
+
+	contentFadedOut(){
+		this.setState({
+			collapseState: CollapseState.CONTENT_FADED_OUT
+		})
+	}
+
+	handleOnRest(isOpened: boolean){
+
+		if (isOpened && this.state.collapseState === CollapseState.COLLAPSED) {
+			this.setState({
+				collapseState: CollapseState.EXPANDED
+			})
+		} else if(!isOpened && this.state.collapseState === CollapseState.EXPANDED) {
+			this.setState({
+				collapseState: CollapseState.COLLAPSED
+			})
+		}
+	}
+
+	render() {
+
+		const open = this.props.open;
+
+		let isOpened = false;
+		let panelAction: PanelAction = PanelAction.NO_OPERATION;
+
+		if (open){
+			switch (this.state.collapseState){
+				case CollapseState.COLLAPSED: {
+					isOpened = true;
+					panelAction = PanelAction.NO_OPERATION;
+				}
+				case CollapseState.EXPANDED: {
+					isOpened = true;
+					panelAction = PanelAction.FADE_IN;
+				}
+				case CollapseState.CONTENT_FADED_IN: {
+					isOpened = true;
+					panelAction = PanelAction.NO_OPERATION;
+				}
+				case CollapseState.CONTENT_FADED_OUT: {
+					isOpened = true;
+					panelAction = PanelAction.FADE_IN;
+				}
+			}
+		} else {
+			switch (this.state.collapseState){
+				case CollapseState.COLLAPSED: {
+					isOpened = false;
+					panelAction = PanelAction.NO_OPERATION;
+				}
+				case CollapseState.EXPANDED: {
+					isOpened = false;
+					panelAction = PanelAction.NO_OPERATION;
+				}
+				case CollapseState.CONTENT_FADED_IN: {
+					isOpened = true;
+					panelAction = PanelAction.FADE_OUT;
+				}
+				case CollapseState.CONTENT_FADED_OUT: {
+					isOpened = false;
+					panelAction = PanelAction.NO_OPERATION;
+				}
+			}
+		}
+
+		return (
+				<Collapse
+					isOpened={isOpened}
+					className="infopanel"
+					onRest={() => this.handleOnRest(isOpened)}
+				>
+					<Panel
+						onFinishedFadingIn={() => this.contentFadedIn()}
+						onFinishedFadingOut={() => this.contentFadedOut()}
+						panelAction={panelAction}
+					>
+						{this.props.children}
+					</Panel>
+
+				</Collapse>
+		);
+
+	}
+}
+
+export default InfoPanel;
---
+++ web/src/frontend/src/nav-soknad/components/infopanel/panel.tsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+
+interface OwnState {
+	actionReceivedButNotCompleted: boolean;
+	currentAction: PanelAction;
+}
+
+interface OwnProps {
+	children?: React.ReactNode;
+	panelAction?: PanelAction;
+	onFinishedFadingIn?: () => void;
+	onFinishedFadingOut?: () => void;
+}
+
+export enum PanelAction {
+	FADE_IN = "fade_in",
+	FADE_OUT = "fade_out",
+	ALWAYS_SHOW = "always_show",
+	NO_OPERATION = "no_operation"
+}
+
+export enum PanelFadeClassName {
+	FADE_IN = "infopanel_fade_in",
+	FADE_OUT = "infopanel_fade_out"
+}
+
+class Panel extends React.Component<OwnProps, OwnState> {
+
+	constructor(props: OwnProps){
+		super(props);
+		this.state = {
+			actionReceivedButNotCompleted: false,
+			currentAction: PanelAction.NO_OPERATION
+		}
+	}
+
+	componentDidUpdate(){
+		if (this.props.panelAction === PanelAction.FADE_IN && this.state.actionReceivedButNotCompleted){
+			this.props.onFinishedFadingIn();
+			this.setState({
+				actionReceivedButNotCompleted: false
+			})
+
+		}
+
+		if (this.props.panelAction === PanelAction.FADE_OUT && this.state.actionReceivedButNotCompleted){
+			this.props.onFinishedFadingOut();
+		}
+	}
+
+	componentWillReceiveProps(){
+		if (this.props.panelAction !== this.state.currentAction){
+			this.setState({
+				actionReceivedButNotCompleted: true,
+				currentAction: this.props.panelAction
+			})
+		}
+	}
+
+	render() {
+
+		const className = this.props.panelAction === PanelAction.FADE_IN
+			? PanelFadeClassName.FADE_IN
+			: PanelFadeClassName.FADE_OUT;
+
+		return (
+			<div className={ className }>
+				<div>
+					{ "panel action: " + className }
+				</div>
+				<div>
+					{ this.props.children }
+				</div>
+			</div>
+			)
+	}
+
+}
+
+export default Panel;
