45a1c582f58987ad09edd7e85bab1274c6277f84
Flemming, Thomas <thomas.flemming@gmail.com>
2018-09-17 11:34:08 +0200

    Konvertere SVG til React komponent for å unngå at ikon_searchAddress.svg lastes ned for hvert adressesøk

--- web/src/frontend/src/nav-soknad/components/digisosIkon/digisosIkon.tsx
+++ web/src/frontend/src/nav-soknad/components/digisosIkon/digisosIkon.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import SVG from "react-inlinesvg";
+import SearchAddress from "./SearchAddress";
 
 export type ikoner =
 	| "lastOpp"
@@ -20,12 +21,16 @@ interface Props {
 }
 
 const DigisosIkon: React.StatelessComponent<Props & {}> = ({className, navn, style}) => {
-	return (
-		<SVG style={style}
-			className={className || ""}
-			src={"/soknadsosialhjelp/statisk/bilder/ikon_" + navn + ".svg"}
-		/>
-	);
+	if (navn === "searchAddresse") {
+		return <SearchAddress/>;
+	} else {
+		return (
+			<SVG style={style}
+			     className={className || ""}
+			     src={"/soknadsosialhjelp/statisk/bilder/ikon_" + navn + ".svg"}
+			/>
+		);
+	}
 };
 
 export default DigisosIkon;
