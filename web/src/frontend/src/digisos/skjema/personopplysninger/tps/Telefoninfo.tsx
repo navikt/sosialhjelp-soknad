ec44f3ee85b86a70e659b25cb332fa7a0bc324cc
André Wolden <andre.wolden@nav.no>
2018-10-23 08:24:41 +0200

    Fjerner en overskrift for mye fra telefoninfo når bruker har systemregistrert telefonnummer men ønsker å bruke et annet

--- web/src/frontend/src/digisos/skjema/personopplysninger/tps/Telefoninfo.tsx
+++ web/src/frontend/src/digisos/skjema/personopplysninger/tps/Telefoninfo.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { injectIntl, InjectedIntlProps, FormattedMessage } from "react-intl";
+import {injectIntl, InjectedIntlProps, FormattedMessage} from "react-intl";
 import SysteminfoFaktum from "../../../../nav-soknad/faktum/SysteminfoFaktum";
-import { harFaktumVerdi, getFaktumVerdi } from "../../../../nav-soknad/utils";
-import { Faktum } from "../../../../nav-soknad/types";
+import {harFaktumVerdi, getFaktumVerdi} from "../../../../nav-soknad/utils";
+import {Faktum} from "../../../../nav-soknad/types";
 import SporsmalFaktum from "../../../../nav-soknad/faktum/SporsmalFaktum";
 import TelefonFaktum from "../../../../nav-soknad/faktum/typedInput/TelefonFaktum";
 import Detaljeliste, {
@@ -12,40 +12,31 @@ import Detaljeliste, {
 interface Props {
 	fakta: Faktum[];
 }
-{/*<SporsmalFaktum faktumKey="kontakt.telefon" className="luftUnderFemRem legendTittelStorBold">*/}
-{/*</SporsmalFaktum>*/}
-
-
-// const Skjema: React.StatelessComponent<{}> = () => (
-// 		<TelefonFaktum faktumKey="kontakt.telefon" id="kontakt_telefon" maxLength={8} className="normalTekst"/>
-// );
-
-const Skjema: React.StatelessComponent<{}> = () => (
-	<SporsmalFaktum faktumKey="kontakt.telefon" className="luftUnderFemRem legendTittelStorBold">
-		<TelefonFaktum faktumKey="kontakt.telefon" id="kontakt_telefon" maxLength={8} />
-	</SporsmalFaktum>
-);
 
 const Telefoninfo: React.StatelessComponent<Props & InjectedIntlProps> = ({
-	fakta,
-	intl
-}) => {
+	                                                                          fakta,
+	                                                                          intl
+                                                                          }) => {
 	if (!harFaktumVerdi(fakta, "kontakt.system.telefon")) {
-		return <Skjema />;
+		return (
+			<SporsmalFaktum faktumKey="kontakt.telefon" className="luftUnderFemRem legendTittelStorBold">
+				<TelefonFaktum faktumKey="kontakt.telefon" id="kontakt_telefon" maxLength={8}/>
+			</SporsmalFaktum>);
 	}
 
 	return (
-		<SporsmalFaktum faktumKey="kontakt.system.telefoninfo" style="system" className="luftUnderFemRem boldOverskrift">
+		<SporsmalFaktum faktumKey="kontakt.system.telefoninfo" style="system"
+		                className="luftUnderFemRem boldOverskrift">
 			<SysteminfoFaktum
 				faktumKey="kontakt.telefon.brukerendrettoggle"
-				skjema={<Skjema />}
+				skjema={<TelefonFaktum faktumKey="kontakt.telefon" id="kontakt_telefon" maxLength={8}/>}
 				endreLabel={intl.formatMessage({
 					id: "kontakt.system.telefon.endreknapp.label"
 				})}
 			>
 				<Detaljeliste>
 					<DetaljelisteElement
-						tittel={<FormattedMessage id="kontakt.system.telefon.label" />}
+						tittel={<FormattedMessage id="kontakt.system.telefon.label"/>}
 						verdi={getFaktumVerdi(fakta, "kontakt.system.telefon")}
 					/>
 				</Detaljeliste>
