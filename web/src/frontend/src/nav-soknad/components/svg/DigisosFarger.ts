1bc40d93d86b432dafa7c47d633c709aed7d54fc
Flemming, Thomas <thomas.flemming@gmail.com>
2018-11-29 12:54:01 +0100

    Justere farger. Endre navn på css klasser.

--- web/src/frontend/src/nav-soknad/components/svg/DigisosFarger.ts
+++ web/src/frontend/src/nav-soknad/components/svg/DigisosFarger.ts
@@ -1,9 +1,4 @@
 export enum DigisosFarge {
-	// NAV_GRONN_LIGHTEN_40 = 'navGronnLighten40',
-	// NAV_GRONN_LIGHTEN_60 = 'navGronnLighten60',
-	// NAV_ORANSJE_LIGHTEN_40 = 'navOransjeLighten40',
-	//  = 'redError',
-
 	SUKSESS = 'suksess-farge',
 	VIKTIG = 'viktig-farge',
 	FEIL = 'feil-farge'
