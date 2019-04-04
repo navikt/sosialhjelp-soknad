6db13a8d076901d97cce5018e9ac30b3c9da1c0a
Flemming, Thomas <thomas.flemming@gmail.com>
2019-04-04 12:45:45 +0200

    Lagt til testeer, bem navnekonvensjon og implementert vilkårlig antall linjer

--- web/src/frontend/src/nav-soknad/components/animasjoner/placeholder/TextPlaceholder.tsx
+++ web/src/frontend/src/nav-soknad/components/animasjoner/placeholder/TextPlaceholder.tsx
@@ -4,11 +4,30 @@ interface Props {
 	lines?: number
 }
 
-const TextPlaceholder: React.FunctionComponent<Props> = ({ lines }) => (
+const bars = (lines: number): React.ReactNode[] => {
+	let barNumber = 0;
+	const divs: React.ReactNode[] = [];
+	for (let index = 0;index<lines;index++) {
+		if (barNumber === 0) {
+			divs.push(<div className="bar" key={index}/>);
+		}
+		if (barNumber === 1) {
+			divs.push(<div className="bar bar__short_delay" key={index}/>);
+		}
+		if (barNumber === 2) {
+			divs.push(<div className="bar bar__long_delay bar__narrow" key={index}/>);
+		}
+		barNumber++;
+		if (barNumber === 3) {
+			barNumber = 0;
+		}
+	}
+	return divs;
+};
+
+const TextPlaceholder: React.FunctionComponent<Props> = ({ lines = 3 }) => (
 	<div className="loaderBar">
-		<div className="bar"/>
-		<div className="bar bar1"/>
-		<div className="bar bar2"/>
+		{bars(lines)}
 	</div>
 );
 
