dcee18cafc03462b7a4e3c1e477cb86c6affc565
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2019-04-11 10:10:50 +0200

    Endre modell for skattbar inntekt

--- web/src/frontend/src/digisos/skjema/inntektFormue/SkattbarInntekt.tsx
+++ web/src/frontend/src/digisos/skjema/inntektFormue/SkattbarInntekt.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import {InjectedIntlProps, injectIntl} from "react-intl";
+import {FormattedMessage, FormattedNumber, InjectedIntlProps, injectIntl} from "react-intl";
 import {
 	connectSoknadsdataContainer,
 	SoknadsdataContainerProps
 } from "../../../nav-soknad/redux/soknadsdata/soknadsdataContainerUtils";
 import {SoknadsSti} from "../../../nav-soknad/redux/soknadsdata/soknadsdataReducer";
 import Ekspanderbartpanel from "nav-frontend-ekspanderbartpanel";
+import {getIntlTextOrKey} from "../../../nav-soknad/utils";
 
 type Props = SoknadsdataContainerProps & InjectedIntlProps;
 
@@ -16,20 +17,40 @@ class SkattbarInntekt extends React.Component<Props, {}> {
 	}
 
 	render() {
-		const {soknadsdata} = this.props;
+		const {intl, soknadsdata} = this.props;
 		const skattbarInntekt = soknadsdata.inntekt.skattbarinntektogforskuddstrekk;
-		const lonn = skattbarInntekt.lonn.map(value => {
-			return (<div key="FIXME" className="utbetaling blokk-s">
-				<div> Fra {value.fom} til {value.tom}</div>
-				<div>Lønn<span className="verdi detaljeliste__verdi">{value.trekkpliktig}</span></div>
-				<div>Arbeidsgiver<span className="verdi detaljeliste__verdi">{value.organisasjon}</span></div>
+		const organisasjoner = skattbarInntekt.organisasjoner.map(organisasjon => {
+			const utbetalinger = organisasjon.utbetalinger.map( utbetaling => {
+				return (<div key="FIXME" className="utbetaling blokk-s">
+					<span><FormattedMessage id={utbetaling.tittel}/>:</span>
+					<span className="verdi detaljeliste__verdi"><FormattedNumber value={utbetaling.belop}/> kr</span>
+				</div>)
+			});
+			return (<div key={organisasjon.orgnr} className="utbetaling blokk-s">
+				<h4>{organisasjon.organisasjonsnavn}</h4>
+				<div> Fra {organisasjon.fom} til {organisasjon.tom}</div>
+				{utbetalinger}
 			</div>)
 		});
+		const title = getIntlTextOrKey(intl, "utbetalinger.inntekt.skattbar.undertittel");
 
-		return (<Ekspanderbartpanel apen={true} tittel={"Utbetalt skattbar inntekt siste måned"}>
-			<div>Lønn</div>
-			{lonn}
-		</Ekspanderbartpanel>);
+		return (<Ekspanderbartpanel apen={true} tittel={title}>
+			<FormattedMessage id="utbetalinger.inntekt.skattbar.oppsummering"/>
+
+			<div className="utbetalinger">
+				<div className="utbetaling blokk-s">
+					<span><FormattedMessage id="utbetalinger.inntekt.skattbar.samlet.inntekt"/>:</span>
+					<span className="verdi detaljeliste__verdi"><FormattedNumber value={skattbarInntekt.samletInntekt}/> kr</span>
+				</div>
+				<div className="utbetaling blokk-s">
+					<span><FormattedMessage id="utbetalinger.inntekt.skattbar.samlet.trekk"/>:</span>
+					<span className="verdi detaljeliste__verdi"><FormattedNumber value={skattbarInntekt.samletTrekk}/> kr</span>
+				</div>
+			</div>
+
+			<div className="utbetalinger">{organisasjoner}</div>
+
+			</Ekspanderbartpanel>);
 	}
 }
 
