19ff7744b185eb33425fa7e3a8cfc59ae4fd5a06
Andre Wolden <andre.wolden@nav.no>
2019-05-09 10:07:16 +0200

    fjerner alle steder bortsett fra side 8 og ettersendelse som ennå ikke er merget til master

--- web/src/frontend/src/digisos/skjema/personopplysninger/index.tsx
+++ web/src/frontend/src/digisos/skjema/personopplysninger/index.tsx
@@ -1,19 +1,9 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { State } from "../../redux/reducers";
-import { DispatchProps, Dispatch } from "../../../nav-soknad/redux/reduxTypes";
+import { DispatchProps } from "../../../nav-soknad/redux/reduxTypes";
 import { FaktumComponentProps } from "../../../nav-soknad/redux/fakta/faktaTypes";
-import {
-	harFaktumVerdi,
-	finnFaktum,
-	getPropertyVerdi,
-	getFaktumVerdi
-} from "../../../nav-soknad/utils";
-import { lagreFaktum } from "../../../nav-soknad/redux/fakta/faktaActions";
 import DigisosSkjemaSteg, { DigisosSteg } from "../DigisosSkjemaSteg";
-import {
-	Faktum
-} from "../../../nav-soknad/types";
 import William from "../../../nav-soknad/components/svg/illustrasjoner/William";
 import { InformasjonspanelIkon } from "../../../nav-soknad/components/informasjonspanel";
 import { DigisosFarge } from "../../../nav-soknad/components/svg/DigisosFarger";
@@ -32,37 +22,6 @@ interface OwnProps {
 export type Props = OwnProps & FaktumComponentProps & DispatchProps;
 
 class Personopplysninger extends React.Component<Props, OwnProps> {
-
-	componentDidMount() {
-		if (!harFaktumVerdi(this.props.fakta, "kontakt.system.telefon")) {
-			this.oppdaterFaktumVerdi(this.props.fakta, "kontakt.telefon.brukerendrettoggle", "true", this.props.dispatch);
-		}
-		if (!harFaktumVerdi(this.props.fakta, "kontakt.system.kontonummer")) {
-			this.oppdaterFaktumVerdi(this.props.fakta, "kontakt.kontonummer.brukerendrettoggle", "true", this.props.dispatch);
-		}
-		if (getPropertyVerdi(this.props.fakta, "kontakt.system.adresse", "type") == null) {
-			this.oppdaterFaktumVerdi(this.props.fakta, "kontakt.adresse.brukerendrettoggle", "true", this.props.dispatch);
-		}
-
-		this.oppdaterHarIkkeKontonummer(
-			this.props.fakta,
-			getFaktumVerdi(this.props.fakta, "kontakt.kontonummer.harikke"),
-			this.props.dispatch
-		);
-	}
-
-	oppdaterHarIkkeKontonummer(fakta: Faktum[], verdi: string, dispatch: Dispatch) {
-		if (verdi === "true" ) {
-			if (getPropertyVerdi(fakta, "kontakt.kontonummer.brukerendrettoggle", "harikke") !== "true") {
-				this.oppdaterFaktumPropertyVerdi(fakta, "kontakt.kontonummer.brukerendrettoggle", "harikke", "true", dispatch);
-			}
-		} else {
-			if (getPropertyVerdi(fakta, "kontakt.kontonummer.brukerendrettoggle", "harikke") !== "false") {
-				this.oppdaterFaktumPropertyVerdi(fakta, "kontakt.kontonummer.brukerendrettoggle", "harikke", "false", dispatch);
-			}
-		}
-	}
-
 	render() {
 		const gjennopptattSoknadInfoPanel = (
 			<div className="skjema-sporsmal">
@@ -85,27 +44,6 @@ class Personopplysninger extends React.Component<Props, OwnProps> {
 				<Telefon />
 				<Bankinformasjon />
 			</DigisosSkjemaSteg>
-		);
-	}
-
-	private oppdaterFaktumVerdi(fakta: Faktum[], faktumKey: string, verdi: string, dispatch: Dispatch) {
-		const brukerEndret = finnFaktum(faktumKey, fakta);
-		brukerEndret.value = verdi;
-		dispatch(
-			lagreFaktum(brukerEndret)
-		);
-	}
-
-	private oppdaterFaktumPropertyVerdi(
-			fakta: Faktum[],
-			faktumKey: string,
-			propertyNavn: string,
-			verdi: string,
-			dispatch: Dispatch) {
-		const brukerEndret = finnFaktum(faktumKey, fakta);
-		brukerEndret.properties[propertyNavn] = verdi;
-		dispatch(
-			lagreFaktum(brukerEndret)
 		);
 	}
 }
