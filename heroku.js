d2080a1a55174b4b409df41113eaa2b3f4324d38
Eivind K. Nilsbakken <eivind.nilsbakken@bouvet.no>
2019-05-13 15:00:27 +0200

    Deploy to Heroku with express.js

---
+++ heroku.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+const port = process.env.PORT
+
+app.get("/soknadsosialhjelp/static/*", (req, res) => {
+  console.log(`static ${req.path}`)
+  res.sendFile(path.resolve(__dirname, "static", req.params[0]))
+})
+
+app.get("/soknadsosialhjelp/*", (req, res) => {
+  console.log(`route  ${req.path}`)
+  res.sendFile(path.resolve(__dirname, "index.html"))
+})
+
+app.listen(port, (req, res) => {
+  console.log(`listening on port ${port}`)
+})
