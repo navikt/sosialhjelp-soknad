b35e759a342db79a9424a6f883ac90d8fee7f1e9
Andre Wolden <andre.wolden@nav.no>
2019-05-14 12:14:51 +0200

    retter bug

--- web/src/frontend/src/nav-soknad/utils/enhancedComponentUtils.ts
+++ web/src/frontend/src/nav-soknad/utils/enhancedComponentUtils.ts
@@ -5,7 +5,7 @@ import { Valideringsfeil } from "../validering/types";
 import { Feil } from "nav-frontend-skjema";
 
 export function getFeil(feil: Valideringsfeil[], intl: InjectedIntl, faktumKey: string, faktumIndex:number): Feil {
-    if (faktumIndex !== null){
+    if (faktumIndex != null){
         const feilkode = feil.find((f: Valideringsfeil) => f.faktumKey === faktumKey + "." + faktumIndex.toString());
         return !feilkode ? null : { feilmelding: intl.formatHTMLMessage({ id: feilkode.feilkode }) };
     } else {
