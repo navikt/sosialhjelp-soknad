75ab3f059614316825d6764ca0e923a8a77c8fe7
Flemming, Thomas <thomas.flemming@gmail.com>
2019-05-21 13:56:54 +0200

    Få feilende tester på telefon til å passere

--- web/src/frontend/src/digisos/skjema/personopplysninger/telefon/Telefon.test.tsx
+++ web/src/frontend/src/digisos/skjema/personopplysninger/telefon/Telefon.test.tsx
@@ -1,7 +1,7 @@
-import { configEnzyme,  harInputfelt, TestContext } from "../../../../nav-soknad/utils/unitTestUtils";
+import { configEnzyme, createMockIntl, harInputfelt, TestContext } from "../../../../nav-soknad/utils/unitTestUtils";
 import { mount } from "enzyme";
 import * as React from "react";
-import TelefonView from "./Telefon";
+import {TelefonView} from "./Telefon";
 import { initialSoknadsdataState } from "../../../../nav-soknad/redux/soknadsdata/soknadsdataReducer";
 
 describe("Telefon react komponent", () => {
@@ -11,30 +11,12 @@ describe("Telefon react komponent", () => {
 	const intlMessages: any = {
 		"kontakt.system.telefon.endreknapp.label": "Endre",
 		"systeminfo.avbrytendringknapp.label": "Avbryt",
-		"kontakt.system.telefon.label": "Telefon"
+		"kontakt.system.telefon.label": "Telefon",
+		"kontakt.telefon.hjelpetekst.tittel": "hjelpetekst 1",
+		"kontakt.system.telefoninfo.hjelpetekst.tittel": "hjelpetekst 2"
 	};
 
-	it("ikke vise inputfelt", () => {
-		const soknadsdata = initialSoknadsdataState;
-		soknadsdata.personalia.telefonnummer = {
-			brukerdefinert: false,
-			systemverdi: "12345678",
-			brukerutfyltVerdi: null
-		};
-		const wrapper = mount(
-			<TestContext messages={intlMessages}>
-				<TelefonView
-					brukerBehandlingId="110000001"
-					soknadsdata={soknadsdata}
-					setValideringsfeil={() => null}
-					hentSoknadsdata={() => null}
-				/>
-			</TestContext>
-		);
-
-		expect(harInputfelt(wrapper, "tel")).toBeFalsy();
-		wrapper.unmount();
-	});
+	const intl = createMockIntl(intlMessages);
 
 	it("vise inputfelt", () => {
 		const soknadsdata = initialSoknadsdataState;
@@ -50,11 +32,35 @@ describe("Telefon react komponent", () => {
 					soknadsdata={soknadsdata}
 					setValideringsfeil={() => null}
 					hentSoknadsdata={() => null}
+					intl={intl}
 				/>
 			</TestContext>
 		);
 
 		expect(harInputfelt(wrapper, "tel")).toBeTruthy();
+		wrapper.unmount();
+	});
+
+	it("ikke vise inputfelt", () => {
+		const soknadsdata = initialSoknadsdataState;
+		soknadsdata.personalia.telefonnummer = {
+			brukerdefinert: false,
+			systemverdi: "12345678",
+			brukerutfyltVerdi: null
+		};
+		const wrapper = mount(
+			<TestContext messages={intlMessages}>
+				<TelefonView
+					brukerBehandlingId="110000001"
+					soknadsdata={soknadsdata}
+					setValideringsfeil={() => null}
+					hentSoknadsdata={() => null}
+					intl={intl}
+				/>
+			</TestContext>
+		);
+
+		expect(harInputfelt(wrapper, "tel")).toBeFalsy();
 		wrapper.unmount();
 	});
 
