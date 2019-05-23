0f7dcdec812d84c15e44459504cc49e7e3639731
Andre Wolden <wolden.andre@gmail.com>
2019-05-23 15:17:29 +0200

    Router og Redux

--- web/src/frontend_2019/src/index.tsx
+++ web/src/frontend_2019/src/index.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from "react";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import * as ReactDOM from "react-dom";
+import { ConnectedRouter } from 'connected-react-router';
+import configureStore, { history } from "./configureStore";
+import {Route, Switch} from "react-router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+window.onerror = (errorMessage, url, line, column, error) => {
+    console.warn("TODO: Implementer clientlogger");
+    // store.dispatch(loggException(errorMessage.toString(), url, line, column, error));
+};
+
+const Digisos: React.FC = () => {
+    return (
+        <Provider store={store}>
+            <div>
+                <ConnectedRouter history={history}>
+                    <div><h1>Digisos 3.0</h1></div>
+                    <Switch>
+                        <Route exact path="/informasjon" render={() => (<div><h2>Informasjon</h2></div>)} />
+                        <Route exact path="/" render={() => (<div>Match</div>)} />
+                        <Route render={() => (<div>Miss</div>)} />
+                    </Switch>
+                </ConnectedRouter>
+            </div>
+        </Provider>
+    )
+};
+
+ReactDOM.render(<Digisos/>, document.getElementById('root'));
