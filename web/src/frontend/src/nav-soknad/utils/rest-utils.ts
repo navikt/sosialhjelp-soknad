d2d15d75710352d6b135b4a183cf09fecd345a8a
Andre Wolden <wolden.andre@gmail.com>
2019-06-09 21:18:51 +0200

    setter på nedlasting av zipfil ifDev

--- web/src/frontend/src/nav-soknad/utils/rest-utils.ts
+++ web/src/frontend/src/nav-soknad/utils/rest-utils.ts
@@ -255,7 +255,6 @@ export function detekterInternFeilKode(feilKode: string): string {
 export function lastNedForsendelseSomZipFilHvisMockMiljoEllerDev(brukerbehandlingId: string) {
     if (erMockMiljoEllerDev()) {
         const url = getApiBaseUrl() + "internal/mock/tjeneste/downloadzip/" + brukerbehandlingId;
-        // window.open(url);
-        console.warn(url);
+        window.open(url);
     }
 }
