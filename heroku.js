bd8d772d23b32e22add0fdc669ff03a094f27ad1
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2019-06-12 11:52:36 +0200

    Fiks resolving av bygde, statiske ressurser i mock-miljø

--- heroku.js
+++ heroku.js
@@ -3,13 +3,15 @@ var app = express()
 var path = require('path')
 const port = process.env.PORT
 
-app.get("/soknadsosialhjelp/static/*", (req, res) => {
+app.get("*/static/*", (req, res) => {
+  //let path = req.params[0].replace("/soknadsosialhjelp/", "");
   console.log(`static ${req.path}`)
-  res.sendFile(path.resolve(__dirname, "static", req.params[0]))
+  //console.dir(req)
+  res.sendFile(path.resolve(__dirname, "static", req.params[1]))
 })
 
 app.get("/soknadsosialhjelp/statisk/*", (req, res) => {
-  console.log(`static ${req.path}`)
+  console.log(`statisk ${req.path}`)
   res.sendFile(path.resolve(__dirname, "soknadsosialhjelp/statisk", req.params[0]))
 })
 
