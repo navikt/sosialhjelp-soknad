38c20861ba1fc915fb226aa48b97d988ed3a5053
Andre Wolden <wolden.andre@gmail.com>
2019-06-27 22:28:01 +0200

    drar inn public folder og scripts folder og gitignore

---
+++ web/src/frontend/scripts/utils.js
@@ -0,0 +1,134 @@
+const fs = require("fs");
+const path = require("path");
+
+var exports = (module.exports = {});
+
+exports.allowCrossDomain = function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type,X-XSRF-TOKEN");
+	next();
+};
+
+exports.lesSpraakfil = function() {
+	var directories = [
+		"C:",
+		"Temp",
+		"sendsoknad",
+		"utave",
+		"soknadsosialhjelp",
+		"tekster",
+		"soknadsosialhjelp_nb_NO.properties"
+	];
+	var fileName = directories.join(path.sep);
+	if (!fs.existsSync(fileName)) {
+		console.log("Advarsel! Finner ikke språkfil i " + fileName);
+		console.log("Bruker backup språkfil fra ./scripts/mock_data/ i stedet");
+		directories = [
+			".",
+			"scripts",
+			"mock_data",
+			"soknadsosialhjelp_nb_NO.properties"
+		];
+		fileName = directories.join(path.sep);
+		fileContent = fs.readFileSync(fileName, "utf8");
+	} else {
+		try {
+			var fileContent = fs.readFileSync(fileName, "utf8");
+		} catch (err) {}
+	}
+
+	var array = fileContent.split("\n");
+	var output = {};
+	for (i in array) {
+		var line = array[i];
+		var splitChar = line.indexOf("=");
+		var key = line.substring(0, splitChar);
+		var val = line.substring(splitChar + 1, line.length);
+		if (val) {
+			output[key] = val.replace("\r", "");
+		}
+	}
+	return output;
+};
+
+exports.getFilePath = function(filnavn) {
+	var directories = [".", "scripts", "mock_data", filnavn];
+	return directories.join(path.sep);
+};
+
+exports.lesMockDataFil = function(filnavn) {
+	const fileName = this.getFilePath(filnavn);
+	if (!fs.existsSync(fileName)) {
+		console.log("Feil! Finner ikke mockdata fil " + fileName);
+	} else {
+		try {
+			return JSON.parse(fs.readFileSync(fileName, "utf8"));
+		} catch (err) {}
+	}
+};
+
+exports.lesMockHtmlFil = function(filnavn) {
+	const fileName = this.getFilePath(filnavn);
+	console.log("Leser html mockdatafil");
+	if (!fs.existsSync(fileName)) {
+		console.log("Advarsel! Finner ikke mockdata htmlfil " + fileName);
+	} else {
+		try {
+			return fs.readFileSync(fileName, "utf8");
+		} catch (err) {}
+	}
+};
+
+exports.delayAllResponses = function(millis) {
+	return function(req, res, next) {
+		setTimeout(next, millis);
+	};
+};
+
+exports.finnFaktaIndex = function(faktaId, fakta) {
+	for (var i = 0; i < fakta.length; i++) {
+		if (
+			faktaId.toString() === (fakta[i].faktumId && fakta[i].faktumId.toString())
+		) {
+			return i;
+		}
+	}
+};
+
+exports.hentFaktum = function(faktaId, fakta) {
+	return fakta[this.finnFaktaIndex(faktaId, fakta)];
+};
+
+exports.updateSoknadFakta = function(fakta) {
+	const lagretSoknad = this.lesMockDataFil("soknad.json");
+	const soknad = JSON.parse(JSON.stringify(lagretSoknad));
+	soknad.fakta = fakta;
+	const fileName = this.getFilePath("soknad.json");
+	fs.writeFileSync(fileName, JSON.stringify(soknad, null, 4));
+};
+
+exports.checkMandatoryQueryParam = function(req, res, paramName) {
+	var parameterVerdi = req.query[paramName];
+	if (typeof parameterVerdi === "undefined") {
+		var feilmelding = "Mangler query parameter: " + paramName;
+		var responskode = 406;
+		console.log("Returnerer feilmelding: " + responskode + ": " + feilmelding);
+		res.status(responskode);
+		res.json({ feil: feilmelding });
+	}
+};
+
+exports.emptyDirectory = function(directory) {
+	if (fs.existsSync(directory)) {
+		fs.readdir(directory, function(err, files) {
+			if (err) throw err;
+
+			files.forEach(function(file) {
+				fs.unlink(path.join(directory, file), function(err) {
+					if (err) throw err;
+				});
+			});
+		});
+	}
+};
---
+++ web/src/frontend/scripts/vedlegg_ressurser.js
@@ -0,0 +1,155 @@
+var express = require("express");
+const path = require("path");
+var router = express.Router();
+const utils = require("./utils.js");
+const fs = require("fs");
+var mime = require("mime");
+
+/*
+ * Mock backend: REST ressurser for vedlegg
+ *
+ */
+
+var DATA_DIR = "./scripts/mock_data";
+var VEDLEGG_ID = "9146";
+utils.emptyDirectory(DATA_DIR + "/vedlegg_" + VEDLEGG_ID + "/");
+
+// Nytt API kall
+router.get("/sosialhjelpvedlegg/oppdaterVedlegg/:behandlingsId", function(
+	req,
+	res
+) {
+	// Returner fullt søknadsobjekt
+	res.json(utils.lesMockDataFil("soknadVedlegg.json"));
+});
+
+// Nytt API kall
+router.get("/soknader/:brukerBehandlingId/vedlegg", function(
+	req,
+	res
+) {
+	var brukerBehandlingId = req.params.brukerBehandlingId;
+	console.log("Mock backend: GET /soknader/" + brukerBehandlingId + "/vedlegg\"");
+	res.json(utils.lesMockDataFil("soknadVedlegg.json"));
+});
+
+// Gammelt API kall
+router.get("/soknader/:brukerBehandlingId/vedlegg", function(
+	req,
+	res
+) {
+	console.log("Mock backend: GET vedlegg (vedleggsforventning)");
+	var forventninger = utils.lesMockDataFil("vedleggsforventning.json");
+	for (var i = 0; i < forventninger.length; i++) {
+		var vedleggId = forventninger[i].vedleggId;
+		var files = [];
+		fs.readdirSync(DATA_DIR + "/vedlegg_" + vedleggId).forEach( function(file) {
+			files.push({navn: file});
+		});
+		forventninger[i].filer = files;
+	}
+	res.json(forventninger);
+});
+
+router.get("/vedlegg/:vedleggId", function(
+	req,
+	res
+) {
+	console.log("Mock backend: GET vedlegg (vedleggsinfo)");
+	utils.checkMandatoryQueryParam(req, res, "behandlingsId");
+	if(res.statusCode !== 200) {return;}
+	var vedleggId = Number(req.params.vedleggId);
+	var response = utils.lesMockDataFil("vedleggsinfo_" + vedleggId + ".json");
+	if (typeof response === 'undefined') {
+		res.status(406);
+		res.json({ feil: "Ukjent vedleggs id: " + vedleggId });
+	} else {
+		res.json(response);
+	}
+});
+
+router.get("/vedlegg/:vedleggId/fil", function(
+	req,
+	res
+) {
+	console.log("Mock backend: GET vedlegg (fil liste)");
+	utils.checkMandatoryQueryParam(req, res, "behandlingsId");
+	if(res.statusCode !== 200) {return;}
+	var vedleggId = Number(req.params.vedleggId);
+	var files = [];
+	fs.readdirSync(DATA_DIR + "/vedlegg_" + vedleggId).forEach( function(file) {
+		files.push({navn: file});
+	});
+	res.json(files);
+});
+
+// router.post("/vedlegg/:vedleggId/fil", function(
+router.post("/sosialhjelpvedlegg/originalfil/:faktumId", function(
+	req,
+	res
+) {
+	console.log("Mock backend: POST vedlegg (fil liste)");
+	// utils.checkMandatoryQueryParam(req, res, "behandlingsId");
+	// if(res.statusCode !== 200) {return;}
+	// var vedleggId = Number(req.params.vedleggId);
+	var faktumId = req.params.faktumId;
+	var files = [];
+
+	Object.keys(req.files).forEach(function(file) {
+		var filename = req.files[file].name;
+		files.push({navn: filename});
+		var outputFilename = DATA_DIR + "/vedlegg_" + faktumId + "/" + filename;
+		req.files[file].mv(outputFilename, function (err) {
+			if (err) {
+				console.log("Warning: " + err);
+			} else {
+				console.log("  Lagret: 'mock_data/vedlegg_" + faktumId + "/" + filename);
+			}
+		});
+	});
+	// res.json(files);
+	const responseDelayInSeconds = 4;
+	setTimeout((function() {
+		res.json(files);
+	}), responseDelayInSeconds * 1000);
+});
+
+router.get("/vedlegg/:vedleggId/:filnavn", function(
+	req,
+    res
+) {
+	console.log("Mock backend: GET vedlegg - Last ned vedlegg.");
+	var vedleggId = Number(req.params.vedleggId);
+	var filnavn = req.params.filnavn;
+	var filsti = utils.getFilePath("/vedlegg_" + vedleggId + "/" + filnavn);
+	var mimeType = mime.getType(filsti);
+	var file = fs.createReadStream(filsti);
+	var stat = fs.statSync(filsti);
+	res.setHeader('Content-Length', stat.size);
+	res.setHeader('Content-Type', mimeType); // 'application/pdf');
+	res.setHeader('Content-Disposition', 'attachment; filename=' + filnavn);
+	file.pipe(res);
+});
+
+router.delete("/vedlegg/:vedleggId/:filnavn", function(
+	req,
+	res
+) {
+	console.log("Mock backend: DELETE vedlegg (enkelt fil)");
+	utils.checkMandatoryQueryParam(req, res, "behandlingsId");
+	if(res.statusCode !== 200) {return;}
+	var vedleggId = req.params.vedleggId;
+	var filnavn = req.params.filnavn;
+	var deleteFilename = DATA_DIR + "/vedlegg_" + vedleggId + "/" + filnavn;
+	fs.unlink(deleteFilename, function(err) {
+		if (err) throw err;
+	});
+
+	// res.send({"status": "ok"});
+	const responseDelayInSeconds = 4;
+	setTimeout((function() {
+		res.send({"status": "ok"});
+	}), responseDelayInSeconds * 1000);
+});
+
+module.exports = router;
