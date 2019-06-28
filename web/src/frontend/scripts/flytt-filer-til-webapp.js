ecfe46cec6989d034e6efda3daafc22e27d3f33d
Andre Wolden <wolden.andre@gmail.com>
2019-06-28 15:02:09 +0200

    flytter alle filer til webapp

--- web/src/frontend/scripts/flytt-filer-til-webapp.js
+++ web/src/frontend/scripts/flytt-filer-til-webapp.js
@@ -27,6 +27,44 @@ function kopierSVG() {
 	});
 }
 
+
+function kopierRessurs(mainFile, folder) {
+	var newFile = mainFile.split(".")
+		.filter((value) => { return value != "chunk" })
+		.join(".");
+	var srcFile = [".", "build", "static", folder, mainFile];
+	var outFile = [
+		"..",
+		"main",
+		"resources",
+		"webapp",
+		"statisk",
+		folder,
+		newFile
+	];
+	fsextra.copy(srcFile.join(path.sep), outFile.join(path.sep), function (
+		err
+	) {
+		if (err) {
+			console.log("build/static/" + folder + "/" + mainFile);
+			return console.error(err);
+		} else {
+			console.log(
+				"Kopiert " +
+				"build/static/" +
+				folder +
+				"/" +
+				mainFile +
+				" til: " +
+				"../main/resources/webapp/statisk/" +
+				folder +
+				"/" +
+				newFile
+			);
+		}
+	});
+}
+
 function kopierStatisk() {
 	const mainFolder = [".", "build", "static"];
 	fs.readdir(mainFolder.join(path.sep), function(err, folders) {
@@ -34,45 +72,11 @@ function kopierStatisk() {
 			var dir = [".", "build", "static", folder];
 
 			fs.readdir(dir.join(path.sep), function(err, files) {
-				var mainFile = files[0];
-
-				if (mainFile) {
-					var fileending = mainFile.split(".")[2];
-					var filename = mainFile.split(".")[0];
-					var srcFile = [".", "build", "static", folder, mainFile];
-					var outFile = [
-						"..",
-						"main",
-						"resources",
-						"webapp",
-						"statisk",
-						folder,
-						filename + "." + fileending
-					];
-					fsextra.copy(srcFile.join(path.sep), outFile.join(path.sep), function(
-						err
-					) {
-						if (err) {
-							console.log("build/static/" + folder + "/" + mainFile);
-							return console.error(err);
-						} else {
-							console.log(
-								"Kopiert " +
-									"build/static/" +
-									folder +
-									"/" +
-									mainFile +
-									" til: " +
-									"../main/resources/webapp/statisk/" +
-									folder +
-									"/" +
-									filename +
-									"." +
-									fileending
-							);
-						}
-					});
-				}
+				files.forEach( (file) => {
+					kopierRessurs(file, folder)
+				});
+				// kopierRessurs(files[0], folder);
+				// kopierRessurs(files[2], folder);
 			});
 		});
 	});
