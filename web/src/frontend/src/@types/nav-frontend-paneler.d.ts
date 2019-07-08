6e824f6175d150fa3cf70f3706e01d68830c8cd9
Andre Wolden <wolden.andre@gmail.com>
2019-07-08 20:57:44 +0200

    bruker panel med border true fra design.nav.no

--- web/src/frontend/src/@types/nav-frontend-paneler.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "nav-frontend-paneler" {
-	import * as React from "react";
-
-	export interface PanelProps {
-		className?: string;
-		children: React.ReactNode;
-	}
-	export class Panel extends React.Component<PanelProps, {}> {}
-}
