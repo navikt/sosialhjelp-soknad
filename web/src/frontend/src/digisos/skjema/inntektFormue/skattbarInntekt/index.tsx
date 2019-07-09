91555919c3b4f9c3f6b8d8a2fa776afee2380cb8
Andre Wolden <wolden.andre@gmail.com>
2019-07-09 13:18:14 +0200

    bytter til lesmerpanel

--- web/src/frontend/src/digisos/skjema/inntektFormue/skattbarInntekt/index.tsx
+++ web/src/frontend/src/digisos/skjema/inntektFormue/skattbarInntekt/index.tsx
@@ -6,12 +6,11 @@ import {
 } from "../../../../nav-soknad/redux/soknadsdata/soknadsdataContainerUtils";
 import {SoknadsSti} from "../../../../nav-soknad/redux/soknadsdata/soknadsdataReducer";
 import {getIntlTextOrKey} from "../../../../nav-soknad/utils";
-import EkspanderbartpanelBase from "nav-frontend-ekspanderbartpanel/lib/ekspanderbartpanel-base";
-import {SkjemaGruppe} from "nav-frontend-skjema";
 import TextPlaceholder from "../../../../nav-soknad/components/animasjoner/placeholder/TextPlaceholder";
 import {REST_STATUS} from "../../../../nav-soknad/types";
 import {Organisasjon, SkattbarInntekt} from "./inntektTypes";
 import {Panel} from "nav-frontend-paneler";
+import Lesmerpanel from 'nav-frontend-lesmerpanel';
 
 type Props = SoknadsdataContainerProps & InjectedIntlProps;
 
@@ -41,10 +40,6 @@ class Skatt extends React.Component<Props, {}> {
 		const skattbarinntektogforskuddstrekk: SkattbarInntekt[] = soknadsdata.inntekt.skattbarinntektogforskuddstrekk;
 
 		const tittel = getIntlTextOrKey(intl, "utbetalinger.inntekt.skattbar.tittel");
-
-		// const harSkattbareInntekter: boolean = skattbareInntekter.length > 0;
-
-		// const skattbarInntekt = skattbareInntekter[0];
 
         const skattbarinntektogforskuddstrekkJsx: JSX.Element[] = skattbarinntektogforskuddstrekk.map((skattbarInntekt: SkattbarInntekt) => {
 
@@ -80,53 +75,22 @@ class Skatt extends React.Component<Props, {}> {
             return <div>{organisasjonerJSX}</div>
         });
 
-		// let organisasjoner: JSX.Element[] = [];
-		// if (skattbarInntekt && skattbarInntekt.organisasjoner){
-		// 	const organisasjoner: JSX.Element[] = skattbarInntekt.organisasjoner.map((organisasjon: Organisasjon) => {
-		// 		const fom = <FormattedDate value={organisasjon.fom!}/>;
-		// 		const tom = <FormattedDate value={organisasjon.tom!}/>;
-		// 		const lenkeSti = `https://skatt.skatteetaten.no/web/innsynamelding/inntekt/${organisasjon.orgnr}
-		// 							?year=${organisasjon.tom!.slice(0, 4)}&month=${organisasjon.tom!.slice(5, 7)}`;
-        //
-		// 		return (
-		// 			<div key={organisasjon.orgnr} className="utbetaling blokk">
-		// 				<div className="blokk-s">
-		// 					<h4 className="blokk-null">{organisasjon.organisasjonsnavn}</h4>
-		// 					<div>Fra {fom} til {tom}</div>
-		// 				</div>
-		// 				<div className="blokk-xs">
-		// 				{organisasjon.utbetalinger.map((utbetaling, index) => {
-		// 					const utbetalinger = [];
-		// 					if (utbetaling.brutto) {
-		// 						utbetalinger.push(Skatt.renderUtbetaling("Bruttoinntekt", utbetaling.brutto, index));
-		// 					}
-		// 					if (utbetaling.forskuddstrekk) {
-		// 						utbetalinger.push(Skatt.renderUtbetaling("Forskuddstrekk", utbetaling.forskuddstrekk, index));
-		// 					}
-		// 					return utbetalinger;
-		// 				})}
-		// 				</div>
-		// 				<a className="blokk-s" href={lenkeSti} target={`skatteetaten_${organisasjon.orgnr}`}>Se detaljer hos Skatteetaten.</a>
-		// 			</div>
-		// 		)
-		// 	});
-		// }
-
 		return (
-			<SkjemaGruppe className={"skjema-sporsmal"}>
+			<div className={"skatt-wrapper"}>
 				{!visAnimerteStreker && skattbarinntektogforskuddstrekkJsx && skattbarinntektogforskuddstrekkJsx.length > 0 && (
-					<EkspanderbartpanelBase
-						heading={
+					<Lesmerpanel
+						apneTekst={"Se detaljer"}
+						lukkTekst={"Lukk"}
+						intro={
 							<div>
 								<h4>{tittel}</h4>
 								<FormattedMessage id="utbetalinger.inntekt.skattbar.beskrivelse"/>
 							</div>
 						}
 						border={true}
-						ariaTittel={intl.formatMessage({id: "utbetalinger.inntekt.skattbar.beskrivelse"})}
 					>
 						<div className="utbetalinger">{skattbarinntektogforskuddstrekkJsx}</div>
-					</EkspanderbartpanelBase>)
+					</Lesmerpanel>)
 				}
 				{!visAnimerteStreker && skattbarinntektogforskuddstrekkJsx && skattbarinntektogforskuddstrekkJsx.length === 0 && (
 					<Panel
@@ -142,7 +106,7 @@ class Skatt extends React.Component<Props, {}> {
 				{ visAnimerteStreker &&
 					<TextPlaceholder lines={3}/>
 				}
-			</SkjemaGruppe>
+			</div>
 		);
 	}
 }
