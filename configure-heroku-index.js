0e3e48438ec79311dafc8ae6d411a4b98960672b
André Wolden <wolden.andre@gmail.com>
2019-08-09 10:10:10 +0200

    asdf

--- configure-heroku-index.js
+++ configure-heroku-index.js
@@ -1,1 +1,19 @@
-console.warn("Now gonna fix src for routes to js and css bundles....");
+const fs = require('fs');
+
+const HEROKU_APP_NAME = process.env.HEROKU_APP_NAME;
+const HEROKU_MASTER_APP_NAME = "sosialhjelp-soknad";
+
+fs.readFile('./index.html', function (err, html) {
+    if (err){
+        throw err;
+    }
+
+    console.warn("The name of this app is:" + HEROKU_APP_NAME);
+
+
+    if (HEROKU_APP_NAME != HEROKU_MASTER_APP_NAME){
+        console.warn(html.toString());
+    }
+});
+
+
