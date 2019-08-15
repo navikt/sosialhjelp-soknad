28eb5873bbcba4dcb16a2f2a29713745aa8143dc
siljee <silje.hansen3@nav.no>
2019-08-15 13:24:45 +0200

    Fikset test som feilet pga context-path ikke var endret i testen

--- src/nav-soknad/utils/rest-utils.test.ts
+++ src/nav-soknad/utils/rest-utils.test.ts
@@ -2,8 +2,8 @@ import {getAbsoluteApiUrlRegex} from "./rest-utils";
 
 
 test("that get getAbsoluteApiUrlRegex returns expected values", () => {
-    expect(getAbsoluteApiUrlRegex("/soknadsosialhjelp/skjema/1000232/5?visTekster=true"))
+    expect(getAbsoluteApiUrlRegex("/sosialhjelp/soknad/skjema/1000232/5?visTekster=true"))
         .toEqual("/sosialhjelp/soknad-api/")
-    expect(getAbsoluteApiUrlRegex("/digisos-1348/soknadsosialhjelp/something-else/behind/here"))
+    expect(getAbsoluteApiUrlRegex("/digisos-1348/sosialhjelp/soknad/something-else/behind/here"))
         .toEqual("/digisos-1348/sosialhjelp/soknad-api/")
 });
