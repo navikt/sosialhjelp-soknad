cdaea6038bf6088f2ec0069b5e2678a084c742c5
André Wolden <wolden.andre@gmail.com>
2019-09-23 14:02:48 +0200

    asdf

--- src/intlProvider.tsx
+++ src/intlProvider.tsx
@@ -25,6 +25,7 @@ type Props = StateProps & IntlProviderProps & DispatchProps;
 
 class IntlProvider extends React.Component<Props, {}> {
 	componentDidMount() {
+		console.warn("Lets fix IE11 the hard way :/");
 		this.props.dispatch(initStart());
 	}
 
