8e5a2ede1631582371f544b9fc2f1073b34d15e9
Thomas Flemming <thomas.flemming@gmail.com>
2019-09-26 13:03:29 +0200

    Ikke vise antall vedlegg ettersendt til NAV

--- src/digisos/skjema/ettersendelse/index.tsx
+++ src/digisos/skjema/ettersendelse/index.tsx
@@ -154,9 +154,7 @@ class Ettersendelse extends React.Component<Props, OwnState> {
                                     venstreIkon={MargIkoner.OK}
                                     key={ettersendelse.behandlingsId}
                                 >
-                                    <h3>
-                                        {ettersendelse.innsendteVedlegg.length} &nbsp;
-                                        <FormattedHTMLMessage id="ettersendelse.vedlegg_sendt"/></h3>
+                                    <h3><FormattedHTMLMessage id="ettersendelse.vedlegg_sendt"/></h3>
                                     <p>
                                         <FormattedHTMLMessage
                                             id="ettersendelse.dato_tid"
