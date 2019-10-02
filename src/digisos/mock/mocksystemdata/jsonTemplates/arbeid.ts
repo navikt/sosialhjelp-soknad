d1cf09f79c5a96509076dc58b0a33d76d6c73813
fleskesvor <eivind.nilsbakken@nav.no>
2019-10-02 22:04:31 +0200

    Gjør om arbeid til interface

--- src/digisos/mock/mocksystemdata/jsonTemplates/arbeid.ts
+++ src/digisos/mock/mocksystemdata/jsonTemplates/arbeid.ts
@@ -1,3 +1,26 @@
-export const arbeidJSON = {
-  "arbeidsforhold" : []
-};
+interface Arbeidsforhold {
+  "arbeidsforholdIDnav": string,
+  "ansettelsesPeriode": {
+    "periode": {
+      "fom": string,
+      "tom": string
+    }
+  },
+  "arbeidsavtale": Arbeidsavtale[]
+  "arbeidsgiver": {
+      "arbeidsgivernummer"?: string,
+      "orgnummer"?: string,
+      "navn"?: string,
+      "ident"?: {
+        "ident": string
+      }
+    }
+}
+
+interface Arbeidsavtale {
+  "stillingsprosent": string
+}
+
+export interface ArbeidJSON {
+  "arbeidsforhold": Arbeidsforhold[]
+}
