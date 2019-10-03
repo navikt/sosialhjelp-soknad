dbcba2cd3d3d0fddb830b438a21ba33d6a5a5297
fleskesvor <eivind.nilsbakken@nav.no>
2019-10-03 12:25:04 +0200

    Revert "Gjør om arbeid til interface"
    This reverts commit 81c1d1a6aa8ea19c3713cd44f29590aab1bb38e1.

--- src/digisos/mock/mocksystemdata/jsonTemplates/arbeid.ts
+++ src/digisos/mock/mocksystemdata/jsonTemplates/arbeid.ts
@@ -1,26 +1,3 @@
-interface Arbeidsforhold {
-  "arbeidsforholdIDnav": string,
-  "ansettelsesPeriode": {
-    "periode": {
-      "fom": string,
-      "tom": string
-    }
-  },
-  "arbeidsavtale": Arbeidsavtale[]
-  "arbeidsgiver": {
-      "arbeidsgivernummer"?: string,
-      "orgnummer"?: string,
-      "navn"?: string,
-      "ident"?: {
-        "ident": string
-      }
-    }
-}
-
-interface Arbeidsavtale {
-  "stillingsprosent": string
-}
-
-export interface ArbeidJSON {
-  "arbeidsforhold": Arbeidsforhold[]
-}
+export const arbeidJSON = {
+  "arbeidsforhold" : []
+};
