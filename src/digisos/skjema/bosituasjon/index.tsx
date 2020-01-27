2cd0c23ec343355b851fff75c591d1427645b6ea
Halvor Bjørn <halvor.bjorn@nav.no>
2020-01-27 14:43:44 +0100

    Skriver om fra klasser til funksjonelle komponenter

--- src/digisos/skjema/bosituasjon/index.tsx
+++ src/digisos/skjema/bosituasjon/index.tsx
@@ -1,16 +1,15 @@
 import * as React from "react";
-import DigisosSkjemaSteg, { DigisosSteg } from "../DigisosSkjemaSteg";
+import DigisosSkjemaSteg, {DigisosSteg} from "../DigisosSkjemaSteg";
 import BoligIllustrasjon from "../../../nav-soknad/components/svg/illustrasjoner/BoligIllustrasjon";
 import BosituasjonView from "./Bosituasjon";
 
-class Bosituasjon extends React.Component<{}, any> {
-	render() {
-		return (
-			<DigisosSkjemaSteg steg={DigisosSteg.bosituasjonbolk} ikon={<BoligIllustrasjon/>}>
-				<BosituasjonView />
-			</DigisosSkjemaSteg>
-		);
-	}
-}
+const Bosituasjon = () => (
+    <DigisosSkjemaSteg
+        steg={DigisosSteg.bosituasjonbolk}
+        ikon={<BoligIllustrasjon />}
+    >
+        <BosituasjonView />
+    </DigisosSkjemaSteg>
+);
 
 export default Bosituasjon;
