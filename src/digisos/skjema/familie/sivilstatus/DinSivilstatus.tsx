d5a32fe3ff89a8dd36620d8861fedefb3851545c
Halvor Bjørn <halvor.bjorn@nav.no>
2020-01-29 15:55:29 +0100

    Skriver om familie til å bruke funksjonelle komponenter og hooks

--- src/digisos/skjema/familie/sivilstatus/DinSivilstatus.tsx
+++ src/digisos/skjema/familie/sivilstatus/DinSivilstatus.tsx
@@ -1,69 +1,63 @@
 import * as React from "react";
-import {
-	connectSoknadsdataContainer,
-	SoknadsdataContainerProps
-} from "../../../redux/soknadsdata/soknadsdataContainerUtils";
-import { SoknadsSti } from "../../../redux/soknadsdata/soknadsdataReducer";
-import { Sivilstatus, Status } from "./FamilieTypes";
+import {useState, useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
+
+import {SoknadsSti} from "../../../redux/soknadsdata/soknadsdataReducer";
+import {Sivilstatus, Status} from "./FamilieTypes";
 import SivilstatusComponent from "./SivilstatusComponent";
 import EktefelleDetaljer from "./EktefelleDetaljer";
 import Sporsmal from "../../../../nav-soknad/components/sporsmal/Sporsmal";
 import TextPlaceholder from "../../../../nav-soknad/components/animasjoner/placeholder/TextPlaceholder";
 import {REST_STATUS} from "../../../redux/soknad/soknadTypes";
+import {State} from "../../../redux/reducers";
+import {hentSoknadsdata} from "../../../redux/soknadsdata/soknadsdataActions";
 
-type Props = SoknadsdataContainerProps;
+const DinSivilstatusView = () => {
+    const [oppstartsModus, setOppstartsModus] = useState(true);
 
-interface State {
-	oppstartsModus: boolean;
-}
+    const soknadsdata = useSelector((state: State) => state.soknadsdata);
+    const behandlingsId = useSelector(
+        (state: State) => state.soknad.behandlingsId
+    );
 
-class DinSivilstatusView extends React.Component<Props, State> {
+    const dispatch = useDispatch();
 
-	constructor(props: Props) {
-		super(props);
-		this.state = {
-			oppstartsModus: true
-		};
-	}
+    useEffect(() => {
+        if (behandlingsId) {
+            dispatch(hentSoknadsdata(behandlingsId, SoknadsSti.SIVILSTATUS));
+        }
+    }, [behandlingsId, dispatch]);
 
-	componentDidMount() {
-		const {behandlingsId} = this.props;
-		if (behandlingsId){
-			this.props.hentSoknadsdata(behandlingsId, SoknadsSti.SIVILSTATUS);
-		}
-	}
+    useEffect(() => {
+        const restStatus = soknadsdata.restStatus.familie.sivilstatus;
+        if (oppstartsModus && restStatus === REST_STATUS.OK) {
+            setOppstartsModus(false);
+        }
+    }, [oppstartsModus, soknadsdata.restStatus.familie.sivilstatus]);
 
-	componentWillUpdate() {
-		const { soknadsdata } = this.props;
-		const restStatus = soknadsdata.restStatus.familie.sivilstatus;
-		if (this.state.oppstartsModus && restStatus === REST_STATUS.OK) {
-			this.setState({ oppstartsModus: false });
-		}
-	}
+    const sivilstatus: Sivilstatus = soknadsdata.familie.sivilstatus;
+    const restStatus = soknadsdata.restStatus.familie.sivilstatus;
+    if (oppstartsModus === true && restStatus === REST_STATUS.OK) {
+        setOppstartsModus(false);
+    }
+    if (oppstartsModus) {
+        return (
+            <div className="skjema-sporsmal">
+                <Sporsmal sprakNokkel="familie.sivilstatus">
+                    <TextPlaceholder lines={6} />
+                </Sporsmal>
+            </div>
+        );
+    }
+    if (
+        sivilstatus &&
+        sivilstatus.sivilstatus === Status.GIFT &&
+        sivilstatus.kildeErSystem === true
+    ) {
+        return <EktefelleDetaljer />;
+    } else {
+        return <SivilstatusComponent />;
+    }
+};
 
-	render() {
-		const { soknadsdata } = this.props;
-		const sivilstatus: Sivilstatus = soknadsdata.familie.sivilstatus;
-		let oppstartsModus = this.state.oppstartsModus;
-		const restStatus = soknadsdata.restStatus.familie.sivilstatus;
-		if (oppstartsModus === true && restStatus === REST_STATUS.OK) {
-			oppstartsModus = false;
-		}
-		if (oppstartsModus) {
-			return (
-				<div className="skjema-sporsmal">
-					<Sporsmal sprakNokkel="familie.sivilstatus">
-						<TextPlaceholder lines={6}/>
-					</Sporsmal>
-				</div>
-			)
-		}
-		if (sivilstatus && sivilstatus.sivilstatus === Status.GIFT && sivilstatus.kildeErSystem === true) {
-			return <EktefelleDetaljer/>
-		} else {
-			return (<SivilstatusComponent/>);
-		}
-	}
-}
-
-export default connectSoknadsdataContainer(DinSivilstatusView);
+export default DinSivilstatusView;
--- src/digisos/skjema/familie/sivilstatus/PersonSkjema.tsx
+++ src/digisos/skjema/familie/sivilstatus/PersonSkjema.tsx
@@ -1,82 +1,96 @@
 import * as React from "react";
 import {Column, Container, Row} from "nav-frontend-grid";
 import InputEnhanced from "../../../../nav-soknad/faktum/InputEnhanced";
-import {setPath} from "../../../redux/soknadsdata/soknadsdataActions";
 import {
-    connectSoknadsdataContainer,
-    SoknadsdataContainerProps
-} from "../../../redux/soknadsdata/soknadsdataContainerUtils";
-import {SoknadsSti} from "../../../redux/soknadsdata/soknadsdataReducer";
-import {erTall, fdato, maksLengde, minLengde} from "../../../../nav-soknad/validering/valideringer";
+    setPath,
+    lagreSoknadsdata,
+} from "../../../redux/soknadsdata/soknadsdataActions";
+import {
+    SoknadsSti,
+    oppdaterSoknadsdataSti,
+} from "../../../redux/soknadsdata/soknadsdataReducer";
+import {
+    erTall,
+    fdato,
+    maksLengde,
+    minLengde,
+} from "../../../../nav-soknad/validering/valideringer";
 import {konverterFraISODato, konverterTilISODato} from "./datoUtils";
 import RadioEnhanced from "../../../../nav-soknad/faktum/RadioEnhanced";
 import Sporsmal from "../../../../nav-soknad/components/sporsmal/Sporsmal";
 import {Familie} from "./FamilieTypes";
-
-type Props = SoknadsdataContainerProps;
+import {useSelector, useDispatch} from "react-redux";
+import {State} from "../../../redux/reducers";
+import {
+    clearValideringsfeil,
+    setValideringsfeil,
+} from "../../../redux/validering/valideringActions";
 
 const FAKTUM_KEY = "familie.sivilstatus.gift.ektefelle";
 const FAKTUM_KEY_FNR = FAKTUM_KEY + ".fnr";
 const FAKTUM_KEY_PERSONNUMMER = FAKTUM_KEY + ".pnr";
 
-class PersonSkjema extends React.Component<Props, {}> {
+const PersonSkjema = () => {
+    const behandlingsId = useSelector(
+        (state: State) => state.soknad.behandlingsId
+    );
+    const soknadsdata = useSelector((state: State) => state.soknadsdata);
 
-    navnInput!: HTMLInputElement;
+    const dispatch = useDispatch();
 
-    constructor(props: Props) {
-        super(props);
-        this.focus = this.focus.bind(this);
-    }
-
-    componentDidMount() {
-        if (this.navnInput) {
-            this.focus();
-        }
-    }
-
-    focus() {
-        this.navnInput.focus();
-    }
-
-    oppdaterTekstfelt(sti: string, verdi: string | null) {
-        this.props.clearValideringsfeil(FAKTUM_KEY_FNR);
-        this.props.clearValideringsfeil(FAKTUM_KEY_PERSONNUMMER);
+    const oppdaterTekstfelt = (sti: string, verdi: string | null) => {
+        dispatch(clearValideringsfeil(FAKTUM_KEY_FNR));
+        dispatch(clearValideringsfeil(FAKTUM_KEY_PERSONNUMMER));
         if (verdi && verdi.length === 0) {
             verdi = null;
         }
-        const {soknadsdata, oppdaterSoknadsdataSti} = this.props;
         const sivilstatus = soknadsdata.familie.sivilstatus;
         const ektefelle = sivilstatus.ektefelle;
-        setPath(ektefelle, sti, verdi);
-        oppdaterSoknadsdataSti(SoknadsSti.SIVILSTATUS, sivilstatus);
-    }
 
-    onBlur() {
-        const {soknadsdata, lagreSoknadsdata, behandlingsId} = this.props;
+        setPath(ektefelle, sti, verdi);
+        dispatch(oppdaterSoknadsdataSti(SoknadsSti.SIVILSTATUS, sivilstatus));
+    };
+
+    const onBlur = () => {
         const sivilstatus = soknadsdata.familie.sivilstatus;
         let feilkodeFodselsdato = null;
-        let fodselsdato: string | null = sivilstatus.ektefelle ? sivilstatus.ektefelle.fodselsdato : null;
+        let fodselsdato: string | null = sivilstatus.ektefelle
+            ? sivilstatus.ektefelle.fodselsdato
+            : null;
 
         if (sivilstatus.ektefelle && sivilstatus.ektefelle.fodselsdato === "") {
             sivilstatus.ektefelle.fodselsdato = null;
         }
-        if (sivilstatus.ektefelle && sivilstatus.ektefelle.personnummer === "") {
+        if (
+            sivilstatus.ektefelle &&
+            sivilstatus.ektefelle.personnummer === ""
+        ) {
             sivilstatus.ektefelle.personnummer = null;
         }
         if (fodselsdato && fodselsdato !== "") {
             fodselsdato = konverterFraISODato(fodselsdato);
             feilkodeFodselsdato = fdato(fodselsdato);
-            (feilkodeFodselsdato) ?
-                this.props.setValideringsfeil(feilkodeFodselsdato, FAKTUM_KEY_FNR) :
-                this.props.clearValideringsfeil(FAKTUM_KEY_FNR);
+            feilkodeFodselsdato
+                ? dispatch(
+                      setValideringsfeil(feilkodeFodselsdato, FAKTUM_KEY_FNR)
+                  )
+                : dispatch(clearValideringsfeil(FAKTUM_KEY_FNR));
 
-            if (!feilkodeFodselsdato && sivilstatus.ektefelle && sivilstatus.ektefelle.fodselsdato) {
-                sivilstatus.ektefelle.fodselsdato = konverterTilISODato(sivilstatus.ektefelle.fodselsdato);
+            if (
+                !feilkodeFodselsdato &&
+                sivilstatus.ektefelle &&
+                sivilstatus.ektefelle.fodselsdato
+            ) {
+                sivilstatus.ektefelle.fodselsdato = konverterTilISODato(
+                    sivilstatus.ektefelle.fodselsdato
+                );
             }
         } else {
-            this.props.clearValideringsfeil(FAKTUM_KEY_FNR);
+            dispatch(clearValideringsfeil(FAKTUM_KEY_FNR));
         }
-        const personnummer: string | null = sivilstatus.ektefelle ? sivilstatus.ektefelle.personnummer : null;
+        const personnummer: string | null = sivilstatus.ektefelle
+            ? sivilstatus.ektefelle.personnummer
+            : null;
         let feilkodePersonnummer = null;
         if (personnummer && personnummer !== "") {
             feilkodePersonnummer = minLengde(personnummer, 5);
@@ -87,143 +101,164 @@ class PersonSkjema extends React.Component<Props, {}> {
                 feilkodePersonnummer = erTall(personnummer, true);
             }
 
-            (feilkodePersonnummer) ?
-                this.props.setValideringsfeil(feilkodePersonnummer, FAKTUM_KEY_PERSONNUMMER) :
-                this.props.clearValideringsfeil(FAKTUM_KEY_PERSONNUMMER);
+            feilkodePersonnummer
+                ? dispatch(
+                      setValideringsfeil(
+                          feilkodePersonnummer,
+                          FAKTUM_KEY_PERSONNUMMER
+                      )
+                  )
+                : dispatch(clearValideringsfeil(FAKTUM_KEY_PERSONNUMMER));
         } else {
-            this.props.clearValideringsfeil(FAKTUM_KEY_PERSONNUMMER);
+            dispatch(clearValideringsfeil(FAKTUM_KEY_PERSONNUMMER));
         }
         if (!feilkodeFodselsdato && !feilkodePersonnummer && behandlingsId) {
-            lagreSoknadsdata(behandlingsId, SoknadsSti.SIVILSTATUS, sivilstatus);
+            dispatch(
+                lagreSoknadsdata(
+                    behandlingsId,
+                    SoknadsSti.SIVILSTATUS,
+                    sivilstatus
+                )
+            );
         }
-    }
+    };
 
-    onClickBorSammen(verdi: boolean) {
-        const {soknadsdata, oppdaterSoknadsdataSti} = this.props;
+    const onClickBorSammen = (verdi: boolean) => {
         const sivilstatus = soknadsdata.familie.sivilstatus;
         sivilstatus.borSammenMed = verdi;
-        oppdaterSoknadsdataSti(SoknadsSti.SIVILSTATUS, sivilstatus);
-        this.onBlur();
+        dispatch(oppdaterSoknadsdataSti(SoknadsSti.SIVILSTATUS, sivilstatus));
+    };
+
+    const ektefelle = soknadsdata.familie.sivilstatus.ektefelle;
+    if (!ektefelle) {
+        return <div className="personskjema" />;
     }
-
-    render() {
-        const {soknadsdata} = this.props;
-        const ektefelle = soknadsdata.familie.sivilstatus.ektefelle;
-        if (!ektefelle) {
-            return <div className="personskjema"/>;
-        }
-        const fodselsdato = ektefelle.fodselsdato ? konverterFraISODato(ektefelle.fodselsdato) : "";
-        if (!ektefelle.personnummer) {
-            ektefelle.personnummer = "";
-        }
-        const personnummer = ektefelle.personnummer || "";
-
-        const familie: Familie = soknadsdata.familie;
-        const borSammenMed = (familie && familie.sivilstatus) ? familie.sivilstatus.borSammenMed : null;
-
-        return (
-            <div className="personskjema">
-                <Container fluid={true} className="container--noPadding">
-                    <Row>
-                        <Column xs="12">
-                            <InputEnhanced
-                                getName={() => FAKTUM_KEY + "_fornavn_input"}
-                                id={FAKTUM_KEY + "_fornavn_input"}
-                                inputRef={ (c: any) => (this.navnInput = c)}
-                                maxLength={100}
-                                verdi={ektefelle.navn.fornavn}
-                                onChange={(verdi: string) => this.oppdaterTekstfelt("navn/fornavn", verdi)}
-                                onBlur={() => this.onBlur()}
-                                faktumKey="familie.sivilstatus.gift.ektefelle.fornavn"
-                                required={true}
-                            />
-                        </Column>
-                    </Row>
-                    <Row>
-                        <Column xs="12">
-                            <InputEnhanced
-                                getName={() => FAKTUM_KEY + "_mellomnavn_input"}
-                                id={FAKTUM_KEY + "_mellomnavn_input"}
-                                maxLength={100}
-                                verdi={ektefelle.navn.mellomnavn ? ektefelle.navn.mellomnavn : ""}
-                                onChange={(verdi: string) => this.oppdaterTekstfelt("navn/mellomnavn", verdi)}
-                                onBlur={() => this.onBlur()}
-                                faktumKey="familie.sivilstatus.gift.ektefelle.mellomnavn"
-                                required={true}
-                            />
-                        </Column>
-                    </Row>
-                    <Row className="add-padding-bottom">
-                        <Column xs="12">
-                            <InputEnhanced
-                                getName={() => FAKTUM_KEY + "_etternavn_input"}
-                                id={FAKTUM_KEY + "_etternavn_input"}
-                                maxLength={100}
-                                verdi={ektefelle.navn.etternavn}
-                                onChange={(verdi: string) => this.oppdaterTekstfelt("navn/etternavn", verdi)}
-                                onBlur={() => this.onBlur()}
-                                faktumKey="familie.sivilstatus.gift.ektefelle.etternavn"
-                                required={true}
-                            />
-                        </Column>
-                    </Row>
-                    <Row>
-                        <Column xs="12">
-                            <InputEnhanced
-                                getName={() => FAKTUM_KEY_FNR}
-                                id={FAKTUM_KEY_FNR}
-                                maxLength={8}
-                                minLength={8}
-                                verdi={fodselsdato}
-                                onChange={(verdi: string) => this.oppdaterTekstfelt("fodselsdato", verdi)}
-                                bredde="S"
-                                onBlur={() => this.onBlur()}
-                                faktumKey="familie.sivilstatus.gift.ektefelle.fnr"
-                                required={true}
-                            />
-                        </Column>
-                    </Row>
-                    <Row>
-                        <Column xs="12">
-                            <InputEnhanced
-                                getName={() => FAKTUM_KEY_PERSONNUMMER}
-                                id={FAKTUM_KEY_PERSONNUMMER}
-                                maxLength={5}
-                                minLength={5}
-                                verdi={personnummer}
-                                onChange={(verdi: string) => this.oppdaterTekstfelt("personnummer", verdi)}
-                                bredde="S"
-                                onBlur={() => this.onBlur()}
-                                faktumKey="familie.sivilstatus.gift.ektefelle.pnr"
-                                required={true}
-                            />
-                        </Column>
-                    </Row>
-                </Container>
-
-                <Sporsmal
-                    sprakNokkel="familie.sivilstatus.gift.ektefelle.borsammen"
-                >
-                    <RadioEnhanced
-                        id={"sivilstatus_gift_bor_sammen_radio_ja"}
-                        faktumKey="familie.sivilstatus.gift.ektefelle.borsammen"
-                        value="true"
-                        checked={borSammenMed === true}
-                        onChange={() => this.onClickBorSammen(true)}
-                    />
-                    <RadioEnhanced
-                        id={"sivilstatus_gift_bor_sammen_radio_nei"}
-                        faktumKey="familie.sivilstatus.gift.ektefelle.borsammen"
-                        value="false"
-                        checked={borSammenMed === false}
-                        onChange={() => this.onClickBorSammen(false)}
-                    />
-                </Sporsmal>
-
-
-            </div>
-        );
+    const fodselsdato = ektefelle.fodselsdato
+        ? konverterFraISODato(ektefelle.fodselsdato)
+        : "";
+    if (!ektefelle.personnummer) {
+        ektefelle.personnummer = "";
     }
-}
+    const personnummer = ektefelle.personnummer || "";
 
-export default connectSoknadsdataContainer(PersonSkjema);
+    const familie: Familie = soknadsdata.familie;
+    const borSammenMed =
+        familie && familie.sivilstatus
+            ? familie.sivilstatus.borSammenMed
+            : null;
+
+    return (
+        <div className="personskjema">
+            <Container fluid={true} className="container--noPadding">
+                <Row>
+                    <Column xs="12">
+                        <InputEnhanced
+                            getName={() => FAKTUM_KEY + "_fornavn_input"}
+                            id={FAKTUM_KEY + "_fornavn_input"}
+                            autoFocus={true}
+                            maxLength={100}
+                            verdi={ektefelle.navn.fornavn}
+                            onChange={(verdi: string) =>
+                                oppdaterTekstfelt("navn/fornavn", verdi)
+                            }
+                            onBlur={() => onBlur()}
+                            faktumKey="familie.sivilstatus.gift.ektefelle.fornavn"
+                            required={true}
+                        />
+                    </Column>
+                </Row>
+                <Row>
+                    <Column xs="12">
+                        <InputEnhanced
+                            getName={() => FAKTUM_KEY + "_mellomnavn_input"}
+                            id={FAKTUM_KEY + "_mellomnavn_input"}
+                            maxLength={100}
+                            verdi={
+                                ektefelle.navn.mellomnavn
+                                    ? ektefelle.navn.mellomnavn
+                                    : ""
+                            }
+                            onChange={(verdi: string) =>
+                                oppdaterTekstfelt("navn/mellomnavn", verdi)
+                            }
+                            onBlur={() => onBlur()}
+                            faktumKey="familie.sivilstatus.gift.ektefelle.mellomnavn"
+                            required={true}
+                        />
+                    </Column>
+                </Row>
+                <Row className="add-padding-bottom">
+                    <Column xs="12">
+                        <InputEnhanced
+                            getName={() => FAKTUM_KEY + "_etternavn_input"}
+                            id={FAKTUM_KEY + "_etternavn_input"}
+                            maxLength={100}
+                            verdi={ektefelle.navn.etternavn}
+                            onChange={(verdi: string) =>
+                                oppdaterTekstfelt("navn/etternavn", verdi)
+                            }
+                            onBlur={() => onBlur()}
+                            faktumKey="familie.sivilstatus.gift.ektefelle.etternavn"
+                            required={true}
+                        />
+                    </Column>
+                </Row>
+                <Row>
+                    <Column xs="12">
+                        <InputEnhanced
+                            getName={() => FAKTUM_KEY_FNR}
+                            id={FAKTUM_KEY_FNR}
+                            maxLength={8}
+                            minLength={8}
+                            verdi={fodselsdato}
+                            onChange={(verdi: string) =>
+                                oppdaterTekstfelt("fodselsdato", verdi)
+                            }
+                            bredde="S"
+                            onBlur={() => onBlur()}
+                            faktumKey="familie.sivilstatus.gift.ektefelle.fnr"
+                            required={true}
+                        />
+                    </Column>
+                </Row>
+                <Row>
+                    <Column xs="12">
+                        <InputEnhanced
+                            getName={() => FAKTUM_KEY_PERSONNUMMER}
+                            id={FAKTUM_KEY_PERSONNUMMER}
+                            maxLength={5}
+                            minLength={5}
+                            verdi={personnummer}
+                            onChange={(verdi: string) =>
+                                oppdaterTekstfelt("personnummer", verdi)
+                            }
+                            bredde="S"
+                            onBlur={() => onBlur()}
+                            faktumKey="familie.sivilstatus.gift.ektefelle.pnr"
+                            required={true}
+                        />
+                    </Column>
+                </Row>
+            </Container>
+
+            <Sporsmal sprakNokkel="familie.sivilstatus.gift.ektefelle.borsammen">
+                <RadioEnhanced
+                    id={"sivilstatus_gift_bor_sammen_radio_ja"}
+                    faktumKey="familie.sivilstatus.gift.ektefelle.borsammen"
+                    value="true"
+                    checked={borSammenMed === true}
+                    onChange={() => onClickBorSammen(true)}
+                />
+                <RadioEnhanced
+                    id={"sivilstatus_gift_bor_sammen_radio_nei"}
+                    faktumKey="familie.sivilstatus.gift.ektefelle.borsammen"
+                    value="false"
+                    checked={borSammenMed === false}
+                    onChange={() => onClickBorSammen(false)}
+                />
+            </Sporsmal>
+        </div>
+    );
+};
+
+export default PersonSkjema;
