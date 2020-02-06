596a7aab6f2b8a2fa1983f614a47abc50b7c5a80
Halvor Bjørn <halvor.bjorn@nav.no>
2020-02-06 09:53:06 +0100

    Oppgraderer nav-frontend-moduler

--- src/nav-soknad/components/hjelpetekst/Hjelpetekst.tsx
+++ src/nav-soknad/components/hjelpetekst/Hjelpetekst.tsx
@@ -1,19 +1,20 @@
 import * as React from "react";
-import { HjelpetekstAuto, HjelpetekstProps } from "nav-frontend-hjelpetekst";
+import NavHjelpetekst, {HjelpetekstProps} from "nav-frontend-hjelpetekst";
+import {PopoverOrientering} from "nav-frontend-popover";
 
 /** Denne wrapper nav-frontend-moduler
  * sin HjelpetekstAuto frem til de fikser en aria-sjekk
  * for described by
  */
 const Hjelpetekst: React.StatelessComponent<HjelpetekstProps> = (
-	props: HjelpetekstProps
+    props: HjelpetekstProps
 ) => (
-	<span>
-		<span className="kunSkjermleser" id={`tooltip-${props.id}`}>
-			{props.children}
-		</span>
-		<HjelpetekstAuto {...props} />
-	</span>
+    <span>
+        <span className="kunSkjermleser" id={`tooltip-${props.id}`}>
+            {props.children}
+        </span>
+        <NavHjelpetekst type={PopoverOrientering.Over} {...props} />
+    </span>
 );
 
 export default Hjelpetekst;
