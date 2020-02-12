c20cd67a642fbad7d94b43e77a907952ae1a4052
martintveter <41987225+martintveter@users.noreply.github.com>
2020-02-12 14:55:35 +0100

    Feature/hotjartrigger ettersendelseside (#619)
    * hotjartrigger for ved feil på ettersendelsessiden

---
+++ src/nav-soknad/components/hotjarTrigger/HotjarTrigger.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {node, string} from 'prop-types';
+import {erMockMiljoEllerDev} from "../../utils";
+
+export default class HotjarTrigger extends Component {
+    componentDidMount() {
+        const {hotjarTrigger} = this.props;
+        if (typeof window.hj === 'function'
+            && !erMockMiljoEllerDev()
+        ) {
+            window.hj('trigger', hotjarTrigger);
+        }
+    }
+
+    render() {
+        const {children} = this.props;
+        return children;
+    }
+}
+
+HotjarTrigger.propTypes = {
+    hotjarTrigger: string.isRequired,
+    children: node.isRequired
+};
+
+export const SoknadEttersendelseFeilerHotjarTrigger = ({children}) => (
+    <HotjarTrigger hotjarTrigger="soknad_ettersendelse_feiler">
+        {children}
+    </HotjarTrigger>
+);
+
+SoknadEttersendelseFeilerHotjarTrigger.propTypes = {
+    children: node
+};
