ca88cc446ed29aec6e01676e821d225fad4b5847
Halvor Bjørn <halvor.bjorn@nav.no>
2020-04-18 13:48:46 +0200

    Oppgraderer panel

--- src/digisos/skjema/inntektFormue/skattbarInntekt/index.tsx
+++ src/digisos/skjema/inntektFormue/skattbarInntekt/index.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
 import {FormattedMessage} from "react-intl";
-import {Panel} from "nav-frontend-paneler";
+import Panel from "nav-frontend-paneler";
 import Lesmerpanel from "nav-frontend-lesmerpanel";
 import {useDispatch, useSelector} from "react-redux";
 
-import {
-    SkattbarInntektInfo,
-    SoknadsSti,
-} from "../../../redux/soknadsdata/soknadsdataReducer";
+import {SkattbarInntektInfo, SoknadsSti} from "../../../redux/soknadsdata/soknadsdataReducer";
 import TextPlaceholder from "../../../../nav-soknad/components/animasjoner/placeholder/TextPlaceholder";
 import {SkattbarInntekt} from "./inntektTypes";
 import {REST_STATUS} from "../../../redux/soknad/soknadTypes";
@@ -19,29 +16,21 @@ const Skatt = () => {
     const dispatch = useDispatch();
 
     const soknadsdata = useSelector((state: State) => state.soknadsdata);
-    const behandlingsId = useSelector(
-        (state: State) => state.soknad.behandlingsId
-    );
+    const behandlingsId = useSelector((state: State) => state.soknad.behandlingsId);
 
     React.useEffect(() => {
         if (behandlingsId) {
-            dispatch(
-                hentSoknadsdata(behandlingsId, SoknadsSti.SKATTBARINNTEKT)
-            );
+            dispatch(hentSoknadsdata(behandlingsId, SoknadsSti.SKATTBARINNTEKT));
         }
     }, [behandlingsId, dispatch]);
 
-    const restStatus =
-        soknadsdata.restStatus.inntekt.skattbarinntektogforskuddstrekk;
-    const skattbarinntektogforskuddstrekk: SkattbarInntektInfo =
-        soknadsdata.inntekt.skattbarinntektogforskuddstrekk;
+    const restStatus = soknadsdata.restStatus.inntekt.skattbarinntektogforskuddstrekk;
+    const skattbarinntektogforskuddstrekk: SkattbarInntektInfo = soknadsdata.inntekt.skattbarinntektogforskuddstrekk;
     const visAnimerteStreker = restStatus !== REST_STATUS.OK;
 
     // TODO DIGISOS-1175: Håndter flere måneder med skattbar inntekt
-    const inntektFraSkatteetaten: SkattbarInntekt[] =
-        skattbarinntektogforskuddstrekk.inntektFraSkatteetaten;
-    const inntektFraSkatteetatenFeilet: boolean =
-        skattbarinntektogforskuddstrekk.inntektFraSkatteetatenFeilet;
+    const inntektFraSkatteetaten: SkattbarInntekt[] = skattbarinntektogforskuddstrekk.inntektFraSkatteetaten;
+    const inntektFraSkatteetatenFeilet: boolean = skattbarinntektogforskuddstrekk.inntektFraSkatteetatenFeilet;
     const tittel: JSX.Element = (
         <h4>
             <FormattedMessage id="utbetalinger.inntekt.skattbar.tittel" />
@@ -58,29 +47,23 @@ const Skatt = () => {
                     </div>
                 </Panel>
             )}
-            {!visAnimerteStreker &&
-                inntektFraSkatteetaten &&
-                inntektFraSkatteetaten.length > 0 && (
-                    <Lesmerpanel
-                        apneTekst={"Se detaljer"}
-                        lukkTekst={"Lukk"}
-                        intro={
-                            <div>
-                                {tittel}
-                                <FormattedMessage id="utbetalinger.inntekt.skattbar.beskrivelse" />
-                            </div>
-                        }
-                        border={true}
-                    >
-                        <div className="utbetalinger">
-                            <SkattbarinntektForskuddstrekk
-                                skattbarinntektogforskuddstrekk={
-                                    inntektFraSkatteetaten
-                                }
-                            />
+            {!visAnimerteStreker && inntektFraSkatteetaten && inntektFraSkatteetaten.length > 0 && (
+                <Lesmerpanel
+                    apneTekst={"Se detaljer"}
+                    lukkTekst={"Lukk"}
+                    intro={
+                        <div>
+                            {tittel}
+                            <FormattedMessage id="utbetalinger.inntekt.skattbar.beskrivelse" />
                         </div>
-                    </Lesmerpanel>
-                )}
+                    }
+                    border={true}
+                >
+                    <div className="utbetalinger">
+                        <SkattbarinntektForskuddstrekk skattbarinntektogforskuddstrekk={inntektFraSkatteetaten} />
+                    </div>
+                </Lesmerpanel>
+            )}
             {!visAnimerteStreker &&
                 !inntektFraSkatteetatenFeilet &&
                 inntektFraSkatteetaten &&
