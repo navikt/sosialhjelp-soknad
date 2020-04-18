c27826189e8b2e6565dfe82fc7564fd7cd0c78dc
Halvor Bjørn <halvor.bjorn@nav.no>
2020-04-18 13:48:46 +0200

    Oppgraderer panel

--- src/nav-soknad/components/infoblokk/index.tsx
+++ src/nav-soknad/components/infoblokk/index.tsx
@@ -1,51 +1,37 @@
 import * as React from "react";
-import { Panel } from "nav-frontend-paneler";
+import Panel from "nav-frontend-paneler";
 import Icon from "nav-frontend-ikoner-assets";
-import { Innholdstittel, Systemtittel } from "nav-frontend-typografi";
+import {Innholdstittel, Systemtittel} from "nav-frontend-typografi";
 import ProblemSirkel from "../svg/ProblemSirkel";
 
 interface Props {
-	tittel?: string;
-	brukSystemtittel?: boolean;
-	className?: string;
-	ikon?: "standard" | "advarsel";
+    tittel?: string;
+    brukSystemtittel?: boolean;
+    className?: string;
+    ikon?: "standard" | "advarsel";
 }
 
-const Infoblokk: React.StatelessComponent<Props> = ({
-	className,
-	children,
-	tittel,
-	ikon,
-	brukSystemtittel
-}) => {
-	return (
-		<Panel className={`skjema-infoblokk ${className}`}>
-			<div className="skjema-infoblokk__content">
-				<div className="skjema-infoblokk__icon">
-					{ikon === "advarsel" ? (
-						<ProblemSirkel />
-					) : (
-						<Icon kind="info-sirkel" />
-					)}
-				</div>
-				{tittel && (
-					<div>
-						{brukSystemtittel ? (
-							<Systemtittel className="skjema-infoblokk__title">
-								{tittel}
-							</Systemtittel>
-						) : (
-							<Innholdstittel className="skjema-infoblokk__title">
-								{tittel}
-							</Innholdstittel>
-						)}
-						<div className="skjema-infoblokk__dash" />
-					</div>
-				)}
-				<div className="skjema-infoblokk__tekst">{children}</div>
-			</div>
-		</Panel>
-	);
+const Infoblokk: React.StatelessComponent<Props> = ({className, children, tittel, ikon, brukSystemtittel}) => {
+    return (
+        <Panel className={`skjema-infoblokk ${className}`}>
+            <div className="skjema-infoblokk__content">
+                <div className="skjema-infoblokk__icon">
+                    {ikon === "advarsel" ? <ProblemSirkel /> : <Icon kind="info-sirkel" />}
+                </div>
+                {tittel && (
+                    <div>
+                        {brukSystemtittel ? (
+                            <Systemtittel className="skjema-infoblokk__title">{tittel}</Systemtittel>
+                        ) : (
+                            <Innholdstittel className="skjema-infoblokk__title">{tittel}</Innholdstittel>
+                        )}
+                        <div className="skjema-infoblokk__dash" />
+                    </div>
+                )}
+                <div className="skjema-infoblokk__tekst">{children}</div>
+            </div>
+        </Panel>
+    );
 };
 
 export default Infoblokk;
--- src/nav-soknad/components/skjemapanel/index.tsx
+++ src/nav-soknad/components/skjemapanel/index.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
-import {Panel, PanelBaseProps} from "nav-frontend-paneler";
+import Panel, {PanelProps} from "nav-frontend-paneler";
 import * as classNames from "classnames";
 
+const Skjemapanel: React.FC<PanelProps> = (props: PanelProps) => {
+    console.log("skjemapanelProps", props);
 
-const Skjemapanel: React.FC<PanelBaseProps> = (
-	props: PanelBaseProps
-) => (
-	<Panel
-		{...props}
-		// @ts-ignore
-		className={classNames("skjema-skjemapanel", props.className)}
-	/>
-);
+    return (
+        <Panel
+            {...props}
+            // @ts-ignore
+            className={classNames("skjema-skjemapanel", props.className)}
+        />
+    );
+};
 
 export default Skjemapanel;
