439f766f811d1524c95cb90f56850569eb696955
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2020-04-30 13:49:49 +0200

    test

--- src/digisos/redux/fil/filReducer.ts
+++ src/digisos/redux/fil/filReducer.ts
@@ -1,5 +1,5 @@
 import {FilActionTypeKeys, FilActionTypes, FilState} from "./filTypes";
-import {REST_STATUS} from "../soknad/soknadTypes";
+import {REST_FEIL, REST_STATUS} from "../soknad/soknadTypes";
 
 const initialState: FilState = {
     opplastingStatus: REST_STATUS.OK,
@@ -22,6 +22,19 @@ export const filReducer = (state: FilState = initialState, action: FilActionType
                 opplastingStatus: REST_STATUS.LAST_OPP_FIL_FEILET,
                 feilKode: action.feilKode,
                 opplysningtype: action.opplysningType,
+            };
+        }
+        case FilActionTypeKeys.START_SLETT_FIL: {
+            const samletVedleggStorrelseForStor: boolean =
+                state.feilKode === REST_FEIL.SAMLET_VEDLEGG_STORRELSE_FOR_STOR;
+            const feilKodeUpdated: REST_FEIL | null = samletVedleggStorrelseForStor ? null : state.feilKode;
+            const opplastingStatusUpdated: REST_STATUS = samletVedleggStorrelseForStor
+                ? REST_STATUS.OK
+                : state.opplastingStatus;
+            return {
+                ...state,
+                feilKode: feilKodeUpdated,
+                opplastingStatus: opplastingStatusUpdated,
             };
         }
         default:
