e73b1e09f7479df64d1475fd5adc6722c58215f7
Halvor Bjørn <halvor.bjorn@nav.no>
2020-05-05 15:40:31 +0200

    Tar i bruk veilederpaneler fra nav-frontend (#644)

---
+++ src/digisos/skjema/ny-oppsummering/SoknadsmottakerInfoPanel.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {State} from "../../redux/reducers";
+import Veilederpanel from "nav-frontend-veilederpanel";
+import Brevkonvolutt from "../../../nav-soknad/components/svg/Brevkonvolutt";
+import {FormattedMessage} from "react-intl";
+import {visSamtykkeInfo} from "../../redux/soknad/soknadActions";
+import {Undertittel, Normaltekst} from "nav-frontend-typografi";
+
+export const SoknadsmottakerInfoPanel = () => {
+    const valgtSoknadsmottaker = useSelector((state: State) => state.soknad.valgtSoknadsmottaker);
+    const dispatch = useDispatch();
+    if (valgtSoknadsmottaker) {
+        const valgtEnhetsNavn = `${valgtSoknadsmottaker.enhetsnavn}, ${valgtSoknadsmottaker.kommunenavn} kommune`;
+        return (
+            <Veilederpanel
+                type="plakat"
+                fargetema="advarsel"
+                svg={<Brevkonvolutt visBakgrundsSirkel={false} />}
+                kompakt
+            >
+                <Undertittel>Søknaden din blir sendt til {valgtEnhetsNavn}</Undertittel>
+                <Normaltekst>
+                    Dette kontoret har ansvar for å behandle søknaden din, og {valgtEnhetsNavn} lagrer opplysningene fra
+                    søknaden.
+                </Normaltekst>
+
+                <button
+                    className="linkbutton linkbutton--normal"
+                    onClick={() => {
+                        dispatch(visSamtykkeInfo(true));
+                    }}
+                >
+                    <FormattedMessage id="informasjon.tekster.personopplysninger.rettigheter.lenke" />
+                </button>
+            </Veilederpanel>
+        );
+    } else {
+        return null;
+    }
+};
--- src/digisos/skjema/ny-oppsummering/UbesvarteSporsmalPanel.tsx
+++ src/digisos/skjema/ny-oppsummering/UbesvarteSporsmalPanel.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import {DigisosFarge} from "../../../nav-soknad/components/svg/DigisosFarger";
-import Informasjonspanel, {InformasjonspanelIkon} from "../../../nav-soknad/components/informasjonspanel";
+import Veilederpanel from "nav-frontend-veilederpanel";
 import {Undertittel, Normaltekst} from "nav-frontend-typografi";
+import Ella from "../../../nav-soknad/components/svg/Ella";
 
 export const UbesvarteSporsmalPanel = () => {
     return (
-        <Informasjonspanel farge={DigisosFarge.VIKTIG} ikon={InformasjonspanelIkon.ELLA}>
+        <Veilederpanel type="plakat" fargetema="advarsel" svg={<Ella visBakgrundsSirkel={false} />} kompakt>
             <Undertittel>Du har ikke besvart alle spørsmålene i søknaden</Undertittel>
             <Normaltekst>
                 For å gjøre det enklere for veilederen å behandle søknaden din anbefaler jeg at du svarer på alle
                 spørsmålene.
             </Normaltekst>
-        </Informasjonspanel>
+        </Veilederpanel>
     );
 };
