69558dfc1d1b8f24fe7abc8e59bd4cf94d98faeb
Halvor Bjørn <halvor.bjorn@nav.no>
2020-05-05 10:05:10 +0200

    Oppdaterer tekster og legger til modal for ikke-besvart panel

---
+++ src/digisos/skjema/okonomiskeOpplysninger/OpplysningerIkkeBesvartPanel.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Informasjonspanel, {InformasjonspanelIkon} from "../../../nav-soknad/components/informasjonspanel";
+import {DigisosFarge} from "../../../nav-soknad/components/svg/DigisosFarger";
+import {useDispatch} from "react-redux";
+import {visLasteOppVedleggModal} from "../../redux/soknad/soknadActions";
+import {OpplastingAvVedleggModal} from "./OpplastingAvVedleggModal";
+import {FormattedMessage} from "react-intl";
+
+export const OpplysningerIkkeBesvartPanel = () => {
+    const dispatch = useDispatch();
+
+    return (
+        <div className="steg-ekstrainformasjon__infopanel">
+            <Informasjonspanel ikon={InformasjonspanelIkon.HENSYN} farge={DigisosFarge.VIKTIG}>
+                <p>
+                    <FormattedMessage id="opplysninger.ikkebesvart.avsnitt1" />
+                </p>
+                <p>
+                    <FormattedMessage id="opplysninger.ikkebesvart.avsnitt2" />
+                </p>
+                <button
+                    className="linkbutton linkbutton--normal"
+                    onClick={() => {
+                        dispatch(visLasteOppVedleggModal(true));
+                    }}
+                >
+                    <FormattedMessage id="opplysninger.informasjon.lenke" />
+                </button>
+                <OpplastingAvVedleggModal />
+            </Informasjonspanel>
+        </div>
+    );
+};
