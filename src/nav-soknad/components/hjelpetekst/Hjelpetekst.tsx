3b8d34a5d11b09be42e12bf989c0fca62742a5e5
Halvor Bjørn <halvor.bjorn@nav.no>
2020-06-10 10:27:23 +0200

    Oppgraderer hjelpetekst til ny major versjon (#660)
    * Oppgraderer hjelpetekst til ny major versjon
    
    * Legger til maksbredde på hjelpetekst

--- src/nav-soknad/components/hjelpetekst/Hjelpetekst.tsx
+++ src/nav-soknad/components/hjelpetekst/Hjelpetekst.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
-import {HjelpetekstAuto, HjelpetekstProps} from "nav-frontend-hjelpetekst";
+import Hjelpetekst, {HjelpetekstProps} from "nav-frontend-hjelpetekst";
 
 /** Denne wrapper nav-frontend-moduler
  * sin HjelpetekstAuto frem til de fikser en aria-sjekk
  * for described by
  */
-const Hjelpetekst: React.StatelessComponent<HjelpetekstProps> = (props: HjelpetekstProps) => (
+const _Hjelpetekst: React.StatelessComponent<HjelpetekstProps> = (props: HjelpetekstProps) => (
     <span>
         <span className="kunSkjermleser" id={`tooltip-${props.id}`}>
             {props.children}
         </span>
-        <HjelpetekstAuto {...props} />
+        <Hjelpetekst {...props} />
     </span>
 );
 
-export default Hjelpetekst;
+export default _Hjelpetekst;
