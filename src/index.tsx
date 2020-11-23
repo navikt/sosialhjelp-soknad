e3bac134756555a7bcaae42a00905a0ca5b53a73
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2020-11-23 15:18:26 +0100

    Egen sjekk på dev-sbs, ingen sentry for andre domener

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ import sagas from "./rootSaga";
 import IntlProvider from "./intlProvider";
 import App from "./digisos";
 import {loggException} from "./digisos/redux/navlogger/navloggerActions";
-import {erLocalhost, erProd} from "./nav-soknad/utils/rest-utils";
+import {erDevSbs, erLocalhost, erProd} from "./nav-soknad/utils/rest-utils";
 import {avbrytSoknad} from "./digisos/redux/soknad/soknadActions";
 import {NAVIGASJONSPROMT} from "./nav-soknad/utils";
 import {visSoknadAlleredeSendtPrompt} from "./digisos/redux/ettersendelse/ettersendelseActions";
@@ -50,7 +50,7 @@ const history = require("history").createBrowserHistory({
 
 if (erProd()) {
     Sentry.init({dsn: "https://9414e5a7f3e641c6b223f34289d373ff@sentry.gc.nav.no/50"});
-} else {
+} else if (erDevSbs()) {
     Sentry.init({
         dsn: "https://f3482eab7c994893bf44bcb26a0c8e68@sentry.gc.nav.no/14",
     });
