745ef62b01158bbc6c8f0fb28bdf42866685f93e
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2021-02-17 09:34:54 +0100

    Sonarcloud fiks.

--- src/nav-soknad/utils/rest-utils.ts
+++ src/nav-soknad/utils/rest-utils.ts
@@ -44,28 +44,34 @@ enum LocalHostMode {
     LOCALHOST_LOKAL_LOGIN_API,
 }
 
+export function getLocalhostApiBaseUrl(withAccessToken: boolean | undefined, apiContextPath: string): string {
+    const mode = LocalHostMode.LOCALHOST_MOCK_PROFIL; // Velg modus her!
+
+    if (mode == LocalHostMode.LOCALHOST_MOCK_PROFIL) {
+        // Kjør mot lokal sosialhjelp-soknad-api:
+        return `http://localhost:8181/${API_CONTEXT_PATH}/`;
+    }
+
+    if (mode == LocalHostMode.LOCALHOST_MOCK_ALT_PROFIL) {
+        // Kjør mot lokal sosialhjelp-soknad-api og mock-alt-api (som login-api):
+        if (withAccessToken) return `http://localhost:8989/sosialhjelp/mock-alt-api/login-api/${API_CONTEXT_PATH}/`;
+        else return `http://localhost:8181/${API_CONTEXT_PATH}/`;
+    }
+
+    if (mode == LocalHostMode.LOCALHOST_LOKAL_LOGIN_API) {
+        // Kjør med login-api som proxy om en ønsker access_token fra idporten
+        const apiPort = withAccessToken ? 7000 : 8181;
+        return `http://localhost:${apiPort}/${apiContextPath}/`;
+    }
+
+    return `http://localhost:8181/${API_CONTEXT_PATH}/`;
+}
+
 export function getApiBaseUrl(withAccessToken?: boolean): string {
     const apiContextPath = withAccessToken ? API_CONTEXT_PATH_WITH_ACCESS_TOKEN : API_CONTEXT_PATH;
 
     if (erLocalhost()) {
-        const mode = LocalHostMode.LOCALHOST_MOCK_PROFIL; // Velg modus her!
-
-        if (mode == LocalHostMode.LOCALHOST_MOCK_PROFIL) {
-            // Kjør mot lokal sosialhjelp-soknad-api:
-            return `http://localhost:8181/${API_CONTEXT_PATH}/`;
-        }
-
-        if (mode == LocalHostMode.LOCALHOST_MOCK_ALT_PROFIL) {
-            // Kjør mot lokal sosialhjelp-soknad-api og mock-alt-api (som login-api):
-            if (withAccessToken) return `http://localhost:8989/sosialhjelp/mock-alt-api/login-api/${API_CONTEXT_PATH}/`;
-            else return `http://localhost:8181/${API_CONTEXT_PATH}/`;
-        }
-
-        if (mode == LocalHostMode.LOCALHOST_LOKAL_LOGIN_API) {
-            // Kjør med login-api som proxy om en ønsker access_token fra idporten
-            const apiPort = withAccessToken ? 7000 : 8181;
-            return `http://localhost:${apiPort}/${apiContextPath}/`;
-        }
+        return getLocalhostApiBaseUrl(withAccessToken, apiContextPath);
     }
     if (window.location.origin.indexOf("nais.oera") >= 0) {
         return window.location.origin.replace(`${CONTEXT_PATH}`, `${API_CONTEXT_PATH}`) + `/${apiContextPath}/`;
