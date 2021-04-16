c259ad03d9f5c074c1418b867585c67a6c1fb3e1
Per-Christian Nielsen <55442993+perchrnie@users.noreply.github.com>
2021-04-16 12:54:40 +0200

    Comment out mock login (#784)
    * Redirect isteden for mock-login.
    
    * Men ikke i prod.

--- src/digisos/index.tsx
+++ src/digisos/index.tsx
@@ -3,7 +3,6 @@ import {Redirect, Route, Switch} from "react-router";
 import SideIkkeFunnet from "../nav-soknad/containers/SideIkkeFunnet";
 import Informasjon from "./informasjon";
 import MockBruker from "./mock/mockbruker";
-import MockLogin from "./mock/mocklogin";
 import SkjemaRouter from "./skjema";
 import Ettersendelse from "./skjema/ettersendelse";
 import Link from "./link";
@@ -20,7 +19,11 @@ const App: React.FC = () => {
                 <Route path={`/informasjon`} exact={true} component={Informasjon} />
                 <Route path={`/link`} exact={true} component={Link} />
                 <Route path={`/mock`} exact={true} component={MockBruker} />
-                {erMockMiljoEllerDev() && <Route path={`/mock-login`} exact={true} component={MockLogin} />}
+                {erMockMiljoEllerDev() && (
+                    <Route path={`/mock-login`} exact={true}>
+                        <Redirect to="/informasjon" />
+                    </Route>
+                )}
                 <Route path={`/undersokelse`} exact={true} component={() => <div style={{height: "67vh"}} />} />
                 <Route path={`/skjema/:brukerBehandlingId/:steg`} component={SkjemaRouter} exact={true} />
                 <Route component={SideIkkeFunnet} />
