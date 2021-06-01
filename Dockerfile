60f0e3baa7211ee069dace641b02f9850eaa6f9b
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-06-01 07:58:21 +0200

    Bytter ut pus-decorator med express og serverside-rendret dekoratør (#789)
    Bytter ut pus-decorator med server-side rendret dekoratør

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
-FROM navikt/pus-decorator
-ENV APPLICATION_NAME=sosialhjelp-soknad
-ENV HEADER_TYPE=WITHOUT_MENU
-ENV FOOTER_TYPE=WITHOUT_ALPHABET
-ENV CONTEXT_PATH=/sosialhjelp/soknad/
-COPY /build /app
+FROM navikt/node-express:12.2.0-alpine
+
+ENV NODE_ENV production
+
+WORKDIR /app
+COPY package.json .
+RUN npm i node-fetch node-cache @navikt/nav-dekoratoren-moduler jsdom
+COPY server.js server.js
+COPY build build/
+
+CMD ["node", "./server.js"]
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const {injectDecoratorServerSide} = require("@navikt/nav-dekoratoren-moduler/ssr");
+const path = require("path");
+
+const decoratorParams = {
+    env: process.env.DEKORATOR_MILJO || "prod",
+    simple: true,
+    feedback: false,
+    chatbot: false,
+    shareScreen: false,
+};
+
+const app = express(); // create express app
+app.disable("x-powered-by");
+
+const buildPath = path.resolve(__dirname, "build");
+
+const basePath = "/sosialhjelp/soknad";
+
+app.use(basePath, express.static(buildPath, {index: false}));
+
+app.get(`${basePath}/internal/isAlive|isReady`, (req, res) => res.sendStatus(200));
+
+app.use(basePath, (req, res, next) => {
+    injectDecoratorServerSide({
+        filePath: `${buildPath}/index.html`,
+        ...decoratorParams,
+    })
+        .then((html) => {
+            res.send(html);
+        })
+        .catch((e) => {
+            console.error(`Failed to get decorator: ${e}`);
+            res.status(500).send("Det har oppstått en feil. Venligst prøv igjen senere.");
+        });
+});
+
+// start express server on port 8080
+app.listen(8080, () => {
+    console.log("server started on port 8080");
+});
