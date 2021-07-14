72d21e4eeeb87d8fceb8211af93e404b67082214
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-07-14 14:57:39 +0200

    Flytter logikk for sletting av søknad ut fra redux-saga (#819)

--- src/nav-soknad/components/avbrytsoknad/AvbrytSoknad.tsx
+++ src/nav-soknad/components/avbrytsoknad/AvbrytSoknad.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import NavFrontendModal from "nav-frontend-modal";
 import {Innholdstittel, Normaltekst} from "nav-frontend-typografi";
 import {Hovedknapp, Knapp} from "nav-frontend-knapper";
-import {fortsettSoknad, slettSoknad} from "../../../digisos/redux/soknad/soknadActions";
+import {fortsettSoknad, showServerFeil} from "../../../digisos/redux/soknad/soknadActions";
 import {FormattedMessage, useIntl} from "react-intl";
 import {useDispatch, useSelector} from "react-redux";
 import {navigerTilDittNav} from "../../../digisos/redux/navigasjon/navigasjonActions";
 import {getContextPathForStaticContent} from "../../../configuration";
 import {State} from "../../../digisos/redux/reducers";
 import AlertStripe from "nav-frontend-alertstriper";
+import {fetchDelete, HttpStatus} from "../../utils/rest-utils";
+import {loggAdvarsel} from "../../../digisos/redux/navlogger/navloggerActions";
 
 export const AvbrytSoknad = () => {
     const {behandlingsId, avbrytDialog, nedetid} = useSelector((state: State) => state.soknad);
@@ -19,7 +21,15 @@ export const AvbrytSoknad = () => {
 
     const onAvbryt = () => {
         if (behandlingsId) {
-            dispatch(slettSoknad(behandlingsId));
+            fetchDelete(`soknader/${behandlingsId}`)
+                .then(() => dispatch(navigerTilDittNav()))
+                .catch((reason) => {
+                    if (reason.message === HttpStatus.UNAUTHORIZED) {
+                        return;
+                    }
+                    dispatch(loggAdvarsel("slett soknad saga feilet: " + reason));
+                    dispatch(showServerFeil(true));
+                });
         }
     };
 
