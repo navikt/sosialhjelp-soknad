88c0f2ccf899d4b4abd933842270c4d8334c933f
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-07-15 10:56:42 +0200

    Skriver om oppsummering til function component og trekker data fetching ut av saga

--- src/digisos/redux/oppsummering/oppsummeringSaga.ts
+++ src/digisos/redux/oppsummering/oppsummeringSaga.ts
@@ -1,21 +1,8 @@
 import {call, put, select, takeEvery} from "redux-saga/effects";
 import {NyOppsummeringResponse, OppsummeringActionTypeKeys} from "./oppsummeringTypes";
-import {fetchOppsummering, HttpStatus, fetchToJson} from "../../../nav-soknad/utils/rest-utils";
-import {hentOppsumeringFeilet, setOppsumering, setNyOppsummering} from "./oppsummeringActions";
+import {HttpStatus, fetchToJson} from "../../../nav-soknad/utils/rest-utils";
+import {hentOppsumeringFeilet, setNyOppsummering} from "./oppsummeringActions";
 import {State} from "../reducers";
-
-function* hentOppsummeringSaga() {
-    try {
-        const behandlingsID: string | undefined = yield select((state: State) => state.soknad.behandlingsId);
-        const response: string = yield call(fetchOppsummering, `soknader/${behandlingsID}/`);
-        yield put(setOppsumering(response));
-    } catch (reason) {
-        if (reason.message === HttpStatus.UNAUTHORIZED) {
-            return;
-        }
-        yield put(hentOppsumeringFeilet(reason));
-    }
-}
 
 function* hentNyOppsummeringSaga() {
     try {
@@ -31,10 +18,7 @@ function* hentNyOppsummeringSaga() {
 }
 
 function* oppsummeringSaga() {
-    yield takeEvery(OppsummeringActionTypeKeys.HENT_OPPSUMMERING, hentOppsummeringSaga);
     yield takeEvery(OppsummeringActionTypeKeys.HENT_NY_OPPSUMMERING, hentNyOppsummeringSaga);
 }
-
-export {hentOppsummeringSaga};
 
 export default oppsummeringSaga;
