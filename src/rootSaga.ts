9f630bda863e330c660c6a6315f4d10c9856d4a5
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-07-15 11:17:47 +0200

    Skriver om ny oppsummeringsside og sletter saga

--- src/rootSaga.ts
+++ src/rootSaga.ts
@@ -2,19 +2,10 @@ import {all} from "redux-saga/effects";
 import navigasjonSaga from "./digisos/redux/navigasjon/navigasjonSaga";
 import soknadSaga from "./digisos/redux/soknad/soknadSaga";
 import navloggerSaga from "./digisos/redux/navlogger/navloggerSaga";
-import oppsummeringSaga from "./digisos/redux/oppsummering/oppsummeringSaga";
 import ettersendelseSaga from "./digisos/redux/ettersendelse/ettersendelseSaga";
 import filSaga from "./digisos/redux/fil/filSaga";
 import opplysningerSaga from "./digisos/redux/okonomiskeOpplysninger/opplysningerSaga";
 
 export default function* rootSaga() {
-    yield all([
-        navigasjonSaga(),
-        navloggerSaga(),
-        oppsummeringSaga(),
-        soknadSaga(),
-        filSaga(),
-        opplysningerSaga(),
-        ettersendelseSaga(),
-    ]);
+    yield all([navigasjonSaga(), navloggerSaga(), soknadSaga(), filSaga(), opplysningerSaga(), ettersendelseSaga()]);
 }
