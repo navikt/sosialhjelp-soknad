b83c7d8ba029a4236da677856d38d3d268d3b1cd
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-07-28 15:40:02 +0200

    Skriver om hotjar komponent til typescript (#834)

---
+++ src/nav-soknad/components/hotjarTrigger/HotjarTrigger.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {erMockMiljoEllerDev} from "../../utils";
+import {useEffect} from "react";
+
+export const HotjarTrigger = (props: {hotjarTrigger: string}) => {
+    useEffect(() => {
+        // hotjar ligger ikke på window som default
+        // @ts-ignore
+        if (typeof window.hj === "function" && !erMockMiljoEllerDev()) {
+            // @ts-ignore
+            window.hj("trigger", props.hotjarTrigger);
+        }
+    }, [props.hotjarTrigger]);
+
+    return <div />;
+};
+
+export const SoknadEttersendelseFeilerHotjarTrigger = () => (
+    <HotjarTrigger hotjarTrigger="digisos_ettersende_gammel_soknad_feiler" />
+);
+
+export const DigisosGammelEttersendelseHotjarTrigger = () => (
+    <HotjarTrigger hotjarTrigger="digisos_ettersende_gammel_soknad" />
+);
