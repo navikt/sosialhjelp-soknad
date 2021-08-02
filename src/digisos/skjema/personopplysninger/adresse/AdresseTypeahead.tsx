998707ff8d68342f35d86c7e2ed63d2322e60836
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-08-02 10:41:58 +0200

    Legger til debounce på fetch-kallet slik at vi ikke kaller backend ved hvert tastetrykk

--- src/digisos/skjema/personopplysninger/adresse/AdresseTypeahead.tsx
+++ src/digisos/skjema/personopplysninger/adresse/AdresseTypeahead.tsx
@@ -1,7 +1,8 @@
 import Downshift from "downshift";
 import {Input} from "nav-frontend-skjema";
 import NavFrontendSpinner from "nav-frontend-spinner";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+import {useDebounce} from "react-use";
 import styled from "styled-components";
 import {erLocalhost, fetchToJson} from "../../../../nav-soknad/utils/rest-utils";
 import {AdressesokTreff} from "./AdresseTypes";
@@ -9,91 +10,10 @@ import {formaterAdresseString, removeDuplicatesAfterTransform} from "./AdresseUt
 
 const json = `[{"adresse":"Trondheimsveien","husnummer":"1","husbokstav":null,"kommunenummer":"5055","kommunenavn":"Heim","postnummer":"7200","poststed":"KYRKSÆTERØRA","geografiskTilknytning":"5055","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen","husnummer":"1","husbokstav":null,"kommunenummer":"3035","kommunenavn":"Eidsvoll","postnummer":"2072","poststed":"DAL","geografiskTilknytning":"3035","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen","husnummer":"1","husbokstav":null,"kommunenummer":"3420","kommunenavn":"Elverum","postnummer":"2406","poststed":"ELVERUM","geografiskTilknytning":"3420","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen","husnummer":"1","husbokstav":null,"kommunenummer":"3431","kommunenavn":"Dovre","postnummer":"2660","poststed":"DOMBÅS","geografiskTilknytning":"3431","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen - Kløfta","husnummer":"1","husbokstav":null,"kommunenummer":"3033","kommunenavn":"Ullensaker","postnummer":"2040","poststed":"KLØFTA","geografiskTilknytning":"3033","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"1","husbokstav":null,"kommunenummer":"3031","kommunenavn":"Nittedal","postnummer":"1481","poststed":"HAGAN","geografiskTilknytning":"3031","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"10","husbokstav":"B","kommunenummer":"0301","kommunenavn":"Oslo","postnummer":"0560","poststed":"OSLO","geografiskTilknytning":"030102","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen","husnummer":"10","husbokstav":null,"kommunenummer":"3431","kommunenavn":"Dovre","postnummer":"2660","poststed":"DOMBÅS","geografiskTilknytning":"3431","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen - Kløfta","husnummer":"10","husbokstav":null,"kommunenummer":"3033","kommunenavn":"Ullensaker","postnummer":"2040","poststed":"KLØFTA","geografiskTilknytning":"3033","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"10","husbokstav":"A","kommunenummer":"3030","kommunenavn":"Lillestrøm","postnummer":"2013","poststed":"SKJETTEN","geografiskTilknytning":"3030","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"10","husbokstav":"C","kommunenummer":"0301","kommunenavn":"Oslo","postnummer":"0560","poststed":"OSLO","geografiskTilknytning":"030102","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"10","husbokstav":"B","kommunenummer":"3030","kommunenavn":"Lillestrøm","postnummer":"2013","poststed":"SKJETTEN","geografiskTilknytning":"3030","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"10","husbokstav":"A","kommunenummer":"0301","kommunenavn":"Oslo","postnummer":"0560","poststed":"OSLO","geografiskTilknytning":"030102","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"10","husbokstav":"D","kommunenummer":"0301","kommunenavn":"Oslo","postnummer":"0560","poststed":"OSLO","geografiskTilknytning":"030102","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen - Kløfta","husnummer":"100","husbokstav":null,"kommunenummer":"3033","kommunenavn":"Ullensaker","postnummer":"2040","poststed":"KLØFTA","geografiskTilknytning":"3033","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen - Jessheim","husnummer":"100","husbokstav":"A","kommunenummer":"3033","kommunenavn":"Ullensaker","postnummer":"2050","poststed":"JESSHEIM","geografiskTilknytning":"3033","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"100","husbokstav":null,"kommunenummer":"0301","kommunenavn":"Oslo","postnummer":"0565","poststed":"OSLO","geografiskTilknytning":"030102","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen - Jessheim","husnummer":"100","husbokstav":"B","kommunenummer":"3033","kommunenavn":"Ullensaker","postnummer":"2050","poststed":"JESSHEIM","geografiskTilknytning":"3033","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen - Jessheim","husnummer":"100","husbokstav":"C","kommunenummer":"3033","kommunenavn":"Ullensaker","postnummer":"2050","poststed":"JESSHEIM","geografiskTilknytning":"3033","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"101","husbokstav":null,"kommunenummer":"0301","kommunenavn":"Oslo","postnummer":"0565","poststed":"OSLO","geografiskTilknytning":"030102","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen","husnummer":"101","husbokstav":null,"kommunenummer":"3035","kommunenavn":"Eidsvoll","postnummer":"2072","poststed":"DAL","geografiskTilknytning":"3035","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen - Jessheim","husnummer":"1010","husbokstav":null,"kommunenummer":"3033","kommunenavn":"Ullensaker","postnummer":"2054","poststed":"MOGREINA","geografiskTilknytning":"3033","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen","husnummer":"1013","husbokstav":null,"kommunenummer":"3420","kommunenavn":"Elverum","postnummer":"2406","poststed":"ELVERUM","geografiskTilknytning":"3420","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen","husnummer":"1015","husbokstav":null,"kommunenummer":"3420","kommunenavn":"Elverum","postnummer":"2406","poststed":"ELVERUM","geografiskTilknytning":"3420","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen","husnummer":"1019","husbokstav":null,"kommunenummer":"3420","kommunenavn":"Elverum","postnummer":"2406","poststed":"ELVERUM","geografiskTilknytning":"3420","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen - Jessheim","husnummer":"102","husbokstav":"B","kommunenummer":"3033","kommunenavn":"Ullensaker","postnummer":"2050","poststed":"JESSHEIM","geografiskTilknytning":"3033","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"102","husbokstav":"B","kommunenummer":"0301","kommunenavn":"Oslo","postnummer":"0565","poststed":"OSLO","geografiskTilknytning":"030102","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"102","husbokstav":"A","kommunenummer":"0301","kommunenavn":"Oslo","postnummer":"0565","poststed":"OSLO","geografiskTilknytning":"030102","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsvegen - Jessheim","husnummer":"102","husbokstav":"A","kommunenummer":"3033","kommunenavn":"Ullensaker","postnummer":"2050","poststed":"JESSHEIM","geografiskTilknytning":"3033","gatekode":null,"bydel":null,"type":"gateadresse"},{"adresse":"Trondheimsveien","husnummer":"102","husbokstav":"C","kommunenummer":"0301","kommunenavn":"Oslo","postnummer":"0565","poststed":"OSLO","geografiskTilknytning":"030102","gatekode":null,"bydel":null,"type":"gateadresse"}]`;
 
-/*const mockdata: AdressesokTreff[] = [
-    {
-        adresse: "Sannergata",
-        husnummer: "2",
-        kommunenavn: "Oslo",
-        postnummer: "0101",
-        husbokstav: null,
-        kommunenummer: "0301",
-        poststed: "OSLO",
-        geografiskTilknytning: null,
-        gatekode: null,
-        bydel: null,
-        type: "gateadresse",
-    },
-    {
-        adresse: "Fyrstikkalleen",
-        husnummer: "1",
-        kommunenavn: "Oslo",
-        postnummer: "0101",
-        husbokstav: null,
-        kommunenummer: null,
-        poststed: null,
-        geografiskTilknytning: null,
-        gatekode: null,
-        bydel: null,
-        type: null,
-    },
-    {
-        adresse: "Trondheimsveien",
-        husnummer: "1",
-        kommunenavn: "Oslo",
-        postnummer: "0101",
-        husbokstav: null,
-        kommunenummer: null,
-        poststed: null,
-        geografiskTilknytning: null,
-        gatekode: null,
-        bydel: null,
-        type: null,
-    },
-    {
-        adresse: "Trondheimsveien",
-        husnummer: "2",
-        kommunenavn: "Oslo",
-        postnummer: "0101",
-        husbokstav: null,
-        kommunenummer: null,
-        poststed: null,
-        geografiskTilknytning: null,
-        gatekode: null,
-        bydel: null,
-        type: null,
-    },
-    {
-        adresse: "Trondheimsveien",
-        husnummer: "3",
-        kommunenavn: "Oslo",
-        postnummer: "0101",
-        husbokstav: null,
-        kommunenummer: null,
-        poststed: null,
-        geografiskTilknytning: null,
-        gatekode: null,
-        bydel: null,
-        type: null,
-    },
-    {
-        adresse: "Trondheimsveien",
-        husnummer: "4",
-        kommunenavn: "Oslo",
-        postnummer: "0101",
-        husbokstav: null,
-        kommunenummer: null,
-        poststed: null,
-        geografiskTilknytning: null,
-        gatekode: null,
-        bydel: null,
-        type: null,
-    },
-];*/
-
 export const mockFetch = async (input: string): Promise<AdressesokTreff[]> => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    return await Promise.resolve(
+    return Promise.resolve(
         JSON.parse(json).filter((data: AdressesokTreff) =>
             formaterAdresseString(data)?.toLocaleLowerCase().includes(input.toLowerCase())
         )
@@ -105,9 +25,10 @@ const searchForAddress = async (value: string): Promise<AdressesokTreff[]> => {
         const adresser = erLocalhost()
             ? await mockFetch(value)
             : await fetchToJson<AdressesokTreff[]>("informasjon/adressesok?sokestreng=" + encodeURI(value));
-        return await Promise.resolve(removeDuplicatesAfterTransform(adresser, formaterAdresseString).slice(0, 8));
+        return Promise.resolve(removeDuplicatesAfterTransform(adresser, formaterAdresseString).slice(0, 8));
     } catch (err) {
-        return await Promise.resolve([]);
+        // TODO: Bedre feilhåndtering her. Hvilke feilmeldinger trenger vi, og hvordan skal de vises?
+        return Promise.resolve([]);
     }
 };
 
@@ -116,23 +37,32 @@ interface FetchAddressProps {
     children(state: {isLoading: boolean; isError: boolean; result: AdressesokTreff[]}): JSX.Element;
 }
 
+const DEBOUNCE_TIMEOUT_MS = 400;
+
 const FetchAddress = (props: FetchAddressProps) => {
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
     const [result, setResult] = useState<AdressesokTreff[]>([]);
 
-    useEffect(() => {
-        setIsLoading(true);
-        setIsError(false);
-        searchForAddress(props.searchvalue ?? "")
-            .then((res) => {
-                setResult(res);
-                setIsLoading(false);
-            })
-            .catch((err) => {
-                setIsError(true);
-            });
-    }, [props.searchvalue]);
+    useDebounce(
+        () => {
+            setIsLoading(true);
+            setIsError(false);
+            setResult([]);
+            searchForAddress(props.searchvalue ?? "")
+                .then((res) => {
+                    setResult(res);
+                    setIsLoading(false);
+                })
+                .catch((err) => {
+                    setIsLoading(false);
+                    setIsError(true);
+                    setResult([]);
+                });
+        },
+        DEBOUNCE_TIMEOUT_MS,
+        [props.searchvalue]
+    );
 
     return props.children({isLoading, isError, result});
 };
@@ -181,7 +111,6 @@ export const AdresseTypeahead = (props: {
 }) => {
     const onSelect = (adresse?: AdressesokTreff) => {
         if (adresse === undefined) {
-            console.log("Nullstiller");
             props.onNullstill();
         } else {
             props.onVelgAnnenAdresse(adresse);
@@ -201,7 +130,7 @@ export const AdresseTypeahead = (props: {
                     <StyledInput {...getInputProps()} />
                     {isOpen && (
                         <FetchAddress searchvalue={inputValue}>
-                            {({isLoading, result}) => (
+                            {({isLoading, isError, result}) => (
                                 <>
                                     {isLoading && (
                                         <SelectMenu>
@@ -210,6 +139,7 @@ export const AdresseTypeahead = (props: {
                                             </Item>
                                         </SelectMenu>
                                     )}
+                                    {isError && <p>Det har oppstått en feil :(</p>}
                                     {result.length > 0 && (
                                         <SelectMenu {...getMenuProps()}>
                                             {result.map((adresse: AdressesokTreff, index: number) => {
