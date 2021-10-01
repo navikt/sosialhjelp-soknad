e86b9d755265af3ebcc50301366f5072ff3dbc73
Halvor Bjørn <halvor.grizzly.bjorn@nav.no>
2021-10-01 15:24:04 +0200

    Formaterer dato

--- src/digisos/skjema/ny-oppsummering/question/FormattedText.tsx
+++ src/digisos/skjema/ny-oppsummering/question/FormattedText.tsx
@@ -1,5 +1,6 @@
 import {BodyShort} from "@navikt/ds-react";
 import {useIntl} from "react-intl";
+import {formatDato} from "../../../../nav-soknad/utils/intlUtils";
 
 export const FormattedText = (props: {value: string; type: string; label?: string; spacing?: boolean}) => {
     const intl = useIntl();
@@ -8,7 +9,7 @@ export const FormattedText = (props: {value: string; type: string; label?: strin
             {props.label && <>{intl.formatMessage({id: props.label})}: </>}
             {props.type === "TEKST" && props.value}
             {props.type === "LOCALE_TEKST" && intl.formatMessage({id: props.value})}
-            {props.type === "DATO" && props.value}
+            {props.type === "DATO" && formatDato(props.value)}
         </BodyShort>
     );
 };
