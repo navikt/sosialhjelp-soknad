5055c9c44fb6230febab93d9d8ad085bd5031162
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-03-31 14:20:12 +0200

    Innføre spennende kompleksitet i typesystemet If it's worth engineering, it's worth overengineering

--- src/digisos/skjema/bosituasjon/Botype.tsx
+++ src/digisos/skjema/bosituasjon/Botype.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import Underskjema from "../../../nav-soknad/components/underskjema";
-import {Bosituasjonsvalg, BosituasjonAnnetvalg} from "./bosituasjonTypes";
+import {BotypeListe, BotypePrimaerValg, BotypeSekundaerValg} from "./bosituasjonTypes";
 import {useBosituasjon} from "./useBosituasjon";
-import {RadioPanelGruppe, SkjemaGruppe} from "nav-frontend-skjema";
+import {RadioPanelGruppe, RadioPanelProps, SkjemaGruppe} from "nav-frontend-skjema";
 import {FormattedMessage, useIntl} from "react-intl";
 import styled from "styled-components";
 
@@ -12,6 +12,7 @@ interface BotypeProps {
 
 const StyledSkjemaGruppe = styled(SkjemaGruppe)`
     padding: 0rem 2rem;
+    margin-bottom: 5rem;
 `;
 
 const Botype = ({behandlingsId}: BotypeProps) => {
@@ -19,39 +20,35 @@ const Botype = ({behandlingsId}: BotypeProps) => {
     const {bosituasjon, setBosituasjon} = useBosituasjon(behandlingsId);
 
     // Hjelpefunksjon: Vis kun undermenyen dersom ikke "eier", "leier", "kommunal" eller "ingen" er valgt
-    const showBosituasjonSubmenu = () =>
-        ![Bosituasjonsvalg.eier, Bosituasjonsvalg.leier, Bosituasjonsvalg.kommunal, Bosituasjonsvalg.ingen].includes(
-            bosituasjon.botype as Bosituasjonsvalg
-        );
+    const showBosituasjonSubmenu = () => !["eier", "leier", "kommunal", "ingen", null].includes(bosituasjon.botype);
+
+    // Hjelpefunksjon: Generer RadioPanelProps fra BotypeListe
+    const radiosFromBotyper = (botypeListe: BotypeListe): RadioPanelProps[] =>
+        Object.entries(botypeListe).map(([name, descriptor]) => ({
+            label: intl.formatMessage(descriptor.messageDescriptor),
+            id: name,
+            value: name,
+            checked: bosituasjon.botype === name,
+        }));
 
     return (
         <StyledSkjemaGruppe>
             <RadioPanelGruppe
                 legend={<FormattedMessage id={"bosituasjon.sporsmal"} defaultMessage={"Hvordan bor du?"} />}
-                radios={Object.keys(Bosituasjonsvalg).map((id) => ({
-                    label: intl.formatMessage({id: `bosituasjon.${id}`}),
-                    id: id,
-                    value: id,
-                    checked: bosituasjon.botype === id,
-                }))}
+                radios={radiosFromBotyper(BotypePrimaerValg)}
                 name={"bosituasjon"}
-                onChange={async (e, value) => {
-                    await setBosituasjon({botype: value as Bosituasjonsvalg});
+                onChange={async (e, botype) => {
+                    await setBosituasjon({botype});
                 }}
             />
 
             <Underskjema visible={showBosituasjonSubmenu()} arrow jaNeiSporsmal>
                 <RadioPanelGruppe
                     legend={<FormattedMessage id={"bosituasjon.sporsmal"} defaultMessage={"Hvordan bor du?"} />}
-                    radios={Object.keys(BosituasjonAnnetvalg).map((id) => ({
-                        label: intl.formatMessage({id: `bosituasjon.annet.botype.${id}`}),
-                        id: id,
-                        value: id,
-                        checked: bosituasjon.botype === id,
-                    }))}
+                    radios={radiosFromBotyper(BotypeSekundaerValg)}
                     name={"bosituasjon.annet.botype"}
-                    onChange={async (e, value) => {
-                        await setBosituasjon({botype: value as BosituasjonAnnetvalg});
+                    onChange={async (e, botype) => {
+                        await setBosituasjon({botype});
                     }}
                 />
             </Underskjema>
--- src/digisos/skjema/bosituasjon/bosituasjonTypes.tsx
+++ src/digisos/skjema/bosituasjon/bosituasjonTypes.tsx
@@ -1,5 +1,35 @@
+import {MessageDescriptor} from "react-intl";
+
+export type BotypePrimaer = "eier" | "leier" | "kommunal" | "ingen" | "annet";
+export type BotypePrimaerListe = Record<BotypePrimaer, BotypeDescriptor>;
+
+export type BotypeSekundaer = "foreldre" | "familie" | "venner" | "institusjon" | "fengsel" | "krisesenter";
+export type BotypeSekundaerListe = Record<BotypeSekundaer, BotypeDescriptor>;
+
+export type Botype = BotypePrimaer | BotypeSekundaer;
+export type BotypeListe = BotypePrimaerListe | BotypeSekundaerListe;
+
+export type BotypeDescriptor = {messageDescriptor: MessageDescriptor};
+
+export const BotypePrimaerValg: Record<BotypePrimaer, BotypeDescriptor> = {
+    eier: {messageDescriptor: {id: "botype.eier", defaultMessage: "Jeg bor i bolig jeg eier selv"}},
+    leier: {messageDescriptor: {id: "botype.leier", defaultMessage: "Jeg leier privat bolig"}},
+    kommunal: {messageDescriptor: {id: "botype.kommunal", defaultMessage: "Jeg leier kommunal bolig"}},
+    ingen: {messageDescriptor: {id: "botype.ingen", defaultMessage: "Jeg har ikke noe sted å bo"}},
+    annet: {messageDescriptor: {id: "botype.annet", defaultMessage: "Annen bosituasjon"}},
+};
+
+export const BotypeSekundaerValg: Record<BotypeSekundaer, BotypeDescriptor> = {
+    foreldre: {messageDescriptor: {id: "bosituasjon.annet.botype.foreldre", defaultMessage: "Bor hos foreldre"}},
+    familie: {messageDescriptor: {id: "bosituasjon.annet.botype.familie", defaultMessage: "Bor hos familie"}},
+    venner: {messageDescriptor: {id: "bosituasjon.annet.botype.venner", defaultMessage: "Bor hos venner"}},
+    institusjon: {messageDescriptor: {id: "bosituasjon.annet.botype.institusjon", defaultMessage: "Institusjon"}},
+    fengsel: {messageDescriptor: {id: "bosituasjon.annet.botype.fengsel", defaultMessage: "Fengsel"}},
+    krisesenter: {messageDescriptor: {id: "bosituasjon.annet.botype.krisesenter", defaultMessage: "Krisesenter"}},
+};
+
 export type BosituasjonData = {
-    botype: null | string;
+    botype: Botype | null;
     antallPersoner: null | string;
 };
 
@@ -7,20 +37,3 @@ export const initialBosituasjonState: BosituasjonData = {
     botype: null,
     antallPersoner: null,
 };
-
-export enum Bosituasjonsvalg {
-    eier = "eier",
-    leier = "leier",
-    kommunal = "kommunal",
-    ingen = "ingen",
-    annet = "annet",
-}
-
-export enum BosituasjonAnnetvalg {
-    foreldre = "annet.botype.foreldre",
-    familie = "annet.botype.familie",
-    venner = "annet.botype.venner",
-    institusjon = "annet.botype.institusjon",
-    fengsel = "annet.botype.fengsel",
-    krisesenter = "annet.botype.krisesenter",
-}
