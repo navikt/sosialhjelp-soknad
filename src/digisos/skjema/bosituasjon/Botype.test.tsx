ccb2c2686fe4be99de21adb4aa84d8ae4422a925
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-04-05 16:11:07 +0200

    Make SonarCloud happy about unit tests
    For some reason, SonarLint doesn't flag any errors locally in the file,
    unless I remove all tests. ¯\_(ツ)_/¯

--- src/digisos/skjema/bosituasjon/Botype.test.tsx
+++ src/digisos/skjema/bosituasjon/Botype.test.tsx
@@ -1,4 +1,4 @@
-import {getByRole, waitFor} from "@testing-library/react";
+import {waitFor} from "@testing-library/react";
 import Botype from "./Botype";
 import {setupServer} from "msw/node";
 import {rest} from "msw";
@@ -14,7 +14,7 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 const endpoint = (behandlingsID: string, mockData: Partial<BosituasjonData>) =>
-    rest.get(getApiBaseUrl(true) + soknadsdataUrl(behandlingsID, SoknadsSti.BOSITUASJON), (req, res, ctx) =>
+    rest.get(getApiBaseUrl(true) + soknadsdataUrl(behandlingsID, SoknadsSti.BOSITUASJON), (_req, res, ctx) =>
         res(ctx.json(mockData))
     );
 
@@ -27,6 +27,7 @@ test("Eier is checked if botype = 'eier'", async () => {
     const {getByRole} = render(<Botype behandlingsId={"botypeEier"} />);
 
     await waitFor(() => expect(getByRole("radio", {name: "Jeg bor i bolig jeg eier selv"})).toBeChecked());
+    await waitFor(() => expect(getByRole("radio", {name: "Jeg leier privat bolig"})).not.toBeChecked());
 });
 
 test("Eier is not checked if botype = 'leier'", async () => {
