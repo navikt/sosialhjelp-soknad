dfce0e6d8eb28c616dd8abafd59ee2156ab7acb7
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-04-05 12:29:56 +0200

    Make the validator happy.

--- src/nav-soknad/components/underskjema/index.tsx
+++ src/nav-soknad/components/underskjema/index.tsx
@@ -3,26 +3,26 @@ import classNames from "classnames";
 import {UnmountClosed} from "react-collapse";
 import {logWarning} from "../../utils/loggerUtils";
 
-interface UnderskjemaProps extends React.Props<any> {
+interface UnderskjemaProps {
     visible?: boolean;
     // Show an arrow in the top-left corner, indicating relevance to element above.
     arrow?: boolean;
     stil?: "default" | "system";
     collapsable?: boolean;
-    // FIXME: Temporary kludge to add grey background.
+    // Temporary kludge to add grey background.
     // Given the somewhat terrible name jaNeiSporsmal because I don't know what other side effects the CSS class has.
     jaNeiSporsmal?: boolean;
     children: React.ReactNode;
 }
 
-const Underskjema: React.StatelessComponent<UnderskjemaProps> = ({
+const Underskjema = ({
     visible,
     arrow = true,
     stil = "default",
     collapsable = true,
     children,
     jaNeiSporsmal,
-}) => {
+}: UnderskjemaProps) => {
     const cls = classNames(
         "underskjema",
         `underskjema--${stil}`,
