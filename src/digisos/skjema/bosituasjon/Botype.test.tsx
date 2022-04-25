e578a0debcdf122149e5431fc2e3dbe253b0d867
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-04-25 17:25:13 +0200

    Add unit test for Botype = fengsel

--- src/digisos/skjema/bosituasjon/Botype.test.tsx
+++ src/digisos/skjema/bosituasjon/Botype.test.tsx
@@ -9,23 +9,24 @@ import {BosituasjonData} from "./bosituasjonTypes";
 import {getApiBaseUrl} from "../../../nav-soknad/utils/rest-utils";
 import {render} from "../../../test/test-utils";
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
-const bosituasjonUrl = (behandlingsID: string) =>
-    getApiBaseUrl(true) + soknadsdataUrl(behandlingsID, SoknadsSti.BOSITUASJON);
-
 const mockGET = <T,>(url: string, mockData: Partial<T>) => {
     return rest.get(url, (_req, res, ctx) => res(ctx.json(mockData)));
 };
+
+const bosituasjonUrl = (behandlingsID: string) =>
+    getApiBaseUrl(true) + soknadsdataUrl(behandlingsID, SoknadsSti.BOSITUASJON);
 
 const server = setupServer(
     mockGET<BosituasjonData>(bosituasjonUrl("botypeEier"), {botype: "eier", antallPersoner: null}),
     mockGET<BosituasjonData>(bosituasjonUrl("botypeLeier"), {botype: "leier", antallPersoner: null}),
     mockGET<BosituasjonData>(bosituasjonUrl("botypeAnnet"), {botype: "annet", antallPersoner: null}),
+    mockGET<BosituasjonData>(bosituasjonUrl("botypeFengsel"), {botype: "fengsel", antallPersoner: null}),
     mockGET<BosituasjonData>(bosituasjonUrl("ingenBotype"), {botype: null, antallPersoner: null})
 );
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 test("Eier is checked if botype = 'eier'", async () => {
     const {getByRole} = render(<Botype behandlingsId={"botypeEier"} />);
@@ -55,6 +56,12 @@ test("Annet submenu not in document if botype = leier", async () => {
 
 test("Annet submenu in document if botype = annet", async () => {
     const {getByText} = render(<Botype behandlingsId={"botypeAnnet"} />);
+
+    await waitFor(() => expect(getByText("Vil du utdype?", {selector: "legend"})).toBeInTheDocument());
+});
+
+test("Annet submenu in document if botype = fengsel", async () => {
+    const {getByText} = render(<Botype behandlingsId={"botypeFengsel"} />);
 
     await waitFor(() => expect(getByText("Vil du utdype?", {selector: "legend"})).toBeInTheDocument());
 });
