27613f60a9a9546dd30d7ff40c26e4b3d8682b94
Martin Tveter <martin.skjelsvik.tveter@nav.no>
2022-09-22 09:33:36 +0200

    rydder litt i readme og erMockAltMiljo()-metode

--- src/nav-soknad/utils/index.ts
+++ src/nav-soknad/utils/index.ts
@@ -18,10 +18,7 @@ export function erMockMiljoEllerDev(): boolean {
 export function erMockAltMiljo(): boolean {
     const url = window.location.href;
     return (
-        url.indexOf("sosialhjelp-soknad-gcp.dev.nav.no") > 0 ||
-        url.indexOf("digisos-gcp.dev.nav.no") > 0 ||
-        url.indexOf("sosialhjelp-soknad-mock.dev.nav.no") > 0 ||
-        url.indexOf("digisos.ekstern.dev.nav.no") > 0 // Fanger ikke opp www-q*.dev.nav.no
+        url.indexOf("sosialhjelp-soknad-mock.dev.nav.no") > 0 || url.indexOf("digisos.ekstern.dev.nav.no") > 0 // Fanger ikke opp www-q*.dev.nav.no
     );
 }
 
