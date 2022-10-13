0c6bc3bb4d1244f042b7c763719cfffae33478f2
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-10-13 15:24:07 +0200

    ReplaceNavKontor har per def aldri fungert, så fjerner
    Vi kan ikke vite hvilket NAV-kontor en bruker har før søknaden er opprettet

--- src/digisos/informasjon/personopplysninger/PersonopplysningerUtfyllendeModal.tsx
+++ src/digisos/informasjon/personopplysninger/PersonopplysningerUtfyllendeModal.tsx
@@ -1,36 +1,23 @@
 import * as React from "react";
 import {FormattedMessage, useIntl} from "react-intl";
 import {useSelector, useDispatch} from "react-redux";
-import {finnValgtEnhetsNavn} from "../../data/kommuner";
-import {Soknadsdata} from "../../redux/soknadsdata/soknadsdataReducer";
 import {State} from "../../redux/reducers";
 import {visSamtykkeInfo} from "../../redux/soknad/soknadActions";
 import {Button, Modal} from "@navikt/ds-react";
 import styled from "styled-components";
-
-export const replaceNavkontor = (text: string, valgtEnhetsNavn?: string) => {
-    // Hvis ikke valgtEnhetsNavn finnes, erstattes søkestrengen med capture-gruppa ([\w\s-]*)
-    // som er 1 eller fler bokstaver, whitespace eller '-', feks "NAV-kontoret ditt" fra søknads-api-tekstene
-    return text.replace(/{navkontor:([\w\s-]*)}/g, valgtEnhetsNavn ? valgtEnhetsNavn : "$1");
-};
-const getText = (soknadsdata: Soknadsdata, text: string) => {
-    const valgtEnhetsNavn = finnValgtEnhetsNavn(soknadsdata);
-    return replaceNavkontor(text, valgtEnhetsNavn);
-};
 
 const CenteredContent = styled.div`
     text-align: center;
 `;
 
 const PersonopplysningerUtfyllendeModal = () => {
-    const soknadsdata = useSelector((state: State) => state.soknadsdata);
     const modalSynlig = useSelector((state: State) => state.soknad.visSamtykkeInfo);
 
     const intl = useIntl();
 
     const dispatch = useDispatch();
 
-    const text = intl.messages["soknadsosialhjelp.forstesiden.bekreftInfoModal.body"].toString();
+    const __html = intl.messages["soknadsosialhjelp.forstesiden.bekreftInfoModal.body"].toString();
     return (
         <Modal
             open={modalSynlig || false}
@@ -40,11 +27,7 @@ const PersonopplysningerUtfyllendeModal = () => {
         >
             <Modal.Content>
                 <div className="personopplysning_info">
-                    <div
-                        dangerouslySetInnerHTML={{
-                            __html: getText(soknadsdata, text),
-                        }}
-                    />
+                    <div dangerouslySetInnerHTML={{__html}} />
                 </div>
 
                 <CenteredContent>
