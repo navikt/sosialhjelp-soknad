5333a43223a53050796827018db4b25a68383b43
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-10-13 15:45:39 +0200

    [chore] Fjern unødvendig oppsplitting

--- src/digisos/informasjon/Informasjon.tsx
+++ src/digisos/informasjon/Informasjon.tsx
@@ -2,17 +2,25 @@ import * as React from "react";
 import {useSelector} from "react-redux";
 import IkkeTilgang from "./feilmld/IkkeTilgang";
 import {State} from "../redux/reducers";
-import {Soknadsoversikt} from "./Soknadsoversikt";
 import {NedetidAlert} from "./feilmld/nedetidAlert";
+import {SkjemaContent} from "../../nav-soknad/components/SkjemaContent";
+import Accordion from "@navikt/ds-react/esm/accordion/Accordion";
+import {NySoknad} from "./oversikt/NySoknad";
+import {PabegynteSoknader} from "./oversikt/PabegynteSoknader";
+import {SendDokumentasjon} from "./oversikt/SendDokumentasjon";
 
 const Informasjon = () => {
     if (!useSelector((state: State) => state.soknad.tilgang?.harTilgang)) return <IkkeTilgang />;
 
     return (
-        <>
+        <SkjemaContent style={{marginTop: "5rem"}}>
             <NedetidAlert />
-            <Soknadsoversikt />
-        </>
+            <Accordion>
+                <NySoknad />
+                <PabegynteSoknader />
+                <SendDokumentasjon />
+            </Accordion>
+        </SkjemaContent>
     );
 };
 
