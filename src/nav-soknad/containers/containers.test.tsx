1df866dd29dcf288b371113e9bf63e62a85c1657
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2022-12-03 01:22:59 +0100

    Utnytt schemavalidering for å rydde i kode

--- src/nav-soknad/containers/containers.test.tsx
+++ src/nav-soknad/containers/containers.test.tsx
@@ -1,50 +1,7 @@
-import {erPaStegEnOgValgtNavEnhetErUgyldig, responseIsOfTypeNavEnhet} from "./containerUtils";
+import {erAktiv, erMidlDeaktivert} from "./containerUtils";
 import {NavEnhet} from "../../digisos/skjema/personopplysninger/adresse/AdresseTypes";
 
-const enUgyldigNavEnhet: NavEnhet = {
-    orgnr: "1234",
-    enhetsnr: "2345",
-    isMottakMidlertidigDeaktivert: true,
-    isMottakDeaktivert: false,
-    enhetsnavn: "asdf",
-    kommunenavn: "fdsa",
-    kommuneNr: "3456",
-    valgt: true,
-};
-
-const enGyldigNavEnhet: NavEnhet = {
-    orgnr: "1234",
-    enhetsnr: "2345",
-    isMottakMidlertidigDeaktivert: false,
-    isMottakDeaktivert: false,
-    enhetsnavn: "asdf",
-    kommunenavn: "fdsa",
-    kommuneNr: "3456",
-    valgt: true,
-};
-
-const ugyldigNavenhetEnhetsnrNull: any = {
-    orgnr: "1234",
-    enhetsnr: null,
-    isMottakMidlertidigDeaktivert: false,
-    isMottakDeaktivert: true,
-    enhetsnavn: "asdf",
-    kommunenavn: "fdsa",
-    kommuneNr: "3456",
-    valgt: true,
-};
-
-test("that erPaStegEnOgValgtNavEnhetErUgyldig gir riktig svar", () => {
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(1, null)).toBe(true);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(2, null)).toBe(false);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(1, enUgyldigNavEnhet)).toBe(true);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(2, enUgyldigNavEnhet)).toBe(false);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(1, enGyldigNavEnhet)).toBe(false);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(2, enGyldigNavEnhet)).toBe(false);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(1, ugyldigNavenhetEnhetsnrNull)).toBe(true);
-});
-
-const gyldigNavEnhet: NavEnhet = {
+const aktivNavEnhet: NavEnhet = {
     orgnr: "12345",
     enhetsnr: "4321",
     isMottakMidlertidigDeaktivert: false,
@@ -55,9 +12,18 @@ const gyldigNavEnhet: NavEnhet = {
     valgt: true,
 };
 
-const undefinedNavEnhet: null = null;
+const deaktivertNavEnhet: NavEnhet = {
+    orgnr: "12345",
+    enhetsnr: "4321",
+    isMottakMidlertidigDeaktivert: false,
+    isMottakDeaktivert: true,
+    enhetsnavn: "Enhetsnavn",
+    kommunenavn: "Kommunenavn",
+    kommuneNr: "15533",
+    valgt: true,
+};
 
-const navEnhetMidlertidigDeaktivert: NavEnhet = {
+const midlertidigDeaktivertNavEnhet: NavEnhet = {
     orgnr: "12345",
     enhetsnr: "4321",
     isMottakMidlertidigDeaktivert: true,
@@ -68,41 +34,17 @@ const navEnhetMidlertidigDeaktivert: NavEnhet = {
     valgt: true,
 };
 
-const navEnhetMedManglendeEnhetsnr: NavEnhet = {
-    orgnr: null,
-    enhetsnr: null,
-    isMottakMidlertidigDeaktivert: true,
-    isMottakDeaktivert: true,
-    enhetsnavn: "Enhetsnavn",
-    kommunenavn: "Kommunenavn",
-    kommuneNr: "15533",
-    valgt: true,
-};
-
-test("that erPaStegEnOgValgtNavEnhetErUgyldig returnerer riktig", () => {
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(1, gyldigNavEnhet)).toBe(false);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(1, undefinedNavEnhet)).toBe(true);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(1, navEnhetMidlertidigDeaktivert)).toBe(true);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(1, navEnhetMedManglendeEnhetsnr)).toBe(true);
-
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(2, gyldigNavEnhet)).toBe(false);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(2, undefinedNavEnhet)).toBe(false);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(2, navEnhetMedManglendeEnhetsnr)).toBe(false);
-    expect(erPaStegEnOgValgtNavEnhetErUgyldig(2, navEnhetMidlertidigDeaktivert)).toBe(false);
+test("erAktiv returnerer true ved aktive NAV-kontorer", () => {
+    expect(erAktiv(aktivNavEnhet)).toBe(true);
 });
 
-const ugyldigResponseUndefined: undefined = undefined;
-const ugyldigResponseNull: null = null;
-const ugyldigResponseString: string = "En random string";
-const ugyldigResponseRandomObject = {bare: "noe", tull: "og", tall: 1234556};
-const ugyldigResponseTomListe: any[] = [];
-const gyldigResponse = gyldigNavEnhet;
+test("erAktiv returnerer false ved alle inaktive NAV-kontorer", () => {
+    expect(erAktiv(deaktivertNavEnhet)).toBe(false);
+    expect(erAktiv(midlertidigDeaktivertNavEnhet)).toBe(false);
+});
 
-test("that responseIsOfTypeListOfNavEnhetAndHasAtleastOneElement ikke fører til nullpointer feil", () => {
-    expect(responseIsOfTypeNavEnhet(ugyldigResponseUndefined)).toBe(false);
-    expect(responseIsOfTypeNavEnhet(ugyldigResponseNull)).toBe(false);
-    expect(responseIsOfTypeNavEnhet(ugyldigResponseString)).toBe(false);
-    expect(responseIsOfTypeNavEnhet(ugyldigResponseRandomObject)).toBe(false);
-    expect(responseIsOfTypeNavEnhet(ugyldigResponseTomListe)).toBe(false);
-    expect(responseIsOfTypeNavEnhet(gyldigResponse)).toBe(true);
+test("erMidlDeaktivert returnerer true kun ved midlertidig deaktiverte", () => {
+    expect(erMidlDeaktivert(aktivNavEnhet)).toBe(false);
+    expect(erMidlDeaktivert(deaktivertNavEnhet)).toBe(false);
+    expect(erMidlDeaktivert(midlertidigDeaktivertNavEnhet)).toBe(true);
 });
