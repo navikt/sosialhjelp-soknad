2ebc0635291cba003b297f4adae05cee7519f04b
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-01-04 13:06:55 +0100

    Fjern tilsynelatende ubrukt "Toppmeny" (#1072)

--- src/nav-soknad/utils/domUtils.ts
+++ src/nav-soknad/utils/domUtils.ts
@@ -1,17 +1,3 @@
-export function skjulToppMeny(): void {
-    const topnav = document.getElementsByClassName("topnavsection-wrapper");
-    if (topnav && topnav[0]) {
-        topnav[0].classList.add("hidden-topnavsection");
-    }
-}
-
-export function visToppMeny(): void {
-    const topnav = document.getElementsByClassName("topnavsection-wrapper");
-    if (topnav && topnav[0]) {
-        topnav[0].classList.remove("hidden-topnavsection");
-    }
-}
-
 export function erMobilVisning(): boolean {
     const width = Math.max(
         document.body.scrollWidth,
