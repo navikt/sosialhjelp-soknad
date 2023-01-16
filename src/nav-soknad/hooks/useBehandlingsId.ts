4d0e7409df671e5828852b966cfcbf5f648ccea5
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-01-16 12:18:27 +0100

    Lag hook for single source of truth for behandlingsId

--- src/nav-soknad/hooks/useBehandlingsId.ts
+++ src/nav-soknad/hooks/useBehandlingsId.ts
@@ -1,11 +1,13 @@
 import {useParams} from "react-router";
 import {useSoknad} from "../../digisos/redux/soknad/useSoknad";
-import {logWarning} from "../utils/loggerUtils";
+import {logError, logWarning} from "../utils/loggerUtils";
 
 type UrlParams = Record<"behandlingsId", string>;
 
 // get behandlingsId from React Router URL parameters.
 // logs warning if Redux and R-R disagree about behandlingsId
+// Draft status.
+// TODO: Fremdeles uavklart: Kan idFraUrl være null/undefined?
 export const useBehandlingsId = () => {
     const {behandlingsId: idFraUrl} = useParams<UrlParams>();
     const {behandlingsId: idFraRedux} = useSoknad();
@@ -13,6 +15,10 @@ export const useBehandlingsId = () => {
     // This should never happen - so we want to know if it does
     if (idFraUrl && idFraRedux && idFraUrl !== idFraRedux) {
         logWarning(`idFraUrl (${idFraUrl}) != idFraRedux (${idFraRedux}) i useBehandlingsId!`);
+    }
+
+    if (!idFraUrl) {
+        logError("useBehandlingsId er invokert, men idFraUrl == false");
     }
 
     return idFraUrl;
