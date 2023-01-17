cef4fb002a6672c77a34feb75c3c4cc66d046d91
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-01-17 11:42:39 +0100

    Legg til bakoverkompatibilitet for Redux i Adresse

--- src/digisos/skjema/personopplysninger/adresse/Adresse.tsx
+++ src/digisos/skjema/personopplysninger/adresse/Adresse.tsx
@@ -16,6 +16,10 @@ import {AdresseSok} from "./AdresseSok";
 import {AdresseFrontend, AdresserFrontend, AdresserFrontendValg} from "../../../../generated/model";
 import cx from "classnames";
 import {useErrorHandler} from "../../../../lib/hooks/useErrorHandler";
+import {oppdaterSoknadsdataSti, SoknadsSti} from "../../../redux/soknadsdata/soknadsdataReducer";
+import {useDispatch} from "react-redux";
+import {Adresser, NavEnhet} from "./AdresseTypes";
+import {clearValideringsfeil} from "../../../redux/validering/valideringActions";
 
 const HorizontalRadioGroup = styled(RadioGroup)`
     .navds-radio {
@@ -31,26 +35,30 @@ const AdresseView = () => {
     const behandlingsId = useBehandlingsId();
     const {request, refetch} = useAlgebraic(useHentAdresser(behandlingsId));
     const errorHandler = useErrorHandler();
+    const dispatch = useDispatch();
 
-    // Because PUTing type = "soknad" and soknad = null yields an error,
-    // we use this to indicate whether we want to show the search box.
-    const [showSearchAnyway, setShowSearchAnyway] = useState<boolean>(false);
+    const [uncommittedAdressevalg, setUncommittedAdressevalg] = useState<AdresserFrontendValg | null>(null);
     const intl = useIntl();
 
     const setAdresser = async (adresser: AdresserFrontend, valg: AdresserFrontendValg, soknad?: AdresseFrontend) => {
-        await updateAdresse(behandlingsId, {
+        const inputAdresser = {
             ...{...adresser, navEnhet: undefined},
             valg: soknad ? "soknad" : valg,
             soknad,
-        });
+        };
+        const navEnheter = (await updateAdresse(behandlingsId, inputAdresser)) as unknown as NavEnhet[];
+        dispatch(oppdaterSoknadsdataSti(SoknadsSti.VALGT_NAV_ENHET, navEnheter[0] ?? null));
+        dispatch(oppdaterSoknadsdataSti(SoknadsSti.NAV_ENHETER, navEnheter));
+        dispatch(oppdaterSoknadsdataSti(SoknadsSti.ADRESSER, inputAdresser as unknown as Adresser));
+        dispatch(clearValideringsfeil("soknadsmottaker"));
         await refetch();
     };
 
     return request.match({
         NotAsked: () => null,
         Loading: () => <TextPlaceholder lines={3} />,
-        Done: (response) =>
-            response.match({
+        Done: (response) => {
+            return response.match({
                 Error: errorHandler,
                 Ok: (adresser) => (
                     <Sporsmal
@@ -62,14 +70,10 @@ const AdresseView = () => {
                         <div className={"mb-4"}>
                             <HorizontalRadioGroup
                                 legend={getIntlText(intl, `soknadsmottaker.infotekst.tekst`)}
-                                value={!showSearchAnyway ? adresser.valg : "soknad"}
+                                value={uncommittedAdressevalg}
                                 onChange={async (valg) => {
-                                    if (valg === "soknad") {
-                                        setShowSearchAnyway(true);
-                                    } else {
-                                        setShowSearchAnyway(false);
-                                        await setAdresser(adresser, valg);
-                                    }
+                                    setUncommittedAdressevalg(valg);
+                                    if (valg !== "soknad") await setAdresser(adresser, valg);
                                 }}
                             >
                                 <Radio value={"folkeregistrert"}>
@@ -83,7 +87,7 @@ const AdresseView = () => {
                                 <Radio value={"soknad"}>
                                     <FormattedMessage id="kontakt.system.oppholdsadresse.valg.soknad" />
                                 </Radio>
-                                {(adresser.valg === "soknad" || showSearchAnyway) && (
+                                {uncommittedAdressevalg === "soknad" && (
                                     <AdresseSok
                                         defaultValue={formaterSoknadsadresse(adresser.soknad?.gateadresse)}
                                         onChange={async (soknad) => setAdresser(adresser, "soknad", soknad)}
@@ -94,7 +98,8 @@ const AdresseView = () => {
                         <SoknadsmottakerInfo navEnhet={adresser.navEnhet} />
                     </Sporsmal>
                 ),
-            }),
+            });
+        },
     });
 };
 
