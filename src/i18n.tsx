1b472b94eca76bc17df9d825babfbb5e3e1fba7b
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-01-18 15:47:04 +0100

    Endre i18next-config så ikke t() returnerer null
    ref: https://www.i18next.com/overview/typescript#argument-of-type-defaulttfuncreturn-is-not-assignable-to-parameter-of-type-xyz

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -17,6 +17,7 @@ i18n
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
+        returnNull: false,
         fallbackLng: "nb",
         defaultNS: "skjema",
         debug: isLocalhost(window.location.href),
