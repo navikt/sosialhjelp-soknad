0a805fd10d00f1457f62dfd1718cac5a873c598e
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-01-24 14:44:45 +0100

    Legg kommentar med kontekst for returnNull

--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -1,5 +1,9 @@
 import "i18next";
 
+// Added January 2023 by Tore Sinding Bekkedal
+// For compatibility with legacy design components, we make t() return string,
+// and not string | null. Might be irrelevant at some future point.
+// See also src/i18n.tsx
 declare module "i18next" {
     interface CustomTypeOptions {
         returnNull: false;
