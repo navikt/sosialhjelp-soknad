2f9525cdf09f62714667113b0f30cf360e891f01
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-02-21 12:03:21 +0100

    hack for å bruke gamle oppsummering

--- src/digisos/skjema/ny-oppsummering/Oppsummering.tsx
+++ src/digisos/skjema/ny-oppsummering/Oppsummering.tsx
@@ -52,6 +52,15 @@ const OppsummeringBegrunnelse = () => {
     });
 };
 
+// Midlertidig hack for å fortsatt bruke gamle oppsummering inntil brukertest av ny
+const INTL_KEYS_MAP: Record<string, string> = {
+    "begrunnelse.hva.sporsmal": "begrunnelse.hva.label",
+    "begrunnelse.hvorfor.sporsmal": "begrunnelse.hvorfor.label",
+    "dinsituasjon.studerer.true.grad.sporsmal": "dinsituasjon.studerer.grad.sporsmal",
+    "dinsituasjon.studerer.true.grad.deltid": "dinsituasjon.studerer.grad.deltid",
+    "dinsituasjon.studerer.true.grad.heltid": "dinsituasjon.studerer.grad.heltid",
+};
+const mapChangedIntlKeys = (intlKey: string): string => INTL_KEYS_MAP?.[intlKey] ?? intlKey;
 export const Oppsummering = () => {
     const {viStolerPaaDeg, oppsummeringNavEnhet} = useFeatureFlags();
     const [bekreftet, setBekreftet] = useState<boolean>(false);
@@ -139,7 +148,9 @@ export const Oppsummering = () => {
                                 {avsnitt.sporsmal?.map((sporsmal) => {
                                     return (
                                         <div key={sporsmal.tittel}>
-                                            {sporsmal.tittel && <Label spacing>{t(sporsmal.tittel)} </Label>}
+                                            {sporsmal.tittel && (
+                                                <Label spacing>{t(mapChangedIntlKeys(sporsmal.tittel))} </Label>
+                                            )}
                                             {!sporsmal.erUtfylt && <Warning />}
                                             <SystemData
                                                 felter={sporsmal.felt?.filter((felt) => felt.type === "SYSTEMDATA")}
