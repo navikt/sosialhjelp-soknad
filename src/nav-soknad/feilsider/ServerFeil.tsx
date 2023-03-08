383edb6fc6fc2d079027728f6a703b540fa41ae6
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-03-08 17:48:44 +0100

    Mer design tweaking

--- src/nav-soknad/feilsider/ServerFeil.tsx
+++ src/nav-soknad/feilsider/ServerFeil.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {useTranslation} from "react-i18next";
 import {useTitle} from "../../lib/hooks/useTitle";
 import {SystemError} from "@navikt/ds-icons";
-import {BodyShort, Heading, Link} from "@navikt/ds-react";
+import {BodyShort, Button, Heading, Link} from "@navikt/ds-react";
 import AppHeader from "../components/appHeader/AppHeader";
 import {logError} from "../utils/loggerUtils";
 
@@ -15,49 +15,46 @@ export const ServerFeil = () => {
         <>
             <AppHeader />
 
-            <div className={"text-center text-[var(--a-text-on-danger)] bg-[var(--a-surface-danger)] p-16"}>
-                <div className={"flex justify-center gap-4 rotate-[4deg] pt-4"}>
-                    <SystemError fontSize={"var(--a-font-size-heading-2xlarge)"} />
-                    <Heading level="1" size="xlarge" spacing>
-                        Vi <span className={"inline-block rotate-[-19deg]"}>beklager</span> teknisk{" "}
-                        <span className={"inline-block rotate-[-10deg]"}>feil</span>
-                    </Heading>
+            <div className={" text-[var(--a-text-on-danger-subtle)] px-8 py-12 md:py-24"}>
+                <div className={"grid md:grid-cols-3 gap-8"}>
+                    <div className={"md:ml-auto mt-3"}>
+                        <SystemError fontSize={"96px"} className={"text-[var(--a-surface-danger)]"} />
+                    </div>
+                    <div className={""}>
+                        <Heading level="1" size="xlarge">
+                            Vi beklager!
+                        </Heading>
+                        <Heading level={"2"} size={"large"}>
+                            En teknisk feil har oppstått.{" "}
+                        </Heading>
+                        <Heading level={"3"} size={"medium"}>
+                            Vi anbefaler at du prøver på nytt.
+                        </Heading>
+                        <Button
+                            className="!mb-8 !mt-4"
+                            variant="secondary"
+                            onClick={() => (window.location.href = "/sosialhjelp/soknad")}
+                        >
+                            Til startsiden for søknad
+                        </Button>
+                        <BodyShort spacing>
+                            Hendelsen har blitt loggført og problemet utbedres så fort som mulig.
+                        </BodyShort>
+                    </div>
                 </div>
-                <Heading level={"2"} size={"medium"} spacing>
-                    En feil har oppstått i digital sosialhjelpsøknad
-                </Heading>
             </div>
-            <div className={"bg-[var(--a-surface-danger-subtle)] p-8"}>
-                <div className={"max-w-3xl mx-auto"}>
-                    <Heading level={"2"} size={"medium"} spacing>
-                        Vi anbefaler at du prøver på nytt litt senere, eller kontakter{" "}
-                        <Link className={"!inline"} href={"https://www.nav.no/finnkontor"}>
-                            ditt NAV-kontor
-                        </Link>
-                        .
+            <div className={"bg-[var(--a-surface-action-subtle)] grid md:grid-cols-3 gap-8 p-8"}>
+                <div className={"md:col-start-2"}>
+                    <Heading level={"3"} size={"large"} spacing>
+                        {t("feilside.serverfeil.nodsituasjon.tittel")}
                     </Heading>
-                    <Heading level={"2"} size={"medium"} spacing>
-                        Problemet er blitt loggført og vil utbedres så fort som mulig.
-                    </Heading>
-                    <Heading level={"2"} size={"medium"} spacing>
-                        Du kan{" "}
+                    <BodyShort spacing>
+                        Du kan også{" "}
                         <Link href={"https://www.nav.no/sosialhjelp/sok-papir"} className={"!inline"}>
                             søke om sosialhjelp på papir
                         </Link>{" "}
                         hos ditt NAV-kontor.
-                    </Heading>
-                    <Heading level={"2"} size={"medium"}>
-                        <Link href="https://www.nav.no/person/kontakt-oss/nb/tilbakemeldinger/feil-og-mangler">
-                            Du kan også sende en feilrapport.
-                        </Link>
-                    </Heading>
-                </div>
-            </div>
-            <div className={"bg-[var(--a-surface-action-subtle)] p-8 pb-96"}>
-                <div className={"max-w-3xl mx-auto"}>
-                    <Heading level={"3"} size={"large"} spacing>
-                        {t("feilside.serverfeil.nodsituasjon.tittel")}
-                    </Heading>
+                    </BodyShort>
                     <BodyShort className={"whitespace-pre-line pb-3"} spacing>
                         {t("feilside.serverfeil.nodsituasjon.tekst")}
                     </BodyShort>
@@ -70,8 +67,13 @@ export const ServerFeil = () => {
                         <Link href="https://www.nav.no">Gå til nav.no</Link>
                     </BodyShort>
 
-                    <BodyShort>
+                    <BodyShort spacing>
                         <Link href="https://www.nav.no/minside">Gå til Min side</Link>
+                    </BodyShort>
+                    <BodyShort>
+                        <Link href="https://www.nav.no/person/kontakt-oss/nb/tilbakemeldinger/feil-og-mangler">
+                            Send feilrapport
+                        </Link>
                     </BodyShort>
                 </div>
             </div>
