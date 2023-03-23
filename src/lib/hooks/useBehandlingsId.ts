941f8dd0f5c4031f90d4d69e6096920a951cc9a6
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-03-23 17:13:09 +0100

    Refaktor/dokumenter useBehandlingsId.ts

--- src/lib/hooks/useBehandlingsId.ts
+++ src/lib/hooks/useBehandlingsId.ts
@@ -1,18 +1,27 @@
 import {useParams} from "react-router";
 import {logError} from "../../nav-soknad/utils/loggerUtils";
 
-type UrlParams = Record<"behandlingsId", string>;
+type SoknadUrlParams = Record<"behandlingsId", string | undefined>;
 
-// get behandlingsId from React Router URL parameters.
-// logs warning if Redux and R-R disagree about behandlingsId
-// Draft status.
-// TODO: Fremdeles uavklart: Kan idFraUrl være null/undefined?
-export const useBehandlingsId = () => {
-    const {behandlingsId: idFraUrl} = useParams<UrlParams>();
+/**
+ * Hent behandlingsId fra React Router URL-parametre.
+ *
+ * Jeg anser det som eksperimentelt bekreftet at behandlingsId
+ * ikke kommer inn som undefined her - men jeg beholder sjekken
+ * og feilmeldingen i tilfelle f. eks. den ventede overgangen
+ * til Next.js påvirker dette.
+ *
+ * @returns behandlingsId - primærnøkkel for søknad under behandling
+ */
+export const useBehandlingsId = (): string => {
+    const {behandlingsId} = useParams<SoknadUrlParams>();
 
-    if (!idFraUrl) {
-        logError("useBehandlingsId er invokert, men idFraUrl == false");
-    }
-
-    return idFraUrl!;
+    if (!(typeof behandlingsId === "string")) {
+        logError(`Sannsynligvis fatal feil: useBehandlingsId returnerer ${typeof behandlingsId} ${behandlingsId}.`);
+        // Om vi er her, kan det være et kort blaff med 'undefined' som så blir endret før
+        // det noengang rekker bli brukers problem eller engang en GET, så ikke nødvendigvis
+        // en fatal feil for brukeren. Jeg tror det kan være like godt å dekke inn med en
+        // hjelpsom verdi :)
+        return "NoeGikkGaltIFrontendSeUseBehandlingsIdTs";
+    } else return behandlingsId;
 };
