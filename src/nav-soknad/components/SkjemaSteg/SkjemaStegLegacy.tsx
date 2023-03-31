7aad247107f4e7fbb6876d0415a015e879ef6638
mvbraathen <marius.vorrang.brathen@nav.no>
2023-03-31 13:36:24 +0200

    Add possibility to skip to main content

--- src/nav-soknad/components/SkjemaSteg/SkjemaStegLegacy.tsx
+++ src/nav-soknad/components/SkjemaSteg/SkjemaStegLegacy.tsx
@@ -6,7 +6,7 @@ import {getIntlTextOrKey, scrollToTop} from "../../utils";
 import AppBanner from "../appHeader/AppHeader";
 import {State} from "../../../digisos/redux/reducers";
 import {useTitle} from "../../../lib/hooks/useTitle";
-import {Heading} from "@navikt/ds-react";
+import {Heading, Link} from "@navikt/ds-react";
 import {NedetidPanel} from "../../../components/common/NedetidPanel";
 import {DigisosSkjemaStegKey, SkjemaConfig} from "./digisosSkjema";
 import {SkjemaStegNavStepperLegacy} from "./SkjemaStegNavStepperLegacy";
@@ -19,6 +19,7 @@ import {useHentNedetidInformasjon} from "../../../generated/nedetid-ressurs/nede
 import {NavEnhetInaktiv} from "../../../skjema/01-personalia/adresse/NavEnhet";
 import {useBehandlingsId} from "../../../lib/hooks/useBehandlingsId";
 import {hentXsrfCookie} from "../../../generated/soknad-ressurs/soknad-ressurs";
+import {t} from "i18next";
 
 interface StegMedNavigasjonProps {
     steg: DigisosSkjemaStegKey;
@@ -75,9 +76,12 @@ export const SkjemaStegLegacy = ({skjemaConfig, steg, ikon, children, onSend}: S
 
     return (
         <div className="pb-4 lg:pb-40 bg-digisosGronnBakgrunn">
+            <Link href="#main-content" className="sr-only sr-only-focusable">
+                {t("hoppTilHovedinnhold")}
+            </Link>
             <AppBanner />
             <SkjemaStegNavStepperLegacy skjemaConfig={skjemaConfig} aktivtSteg={steg} onStepChange={gotoPage} />
-            <main>
+            <main id="main-content">
                 <div className={"max-w-3xl mx-auto skjema-steg skjema-content"}>
                     <NedetidPanel varselType={"infoside"} />
                     <Feiloppsummering valideringsfeil={feil} visFeilliste={visValideringsfeil} />
