efd03b2a6c37ad1bbc8a5269316d797e9b15fa2c
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-04-20 14:29:36 +0200

    [hotfix] Fiks dekorator

--- server.js
+++ server.js
@@ -7,16 +7,7 @@ const basePath = "/sosialhjelp/soknad";
 
 const buildPath = path.resolve(__dirname, "build");
 
-console.log(process.env.DEKORATOR_MILJO);
-
-const decoratorParams = {
-    env: process.env.DEKORATOR_MILJO ?? "prod",
-    filePath: `${buildPath}/index.html`,
-    simple: true,
-    feedback: false,
-    chatbot: false,
-    shareScreen: false,
-};
+console.log(`Dekoratormiljø: ${process.env.DEKORATOR_MILJO}`);
 
 const app = express(); // create express app
 app.disable("x-powered-by");
@@ -25,9 +16,16 @@ app.use(basePath, express.static(buildPath, {index: false}));
 
 app.get(`${basePath}/internal/isAlive|isReady`, (_, res) => res.sendStatus(200));
 
-app.use(basePath, (_, res, __) => {
-    injectDecoratorServerSide(decoratorParams)
-        .then(res.send)
+app.use(basePath, (req, res, __) => {
+    injectDecoratorServerSide({
+        env: process.env.DEKORATOR_MILJO ?? "dev",
+        filePath: `${buildPath}/index.html`,
+        simple: true,
+        feedback: false,
+        chatbot: false,
+        shareScreen: false,
+    })
+        .then((text) => res.send(text))
         .catch((e) => {
             console.error(`Failed to get decorator: ${e}`);
             res.status(500).send("Det har oppstått en feil. Venligst prøv igjen senere.");
