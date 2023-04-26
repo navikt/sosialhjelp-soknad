784ef25078d0d0a4327fe3ba468c4de079a5267e
mvbraathen <marius.vorrang.brathen@nav.no>
2023-04-26 12:49:36 +0200

    Update code

--- src/nav-soknad/components/SkjemaSteg/useSkjemaNavigation.ts
+++ src/nav-soknad/components/SkjemaSteg/useSkjemaNavigation.ts
@@ -3,11 +3,18 @@ import {clearAllValideringsfeil, visValideringsfeilPanel} from "../../../digisos
 import {useDispatch, useSelector} from "react-redux";
 import {State} from "../../../digisos/redux/reducers";
 import {useNavigate} from "react-router";
+import {Opplysning} from "../../../digisos/redux/okonomiskeOpplysninger/opplysningerTypes";
 
 export const useSkjemaNavigation = (steg: number) => {
     const {validering} = useSelector((state: State) => state);
     const navigate = useNavigate();
     const dispatch = useDispatch();
+
+    const uploadedDocumentsCount = useSelector((state: State) =>
+        state.okonomiskeOpplysninger.opplysningerSortert
+            .map((opplysning: Opplysning) => opplysning.filer.length)
+            .reduce((total: number, fileCount: number) => total + fileCount)
+    );
 
     const gotoPage = (newPage: number) => {
         if (newPage < steg) {
@@ -16,6 +23,9 @@ export const useSkjemaNavigation = (steg: number) => {
             if (validering.feil.length) {
                 dispatch(visValideringsfeilPanel());
             } else {
+                if (steg === 8 && uploadedDocumentsCount === 0) {
+                    logAmplitudeEvent("skjemasteg 8 - no documents uploaded");
+                }
                 dispatch(clearAllValideringsfeil());
                 logAmplitudeEvent("skjemasteg fullført", {steg});
             }
