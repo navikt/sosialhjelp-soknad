0cc0569073c321381b7822815e1a468f700e01b5
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-05-23 13:28:49 +0200

    Fjerner mer Redux-kode

--- src/rootSaga.ts
+++ src/rootSaga.ts
@@ -1,7 +1,6 @@
 import {all} from "redux-saga/effects";
 import ettersendelseSaga from "./digisos/redux/ettersendelse/ettersendelseSaga";
-import opplysningerSaga from "./digisos/redux/okonomiskeOpplysninger/opplysningerSaga";
 
 export default function* rootSaga() {
-    yield all([opplysningerSaga(), ettersendelseSaga()]);
+    yield all([ettersendelseSaga()]);
 }
--- src/skjema/08-vedlegg/useOpplysninger.tsx
+++ src/skjema/08-vedlegg/useOpplysninger.tsx
@@ -1,5 +1,4 @@
 import {useBehandlingsId} from "../../lib/hooks/useBehandlingsId";
-import {useDispatch} from "react-redux";
 import {useHentOkonomiskeOpplysninger} from "../../generated/okonomiske-opplysninger-ressurs/okonomiske-opplysninger-ressurs";
 import {VedleggFrontends} from "../../generated/model";
 import {
@@ -7,33 +6,20 @@ import {
     VedleggFrontendsMinusEtParTingSomTrengerAvklaring,
     vedleggGrupper,
 } from "../../digisos/redux/okonomiskeOpplysninger/opplysningerConfig";
-import {gotDataFromBackend} from "../../digisos/redux/okonomiskeOpplysninger/opplysningerActions";
 import * as React from "react";
-import {logWarning} from "../../nav-soknad/utils/loggerUtils";
 import {getSortertListeAvOpplysninger} from "../../digisos/redux/okonomiskeOpplysninger/opplysningerUtils";
 import {useMemo} from "react";
 
 export const useOpplysninger = () => {
     const behandlingsId = useBehandlingsId();
 
-    //// Redux-hack for å forsikre oss om at retro-koden fremdeles henter dataene
-    //// Har lært av feilen på barnebidragsiden :)
-    const dispatch = useDispatch();
-
-    const {data, isLoading, isSuccess, isError, error} = useHentOkonomiskeOpplysninger<
+    const {data, isLoading} = useHentOkonomiskeOpplysninger<
         VedleggFrontends | VedleggFrontendsMinusEtParTingSomTrengerAvklaring
     >(behandlingsId, {});
 
     if (data && !validVedleggFrontends(data)) {
         throw new Error(`useOpplysninger mottok ugyldig type ${data} - frontends API ute av synk med Swagger?`);
     }
-
-    //// Redux-hack for å forsikre oss om at retro-koden fremdeles henter dataene
-    //// Har lært av feilen på barnebidragsiden :)
-    React.useEffect(() => {
-        if (isSuccess) dispatch(gotDataFromBackend(data));
-        if (isError) logWarning(`isError i useOpplysninger, error ${error}`);
-    }, [data, dispatch, isSuccess, isError, error]);
 
     const sorterte = useMemo(() => (data ? getSortertListeAvOpplysninger(data) : []), [data]);
 
