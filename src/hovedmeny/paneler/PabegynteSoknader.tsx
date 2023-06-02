20e67803e7e307079ba5321c5a2d157cca2b035d
mvbraathen <marius.vorrang.brathen@nav.no>
2023-06-02 15:40:22 +0200

    Add missing english translations

--- src/hovedmeny/paneler/PabegynteSoknader.tsx
+++ src/hovedmeny/paneler/PabegynteSoknader.tsx
@@ -7,6 +7,8 @@ import {nb} from "date-fns/locale";
 import {basePath} from "../../configuration";
 import {DAYS_BEFORE_DELETION, usePabegynteSoknader} from "../usePabegynteSoknader";
 import TextPlaceholder from "../../nav-soknad/components/animasjoner/placeholder/TextPlaceholder";
+import {useTranslation} from "react-i18next";
+import i18n from "../../i18n";
 
 const PabegyntSoknad = ({
     behandlingsId,
@@ -21,6 +23,7 @@ const PabegyntSoknad = ({
     deleteDate: Date;
     antallPabegynteSoknader: number;
 }) => {
+    const {t} = useTranslation("skjema");
     const onPabegyntSoknadClick = (event: React.SyntheticEvent, href: string) => {
         event.preventDefault();
         logAmplitudeEvent("Klikk på påbegynt søknad", {
@@ -39,9 +42,13 @@ const PabegyntSoknad = ({
             >
                 <LinkPanel.Title className={"flex flex-col lg:flex-row align-center"}>
                     <Label style={{marginRight: "1rem"}}>
-                        Sist oppdatert {format(lastUpdatedDate, "d. MMMM H  HH:mm", {locale: nb})}
+                        {t("applikasjon.paabegynt.soknad.sist.oppdatert")}{" "}
+                        {format(lastUpdatedDate, "d. MMMM H  HH:mm", {locale: i18n.language === "en" ? undefined : nb})}
                     </Label>
-                    <BodyShort>Slettes om {formatDistance(deleteDate, currentDate, {locale: nb})}</BodyShort>
+                    <BodyShort>
+                        {t("applikasjon.paabegynt.soknad.slettes")}{" "}
+                        {formatDistance(deleteDate, currentDate, {locale: i18n.language === "en" ? undefined : nb})}
+                    </BodyShort>
                 </LinkPanel.Title>
             </LinkPanel>
         </li>
@@ -50,6 +57,7 @@ const PabegyntSoknad = ({
 
 const PabegynteSoknaderCount = () => {
     const num = usePabegynteSoknader()?.length;
+    const {t} = useTranslation("skjema");
 
     if (num === undefined) return <TextPlaceholder lines={1} />;
 
@@ -57,13 +65,16 @@ const PabegynteSoknaderCount = () => {
 
     return (
         <span className={"opacity-70 lg:pl-4 font-normal"}>
-            {num === 1 ? `1 påbegynt søknad` : `${num} påbegynte søknader`}
+            {num === 1
+                ? `1 ${t("applikasjon.paabegynt.soknad")}`
+                : `${num} ${t("applikasjon.paabegynt.soknad.flertall")}`}
         </span>
     );
 };
 
 export const PabegynteSoknaderPanel = () => {
     const pabegynteSoknader = usePabegynteSoknader();
+    const {t} = useTranslation("skjema");
 
     if (!pabegynteSoknader?.length) return null;
 
@@ -80,7 +91,7 @@ export const PabegynteSoknaderPanel = () => {
                             <FileContent className={"w-6 h-6"} aria-hidden="true" />
                         </div>
                         <Heading level="2" size="small" className={"flex flex-col lg:flex-row"}>
-                            Fortsett på en søknad du har begynt på
+                            {t("applikasjon.fortsett.soknad")}
                             <PabegynteSoknaderCount />
                         </Heading>
                     </div>
@@ -88,7 +99,9 @@ export const PabegynteSoknaderPanel = () => {
                 <Accordion.Content className={"!px-0 !border-0"}>
                     <div className={"p-8 lg:pl-24"}>
                         <BodyShort className={"pb-4"}>
-                            Vær oppmerksom på at påbegynte søknader slettes etter {DAYS_BEFORE_DELETION} dager.
+                            {t("applikasjon.paabegynt.soknad.informasjon", {
+                                DAYS_BEFORE_DELETION,
+                            })}
                         </BodyShort>
                         <ul className={"space-y-4"}>
                             {pabegynteSoknader?.map(({lastUpdatedDate, deleteDate, behandlingsId}) => (
