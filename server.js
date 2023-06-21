4717030fe29ecdba6ff10935249dabbe7bf6ea30
Line Moseng <line.moseng@nav.no>
2023-06-21 13:57:00 +0200

    Logger docker image ved oppstart

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const basePath = "/sosialhjelp/soknad";
 
 const buildPath = path.resolve(__dirname, "build");
 
+const dockerImage = process.env.IMAGE || process.env.NAIS_APP_IMAGE;
+console.log("starter fra docker image: ", dockerImage);
+
 console.log(`Dekoratormiljø: ${process.env.DEKORATOR_MILJO}`);
 
 const app = express(); // create express app
