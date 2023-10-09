79a86266c9aea2f38cbfbfa2c06a2598514762f7
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-09 17:02:55 +0200

    Ajourfører BehandlingAvPersonopplysningerModal.tsx for ny DS

--- src/hovedmeny/paneler/BehandlingAvPersonopplysningerModal.tsx
+++ src/hovedmeny/paneler/BehandlingAvPersonopplysningerModal.tsx
@@ -5,23 +5,22 @@ import {useTranslation} from "react-i18next";
 const BehandlingAvPersonopplysningerModal = ({open, onClose}: {open: boolean; onClose: () => void}) => {
     const {t} = useTranslation("skjema");
 
-    // FIXME: this is broken!!
     return (
         <Modal open={open} onClose={onClose}>
+            <Modal.Header />
             <Modal.Body>
-                <div className="personopplysning_info">
-                    <div
-                        dangerouslySetInnerHTML={{
-                            __html: t("soknadsosialhjelp.forstesiden.bekreftInfoModal.body"),
-                        }}
-                    />
-                    <div className={"text-center m-8"}>
-                        <Button variant="primary" onClick={() => onClose()}>
-                            {t("soknadsosialhjelp.forstesiden.bekreftInfoModal.lukk")}
-                        </Button>
-                    </div>
-                </div>
+                <div
+                    className="personopplysning_info"
+                    dangerouslySetInnerHTML={{
+                        __html: t("soknadsosialhjelp.forstesiden.bekreftInfoModal.body"),
+                    }}
+                />
             </Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={() => onClose()}>
+                    {t("lukk")}
+                </Button>
+            </Modal.Footer>
         </Modal>
     );
 };
