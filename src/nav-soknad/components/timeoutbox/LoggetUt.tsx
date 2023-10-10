4900e7a35d4f45a92468a5a3a65fa97c16b2b51e
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-10 18:18:16 +0200

    Fjerner nav-frontend fra TimeoutBox/Nedtelling/LoggetUt

--- src/nav-soknad/components/timeoutbox/LoggetUt.tsx
+++ src/nav-soknad/components/timeoutbox/LoggetUt.tsx
@@ -1,26 +1,30 @@
 import * as React from "react";
 
-import {Innholdstittel, Normaltekst} from "nav-frontend-typografi";
-import {Button} from "@navikt/ds-react";
+import {Button, Heading, Modal} from "@navikt/ds-react";
 import {useTranslation} from "react-i18next";
 
 interface LoggetUtProps {
     onLoginAgainClick: () => void;
+    open: boolean;
 }
 
-const LoggetUt = ({onLoginAgainClick}: LoggetUtProps) => {
+const LoggetUt = ({onLoginAgainClick, open}: LoggetUtProps) => {
     const {t} = useTranslation();
 
     return (
-        <div>
-            <Innholdstittel className="blokk-s timeoutbox__overskrift">{t("timeout.overskrift")}</Innholdstittel>
-            <Normaltekst className="blokk-xxs">{t("timeout.utlopt")}</Normaltekst>
-            <div className="timeoutbox__knapperad">
+        <Modal open={open}>
+            <Modal.Header closeButton={false}>
+                <Heading size={"large"} level={"2"}>
+                    {t("timeout.overskrift")}
+                </Heading>
+            </Modal.Header>
+            <Modal.Body>{t("timeout.utlopt")}</Modal.Body>
+            <Modal.Footer>
                 <Button variant="primary" onClick={onLoginAgainClick}>
                     {t("timeout.logginn")}
                 </Button>
-            </div>
-        </div>
+            </Modal.Footer>
+        </Modal>
     );
 };
 
--- src/nav-soknad/components/timeoutbox/Nedtelling.tsx
+++ src/nav-soknad/components/timeoutbox/Nedtelling.tsx
@@ -1,30 +1,34 @@
 import * as React from "react";
 
-import {Innholdstittel, Normaltekst} from "nav-frontend-typografi";
-import {Button} from "@navikt/ds-react";
+import {Button, Heading, Modal} from "@navikt/ds-react";
 import {useTranslation} from "react-i18next";
 
 interface NedtellingProps {
     onContinueClick: () => void;
     utloggingsUrl: string;
+    open: boolean;
 }
 
-const Nedtelling = ({onContinueClick, utloggingsUrl}: NedtellingProps) => {
+const Nedtelling = ({onContinueClick, utloggingsUrl, open}: NedtellingProps) => {
     const {t} = useTranslation();
 
     return (
-        <div>
-            <Innholdstittel className="blokk-s timeoutbox__overskrift">{t("timeout.overskrift")}</Innholdstittel>
-            <Normaltekst className="blokk-xxs">{t("timeout.nedtelling")}</Normaltekst>
-            <div className="timeoutbox__knapperad">
+        <Modal open={open}>
+            <Modal.Header closeButton={false}>
+                <Heading size={"large"} level={"2"}>
+                    {t("timeout.overskrift")}
+                </Heading>
+            </Modal.Header>
+            <Modal.Body>{t("timeout.nedtelling")}</Modal.Body>
+            <Modal.Footer>
                 <Button variant="primary" onClick={onContinueClick}>
                     {t("timeout.fortsett")}
                 </Button>
                 <Button variant="tertiary" as="a" href={utloggingsUrl} className="timeoutbox__loggutknapp">
                     {t("timeout.loggut")}
                 </Button>
-            </div>
-        </div>
+            </Modal.Footer>
+        </Modal>
     );
 };
 
