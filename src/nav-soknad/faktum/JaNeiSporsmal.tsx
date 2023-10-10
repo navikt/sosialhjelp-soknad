4c0a4f6aefde2949c564ebf9c62caf0673a48fb6
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-10 19:13:20 +0200

    Refaktor JaNeiSporsmal.tsx

--- src/nav-soknad/faktum/JaNeiSporsmal.tsx
+++ src/nav-soknad/faktum/JaNeiSporsmal.tsx
@@ -3,6 +3,7 @@ import ValgMedUnderskjema from "../components/valgMedUnderskjema";
 import Underskjema from "../components/underskjema";
 import Sporsmal, {LegendTittleStyle} from "../components/sporsmal/Sporsmal";
 import RadioEnhanced from "./RadioEnhanced";
+import {useTranslation} from "react-i18next";
 
 interface JaNeiSporsmalProps {
     faktumKey: string;
@@ -26,85 +27,62 @@ const erMobilVisning = () =>
         document.documentElement.clientWidth
     ) < 480;
 
-class JaNeiSporsmal extends React.Component<JaNeiSporsmalProps> {
-    handleOnChange(verdi: any) {
-        this.props.onChange && this.props.onChange(verdi);
+const JaNeiSporsmal = (props: JaNeiSporsmalProps) => {
+    const handleOnChange = (verdi: any) => {
+        props.onChange && props.onChange(verdi);
+    };
+    const {faktumKey, children, verdi} = props;
+    const {t} = useTranslation("skjema");
+    const harUnderSkjema = children !== undefined;
+    const visUnderSkjema = harUnderSkjema && verdi === true;
+
+    let idRadioJa: string;
+    let idRadioNei: string;
+    if (props.id) {
+        idRadioJa = props.id + "_radio_ja";
+        idRadioNei = props.id + "_radio_nei";
+    } else {
+        idRadioJa = faktumKey.replace(/\./g, "_") + "_radio_ja";
+        idRadioJa = idRadioJa.replace(/__/g, "_");
+        idRadioNei = faktumKey.replace(/\./g, "_") + "_radio_nei";
+        idRadioNei = idRadioNei.replace(/__/g, "_");
     }
+    const radioName = faktumKey.replace(/\./g, "_") + "_radio";
 
-    render() {
-        const {faktumKey, children, verdi} = this.props;
+    const mobilVisning = erMobilVisning();
 
-        const harUnderSkjema = children !== undefined;
-        const visUnderSkjema = harUnderSkjema && verdi === true;
+    if (props.visible === false) return null;
 
-        let idRadioJa: string;
-        let idRadioNei: string;
-        if (this.props.id) {
-            idRadioJa = this.props.id + "_radio_ja";
-            idRadioNei = this.props.id + "_radio_nei";
-        } else {
-            idRadioJa = faktumKey.replace(/\./g, "_") + "_radio_ja";
-            idRadioJa = idRadioJa.replace(/__/g, "_");
-            idRadioNei = faktumKey.replace(/\./g, "_") + "_radio_nei";
-            idRadioNei = idRadioNei.replace(/__/g, "_");
-        }
-        const radioName = faktumKey.replace(/\./g, "_") + "_radio";
-
-        const mobilVisning = erMobilVisning();
-
-        if (this.props.visible === false) return null;
-
-        return (
-            <Sporsmal
-                tekster={this.props.tekster}
-                stil={harUnderSkjema ? "jaNeiSporsmal" : "normal"}
-                legendTittelStyle={this.props.legendTittelStyle || LegendTittleStyle.DEFAULT}
+    return (
+        <Sporsmal
+            tekster={props.tekster}
+            stil={harUnderSkjema ? "jaNeiSporsmal" : "normal"}
+            legendTittelStyle={props.legendTittelStyle || LegendTittleStyle.DEFAULT}
+        >
+            <ValgMedUnderskjema
+                underskjema={children ? <Underskjema visible={visUnderSkjema}>{children}</Underskjema> : <span />}
             >
-                <ValgMedUnderskjema
-                    underskjema={children ? <Underskjema visible={visUnderSkjema}>{children}</Underskjema> : <span />}
-                >
-                    <RadioEnhanced
-                        checked={verdi === true}
-                        className="jaNeiSpormal"
-                        faktumKey={this.props.faktumKey}
-                        id={idRadioJa}
-                        name={radioName}
-                        onChange={() => this.handleOnChange(true)}
-                        value={"true"}
-                        visPlaceholder={this.props.visPlaceholder}
-                    />
-                    {!mobilVisning && (
-                        <RadioEnhanced
-                            checked={verdi === false}
-                            className="jaNeiSpormal"
-                            faktumKey={this.props.faktumKey}
-                            id={idRadioNei}
-                            name={radioName}
-                            onChange={() => this.handleOnChange(false)}
-                            value={"false"}
-                            visPlaceholder={this.props.visPlaceholder}
-                        />
-                    )}
-                </ValgMedUnderskjema>
-                {mobilVisning && (
-                    <RadioEnhanced
-                        faktumKey={this.props.faktumKey}
-                        id={idRadioNei}
-                        value={"false"}
-                        checked={!verdi}
-                        name={radioName}
-                        onChange={() => this.handleOnChange(false)}
-                        className={
-                            "jaNeiSpormal inputPanel__mobil--nei " + visUnderSkjema
-                                ? "inputPanel__mobil--uten-underSkjema"
-                                : ""
-                        }
-                        visPlaceholder={this.props.visPlaceholder}
-                    />
-                )}
-            </Sporsmal>
-        );
-    }
-}
+                <RadioEnhanced
+                    checked={verdi === true}
+                    className="jaNeiSpormal"
+                    label={`${t(`${props.faktumKey}.true`)}`}
+                    id={idRadioJa}
+                    name={radioName}
+                    onChange={() => handleOnChange(true)}
+                    value={"true"}
+                />
+                <RadioEnhanced
+                    checked={verdi === false}
+                    className="jaNeiSpormal"
+                    label={`${t(`${props.faktumKey}.false`)}`}
+                    id={idRadioNei}
+                    name={radioName}
+                    onChange={() => handleOnChange(false)}
+                    value={"false"}
+                />
+            </ValgMedUnderskjema>
+        </Sporsmal>
+    );
+};
 
 export default JaNeiSporsmal;
