1afe1396e0a96f9be2005958c0e10428b0d90345
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-11 12:42:57 +0200

    Rydder opp i RadioEnhanced.tsx

--- src/nav-soknad/faktum/RadioEnhanced.tsx
+++ src/nav-soknad/faktum/RadioEnhanced.tsx
@@ -9,12 +9,12 @@ interface Props {
     checked?: null | boolean;
     faktumKey?: string;
     disabled?: boolean;
-    id: string;
+    id?: string;
     label?: any;
     onChange: any;
     className?: string;
     required?: boolean;
-    name: string;
+    name?: string;
     visPlaceholder?: boolean;
 }
 
@@ -40,18 +40,16 @@ const RadioEnhanced = (props: Props) => {
     const id = props.id ? props.id : faktumKey;
 
     return (
-        <div className={props.className}>
-            <RadioPanel
-                id={id}
-                name={props.name}
-                checked={checked()}
-                disabled={props.disabled}
-                value={props.value}
-                required={props.required}
-                onChange={props.onChange}
-                label={determineLabel(props.faktumKey ? props.faktumKey : "", label, props.value)}
-            />
-        </div>
+        <RadioPanel
+            id={id}
+            name={props.name}
+            checked={checked()}
+            disabled={props.disabled}
+            value={props.value}
+            required={props.required}
+            onChange={props.onChange}
+            label={determineLabel(props.faktumKey ? props.faktumKey : "", label, props.value)}
+        />
     );
 };
 
