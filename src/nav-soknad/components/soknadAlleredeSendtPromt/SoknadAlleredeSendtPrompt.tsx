ba3e52bc261d686e49c2530199a6149b6c579fa4
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-11 15:27:47 +0200

    nav-frontend ut av SoknadAllˆeredeSendtPrompt.tsx

--- src/nav-soknad/components/soknadAlleredeSendtPromt/SoknadAlleredeSendtPrompt.tsx
+++ src/nav-soknad/components/soknadAlleredeSendtPromt/SoknadAlleredeSendtPrompt.tsx
@@ -1,33 +1,25 @@
 import * as React from "react";
-import {Innholdstittel, Normaltekst} from "nav-frontend-typografi";
 import {useDispatch, useSelector} from "react-redux";
 import {visSoknadAlleredeSendtPrompt} from "../../../digisos/redux/ettersendelse/ettersendelseActions";
-import {basePath} from "../../../configuration";
 import {State} from "../../../digisos/redux/reducers";
-import {Modal} from "@navikt/ds-react";
+import {BodyShort, Heading, Modal} from "@navikt/ds-react";
 import {useTranslation} from "react-i18next";
 
 const SoknadAlleredeSendtPrompt = () => {
     const dispatch = useDispatch();
     const {t} = useTranslation("skjema", {keyPrefix: "visSoknadAlleredeSendtPrompt"});
 
-    const visPrompt = useSelector((state: State) => state.ettersendelse.visSoknadAlleredeSendtPromt);
-
-    const onClose = () => {
-        dispatch(visSoknadAlleredeSendtPrompt(false));
-    };
+    const open = useSelector((state: State) => state.ettersendelse.visSoknadAlleredeSendtPromt);
 
     return (
-        <Modal open={visPrompt} onClose={() => onClose()}>
-            <Modal.Body>
-                <div className="avbrytmodal">
-                    <div className="avbrytmodal__infoikon_wrapper">
-                        <img src={`${basePath}/statisk/bilder/ikon_ark.svg`} alt={""} />
-                    </div>
-
-                    <Innholdstittel className="blokk-s avbrytmodal__overskrift">{t("overskrift")}</Innholdstittel>
-                    <Normaltekst className="blokk-xxs avbrytmodal__tekst">{t("tekst")}</Normaltekst>
-                </div>
+        <Modal open={open} onClose={() => dispatch(visSoknadAlleredeSendtPrompt(false))}>
+            <Modal.Header>
+                <Heading size={"large"} level={"2"}>
+                    {t("overskrift")}
+                </Heading>
+            </Modal.Header>
+            <Modal.Body className={"flex gap-2"}>
+                <BodyShort>{t("tekst")}</BodyShort>
             </Modal.Body>
         </Modal>
     );
