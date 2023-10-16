d2a852076afd61e34512f3b8a8669d98cbc66263
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-16 19:34:41 +0200

    Rydder opp i skjemahåndtering for Kontonr.tsx

--- src/skjema/01-personalia/Kontonr.tsx
+++ src/skjema/01-personalia/Kontonr.tsx
@@ -2,24 +2,33 @@ import * as React from "react";
 import {useState} from "react";
 import {useTranslation} from "react-i18next";
 import {useBehandlingsId} from "../../lib/hooks/useBehandlingsId";
-import {useHentKontonummer} from "../../generated/kontonummer-ressurs/kontonummer-ressurs";
+import {updateKontonummer, useHentKontonummer} from "../../generated/kontonummer-ressurs/kontonummer-ressurs";
 import Sporsmal from "../../nav-soknad/components/sporsmal/Sporsmal";
 import {KontonrShow} from "./KontonrShow";
 import {KontonrEdit} from "./KontonrEdit";
 import {LinkButton} from "../../nav-soknad/components/linkButton/LinkButton";
 import {useAlgebraic} from "../../lib/hooks/useAlgebraic";
 import {Systeminfo} from "../../nav-soknad/components/systeminfo/Systeminfo";
+import {useDigisosMutation} from "./useDigisosMutation";
 
 export const Kontonr = () => {
     const behandlingsId = useBehandlingsId();
     const [editMode, setEditMode] = useState<boolean>(false);
     const {expectOK} = useAlgebraic(useHentKontonummer(behandlingsId));
     const {t} = useTranslation("skjema", {keyPrefix: "kontakt"});
+    const {mutate} = useDigisosMutation(useHentKontonummer, updateKontonummer);
 
-    return expectOK(({brukerutfyltVerdi, brukerdefinert, harIkkeKonto}) => (
+    return expectOK((data) => (
         <Sporsmal sporsmal={t("kontonummer.sporsmal")} infotekst={t("kontonummer.infotekst.tekst")}>
             {editMode ? (
-                <KontonrEdit onClose={() => setEditMode(false)} />
+                <KontonrEdit
+                    defaultValues={data}
+                    onSave={async (data) => {
+                        await mutate(data);
+                        setEditMode(false);
+                    }}
+                    onCancel={() => setEditMode(false)}
+                />
             ) : (
                 <Systeminfo>
                     <KontonrShow />
--- src/skjema/01-personalia/KontonrEdit.tsx
+++ src/skjema/01-personalia/KontonrEdit.tsx
@@ -1,21 +1,28 @@
 import {useTranslation} from "react-i18next";
-import {useForm} from "react-hook-form";
+import {SubmitHandler, useForm} from "react-hook-form";
 import {Button, Checkbox, TextField} from "@navikt/ds-react";
 import * as React from "react";
-import {KontonummerInputDTO} from "../../generated/model";
+import {KontonummerFrontend, KontonummerInputDTO} from "../../generated/model";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {z} from "zod";
-import {useKontonummer} from "./useKontonummer";
 
 const KontonummerRegex = new RegExp("^\\d{11}$");
 
 const KontonummerSchema = z.object({
     brukerutfyltVerdi: z.string().regex(KontonummerRegex, "kontakt.kontonummer.feilmelding").optional(),
+    harIkkeKonto: z.boolean().optional(),
 });
 
-export const KontonrEdit = ({onClose}: {onClose: () => void}) => {
+export const KontonrEdit = ({
+    defaultValues,
+    onSave,
+    onCancel,
+}: {
+    defaultValues: KontonummerFrontend;
+    onSave: SubmitHandler<KontonummerInputDTO>;
+    onCancel: () => void;
+}) => {
     const {t} = useTranslation("skjema");
-    const {load, save} = useKontonummer();
 
     const {
         handleSubmit,
@@ -25,17 +32,12 @@ export const KontonrEdit = ({onClose}: {onClose: () => void}) => {
             errors: {brukerutfyltVerdi: kontonummerError},
         },
     } = useForm<KontonummerInputDTO>({
-        defaultValues: load,
+        defaultValues: defaultValues,
         resolver: zodResolver(KontonummerSchema),
     });
 
-    const update = async (konto: KontonummerInputDTO) => {
-        save(konto);
-        onClose();
-    };
-
     return (
-        <form onSubmit={handleSubmit(update)}>
+        <form onSubmit={handleSubmit(onSave)}>
             <TextField
                 {...register("brukerutfyltVerdi")}
                 label={t("kontakt.kontonummer.sporsmal")}
@@ -49,7 +51,7 @@ export const KontonrEdit = ({onClose}: {onClose: () => void}) => {
             />
             <Checkbox {...register("harIkkeKonto")}>{t("kontakt.kontonummer.harikke")}</Checkbox>
             <div className={"space-x-2"}>
-                <Button variant="secondary" onClick={onClose}>
+                <Button variant="secondary" onClick={onCancel}>
                     {t("avbryt")}
                 </Button>
                 <Button type={"submit"} data-testid="lagre-kontonummer">
