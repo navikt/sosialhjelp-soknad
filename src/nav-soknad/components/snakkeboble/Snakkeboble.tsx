c24f6753efa31d3f9c6050b6a975f709a6974126
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-18 16:25:30 +0200

    Korrigerer styling av EllaBlunk

--- src/nav-soknad/components/snakkeboble/Snakkeboble.tsx
+++ src/nav-soknad/components/snakkeboble/Snakkeboble.tsx
@@ -8,13 +8,13 @@ const Snakkeboble = ({fornavn}: {fornavn?: string}) => {
 
     return (
         <div className="mb-4 flex flex-col items-center">
-            <div className="p-8 lg:ml-20 max-w-[360px] rounded-lg bg-gradient-to-b from-digisosGronnBakgrunnTop to-digisosGronnBakgrunnBottom">
+            <div className="p-8 lg:ml-20 max-w-[360px] rounded-lg bg-digisosGronnBakgrunn">
                 {fornavn?.length && <Label spacing>{t("informasjon.hilsen.hei", {fornavn})}</Label>}
                 <BodyShort>{t("informasjon.hilsen.tittel")}</BodyShort>
             </div>
             <div className={"flex justify-center pl-24 -mb-2"}>
                 <div
-                    className={"border-transparent border-t-digisosGronnBakgrunnBottom w-0 h-0"}
+                    className={"border-transparent border-t-digisosGronnBakgrunn w-0 h-0"}
                     aria-hidden="true"
                     style={{
                         borderStyle: "solid",
