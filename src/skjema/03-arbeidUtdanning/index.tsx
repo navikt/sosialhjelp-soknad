eacded5e1a54648b1d7958914934e88dabc3c9d3
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2023-10-18 13:54:00 +0200

    Rydd opp i ArbeidOgUtdanning

--- src/skjema/03-arbeidUtdanning/index.tsx
+++ src/skjema/03-arbeidUtdanning/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Detail, Heading, Textarea} from "@navikt/ds-react";
+import {Detail, Heading, Radio, RadioGroup, Textarea} from "@navikt/ds-react";
 import {useTranslation} from "react-i18next";
 import {DigisosValidationError, SkjemaSteg} from "../../nav-soknad/components/SkjemaSteg/ny/SkjemaSteg";
 import {useForm} from "react-hook-form";
@@ -7,12 +7,14 @@ import {ArbeidFrontend, UtdanningFrontend} from "../../generated/model";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useBehandlingsId} from "../../lib/hooks/useBehandlingsId";
 import {z} from "zod";
-import {hentArbeid, updateArbeid} from "../../generated/arbeid-ressurs/arbeid-ressurs";
+import {updateArbeid, useHentArbeid} from "../../generated/arbeid-ressurs/arbeid-ressurs";
 import {ArbeidsforholdListe} from "./arbeid/ArbeidsforholdListe";
 import {TranslatedError} from "../02-begrunnelse";
-import {hentUtdanning, updateUtdanning} from "../../generated/utdanning-ressurs/utdanning-ressurs";
-import UtdanningInput from "./utdanning/Utdanning";
+import {updateUtdanning, useHentUtdanning} from "../../generated/utdanning-ressurs/utdanning-ressurs";
 import * as Sentry from "@sentry/react";
+import {YesNoInput} from "../../nav-soknad/components/form/YesNoInput";
+import {UnmountClosed} from "react-collapse";
+import {useQueryClient} from "@tanstack/react-query";
 
 const ArbeidOgUtdanningSchema = z.object({
     arbeid: z.object({kommentarTilArbeidsforhold: z.string().max(500, "maksLengde").nullable()}),
@@ -27,38 +29,52 @@ export type ArbeidOgUtdanningType = {
     utdanning: UtdanningFrontend;
 };
 
-const ArbeidOgUtdanning = () => {
+const useArbeidOgUtdanning = () => {
     const behandlingsId = useBehandlingsId();
-    const {t} = useTranslation("skjema");
+    const queryClient = useQueryClient();
+    const {data: arbeid, isLoading: isLoadingArbeid, queryKey: arbeidKey} = useHentArbeid(behandlingsId);
+    const {data: utdanning, isLoading: isLoadingUtdanning, queryKey: utdanningKey} = useHentUtdanning(behandlingsId);
 
+    const data: ArbeidOgUtdanningType | undefined = arbeid && utdanning ? {arbeid, utdanning} : undefined;
+
+    const mutate = async (data: ArbeidOgUtdanningType) => {
+        await updateArbeid(behandlingsId, data.arbeid);
+        queryClient.setQueryData(arbeidKey, data.arbeid);
+        await updateUtdanning(behandlingsId, data.utdanning);
+        queryClient.setQueryData(utdanningKey, data.utdanning);
+    };
+
+    return {data, mutate, isLoading: isLoadingUtdanning || isLoadingArbeid};
+};
+
+const ArbeidOgUtdanningForm = ({data}: {data: ArbeidOgUtdanningType}) => {
     const {
         register,
         handleSubmit,
         formState: {errors},
-        control,
+        getValues,
+        setValue,
+        watch,
     } = useForm<ArbeidOgUtdanningType>({
-        defaultValues: async () => ({
-            arbeid: await hentArbeid(behandlingsId),
-            utdanning: await hentUtdanning(behandlingsId),
-        }),
+        defaultValues: data,
         resolver: zodResolver(ArbeidOgUtdanningSchema),
     });
-
+    const {t} = useTranslation("skjema");
     const [error, setError] = React.useState<boolean>(false);
+    const {mutate} = useArbeidOgUtdanning();
 
     const onRequestNavigation = async () =>
         handleSubmit(
             async (data: ArbeidOgUtdanningType) => {
                 try {
-                    await updateArbeid(behandlingsId, data.arbeid);
-                    await updateUtdanning(behandlingsId, data.utdanning);
+                    await mutate(data);
                 } catch (e) {
                     Sentry.captureException(e);
                     setError(true);
                     throw new DigisosValidationError();
                 }
             },
-            () => {
+            (errors) => {
                 throw new DigisosValidationError();
             }
         )();
@@ -67,8 +83,9 @@ const ArbeidOgUtdanning = () => {
         <SkjemaSteg page={3} onRequestNavigation={onRequestNavigation}>
             <SkjemaSteg.Content>
                 <SkjemaSteg.Title />
-                <SkjemaSteg.ErrorSummary errors={errors} />
                 <form className={"space-y-12 lg:space-y-24"} onSubmit={(e) => e.preventDefault()}>
+                    <SkjemaSteg.ErrorSummary errors={errors} />
+
                     <div className={"space-y-6"}>
                         <Heading size="medium" spacing>
                             {t("arbeidsforhold.sporsmal")}
@@ -93,7 +110,22 @@ const ArbeidOgUtdanning = () => {
                         <Heading size="medium" spacing>
                             {t("arbeid.dinsituasjon.studerer.undertittel")}
                         </Heading>
-                        <UtdanningInput control={control} />
+
+                        <YesNoInput
+                            defaultValue={getValues("utdanning.erStudent")}
+                            onChange={(value: boolean) => setValue("utdanning.erStudent", value)}
+                            legend={t("dinsituasjon.studerer.sporsmal")}
+                        />
+                        <UnmountClosed isOpened={watch("utdanning.erStudent") === true}>
+                            <RadioGroup
+                                legend={t("dinsituasjon.studerer.grad.sporsmal")}
+                                defaultValue={getValues("utdanning.studengradErHeltid")?.toString() ?? null}
+                                onChange={(value: string) => setValue("utdanning.studengradErHeltid", value === "true")}
+                            >
+                                <Radio value={"true"}>{t("dinsituasjon.studerer.grad.heltid")}</Radio>
+                                <Radio value={"false"}>{t("dinsituasjon.studerer.grad.deltid")}</Radio>
+                            </RadioGroup>
+                        </UnmountClosed>
                     </div>
                     {error && <div>{t("skjema.navigering.feil")}</div>}
                     <SkjemaSteg.Buttons />
@@ -101,6 +133,14 @@ const ArbeidOgUtdanning = () => {
             </SkjemaSteg.Content>
         </SkjemaSteg>
     );
+};
+
+const ArbeidOgUtdanning = () => {
+    const {data} = useArbeidOgUtdanning();
+
+    if (!data) return null;
+
+    return <ArbeidOgUtdanningForm data={data} />;
 };
 
 export default ArbeidOgUtdanning;
