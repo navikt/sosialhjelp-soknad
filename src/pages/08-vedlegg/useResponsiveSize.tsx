417eb3b8b04cf77cf18343a044bd344341f7ac2a
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-02-15 11:11:44 +0100

    Fjerner paddingOffset
    clientHeight inkluderer ikke border

--- src/pages/08-vedlegg/useResponsiveSize.tsx
+++ src/pages/08-vedlegg/useResponsiveSize.tsx
@@ -7,17 +7,16 @@ import React, {useEffect, useState} from "react";
  * Basically, pdfjs renders to a canvas of a given size. This size is set in the `Page` component.
  * However, we want the size of the `Page` component to be responsive to the size of the container.
  *
- * @param paddingOffset - Pixel offset to subtract from the height of the container. This is to account for padding and margins.
  * @warning You *must* set paddingOffset to at least the number of vertical padding pixels!
  *          Otherwise, the `Page` component will grow indefinitely as each resize grows the parent, triggering a new resize.
  */
-export const useResponsiveSize = (paddingOffset: number) => {
+export const useResponsiveSize = () => {
     const containerRef = React.useRef<HTMLDivElement>(null);
     const [height, setHeight] = useState<number>(20);
     useEffect(() => {
         if (!containerRef.current) return;
         const observer = new ResizeObserver(() => {
-            if (containerRef.current) setHeight(containerRef.current.clientHeight - paddingOffset);
+            if (containerRef.current) setHeight(containerRef.current.clientHeight);
         });
         observer.observe(containerRef.current);
         return () => observer.disconnect();
