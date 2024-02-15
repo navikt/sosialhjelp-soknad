917935170f6ca76181e2dc0b0f0cb85ec9a7e66a
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-02-15 11:11:13 +0100

    Refaktor og dokumentasjon av usePDFConverter

--- src/pages/08-vedlegg/usePDFConverter.tsx
+++ src/pages/08-vedlegg/usePDFConverter.tsx
@@ -1,28 +1,38 @@
 import * as React from "react";
 import {konverterVedlegg} from "../../generated/file-converter-controller/file-converter-controller";
 
-/**
- * React-hook for konvertering av PDF via backend.
- *
- * @returns {conversionPending} - True if a conversion is pending.
- * @returns {convertToPDF} - Function to upload a file for PDF conversion. Returns a promise that resolves to the converted PDF.
- * @throws {PdfConversionError} - If the conversion fails.
- */
-export const usePDFConverter = () => {
-    const [conversionPending, setConversionPending] = React.useState(false);
-
-    const convertToPDF = (file: Blob): Promise<Blob> => {
-        setConversionPending(true);
-        try {
-            return konverterVedlegg({file: file});
-        } catch (e) {
-            throw new PdfConversionError(e);
-        } finally {
-            setConversionPending(false);
-        }
-    };
-
-    return {conversionPending, convertToPDF};
+type UsePDFConverterResult = {
+    // En konvertering er i gang.
+    conversionPending: boolean;
+    // Dersom en feil oppstår under konvertering, vil denne settes.
+    conversionError: Error | null;
+    // Sender en gitt fil til backend for konvertering til PDF.
+    convertToPDF: (file: Blob) => Promise<Blob>;
 };
 
-export class PdfConversionError extends Error {}
+/**
+ * React-hook for konvertering av PDF via backend.
+ */
+export const usePDFConverter = (): UsePDFConverterResult => {
+    const [conversionPending, setConversionPending] = React.useState(false);
+    const [conversionError, setConversionError] = React.useState<Error | null>(null);
+
+    const convertToPDF = async (file: Blob): Promise<Blob> => {
+        setConversionPending(true);
+
+        return konverterVedlegg({file: file})
+            .then((blob) => {
+                setConversionPending(false);
+                return blob;
+            })
+            .catch((e) => {
+                setConversionError(e);
+                throw e;
+            })
+            .finally(() => {
+                setConversionPending(false);
+            });
+    };
+
+    return {conversionPending, conversionError, convertToPDF};
+};
