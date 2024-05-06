fb3de9d0bbd0b344dd2d9040923259190e8d75d8
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-05-06 12:36:02 +0200

    Fjerner oppsummering fra Redux

--- src/lib/redux/reducers.ts
+++ src/lib/redux/reducers.ts
@@ -1,15 +1,12 @@
 import {combineReducers} from "redux";
 import {valideringsReducer, ValideringState} from "./validering/valideringReducer";
-import OppsummeringReducer, {OppsummeringState} from "./oppsummering/oppsummeringReducer";
 
 export interface State {
-    oppsummering: OppsummeringState;
     validering: ValideringState;
 }
 
 const reducers = () =>
     combineReducers({
-        oppsummering: OppsummeringReducer,
         validering: valideringsReducer,
     });
 
