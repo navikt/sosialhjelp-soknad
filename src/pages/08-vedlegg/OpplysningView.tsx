ee70b5f96f56cef0500f818db76dad895ff7cacb
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-05-08 13:40:01 +0200

    VedleggSlettet er ikke støttet pt
    (dette bygget på gammel redux, om den er slettet nå er den simpelthen borte)

--- src/pages/08-vedlegg/OpplysningView.tsx
+++ src/pages/08-vedlegg/OpplysningView.tsx
@@ -1,14 +1,11 @@
 import {TabellView} from "./TabellView";
 import {VedleggView} from "./upload/VedleggView";
-import {VedleggSlettet} from "./vedleggSlettet";
 import {Opplysning} from "../../lib/opplysninger";
 import {BodyShort, Heading} from "@navikt/ds-react";
 import {useOpplysningTekster} from "./useOpplysningTekster";
 
 export const OpplysningView = ({opplysning}: {opplysning: Opplysning}) => {
     const {sporsmal, undertekst} = useOpplysningTekster(opplysning.type);
-
-    if (opplysning.slettet) return <VedleggSlettet opplysning={opplysning} />;
 
     return (
         <div className={"rounded-md bg-surface-subtle p-8"}>
