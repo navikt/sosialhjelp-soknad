c9bac4ab3c4a0e48ac2dc2527dcb4b5a5f731616
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-05-09 15:42:32 +0200

    Oppdaterer eslint-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-);
+export default tseslint.config({ignores: ['build/', 'src/generated/']},eslint.configs.recommended, ...tseslint.configs.recommended, {
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
+    },
+});
