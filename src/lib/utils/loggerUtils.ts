7221b3a2792143134bcc3e73fb4a873b6405ea9e
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-05-29 16:55:47 +0200

    Fjern dupliserte datatyper

--- src/lib/utils/loggerUtils.ts
+++ src/lib/utils/loggerUtils.ts
@@ -1,42 +1,21 @@
 import {axiosInstance} from "../orval/soknad-api-axios";
-
-// Caveat coder: These types are originally from the OpenAPI spec, but I've
-// duplicated them here to prevent orval throwing an error when generating
-// the types. Presumed harmless; I suspect this API won't change much before
-// we get rid of it.
-type LoggLevel = (typeof LoggLevelValues)[keyof typeof LoggLevelValues];
-
-const LoggLevelValues = {
-    ERROR: "ERROR",
-    WARN: "WARN",
-    INFO: "INFO",
-} as const;
-
-interface Logg {
-    level: LoggLevel;
-    message?: string;
-    jsFileUrl?: string;
-    lineNumber?: string;
-    columnNumber?: string;
-    url?: string;
-    userAgent?: string;
-}
+import {Logg, LoggLevel} from "../../generated/model";
 
 export const logWindowError: typeof window.onerror = (message, jsFileUrl, lineNumber, columnNumber) => {
     logToServer({
         url: window.location.href,
         userAgent: window.navigator.userAgent,
         message: `window.onerror: ${message.toString()}`,
-        level: LoggLevelValues.WARN,
+        level: LoggLevel.WARN,
         jsFileUrl,
         lineNumber: lineNumber?.toString(),
         columnNumber: columnNumber?.toString(),
     }).then();
 };
 
-export const logInfo = (message: string): Promise<void> => log(message, LoggLevelValues.INFO);
-export const logWarning = (message: string): Promise<void> => log(message, LoggLevelValues.WARN);
-export const logError = (message: string): Promise<void> => log(message, LoggLevelValues.ERROR);
+export const logInfo = (message: string): Promise<void> => log(message, LoggLevel.INFO);
+export const logWarning = (message: string): Promise<void> => log(message, LoggLevel.WARN);
+export const logError = (message: string): Promise<void> => log(message, LoggLevel.ERROR);
 
 const logLocally = ({message, level}: Logg) => {
     switch (level) {
