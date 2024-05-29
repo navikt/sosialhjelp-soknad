5abf2f140d4024fb58ceb86fc26012a5cd21c676
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-05-29 13:07:44 +0200

    Rydd opp i ustabil i18n-kode

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 import "@navikt/ds-css";
-import {Suspense, useEffect} from "react";
+import {Suspense} from "react";
 import {createRoot} from "react-dom/client";
 import {logWindowError} from "./lib/utils/loggerUtils";
 import {injectDecoratorClientSide} from "@navikt/nav-dekoratoren-moduler";
@@ -8,7 +8,7 @@ import {RouterProvider} from "react-router-dom";
 import {router} from "./routes";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
-import {i18nSetLangFromLocalStorage} from "./lib/i18n";
+import {useLocalStorageLangSelector} from "./lib/i18n";
 import {withFaroProfiler} from "@grafana/faro-react";
 import {ApplicationSpinner} from "./lib/components/animasjoner/ApplicationSpinner";
 import {ValideringsContextProvider} from "./lib/valideringContextProvider";
@@ -19,7 +19,7 @@ window.onerror = logWindowError;
 const queryClient = new QueryClient();
 
 const App = () => {
-    useEffect(i18nSetLangFromLocalStorage, []);
+    useLocalStorageLangSelector();
 
     return (
         <Suspense fallback={<ApplicationSpinner />}>
