8a531c0909149a49fef02b8a40d58a0cc4d31302
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-05-29 13:08:11 +0200

    Fjern unødvendig abstraksjon av amplitude

--- src/lib/amplitude/worker.ts
+++ src/lib/amplitude/worker.ts
@@ -1,7 +1,19 @@
-import {logAmplitudeEvent} from "../utils/amplitude";
+import {logAmplitudeEvent} from "@navikt/nav-dekoratoren-moduler";
+import {logWarning} from "../utils/loggerUtils";
 
-type AmplitudePayload = {eventName: string; eventData?: Record<string, unknown>};
+type AmplitudePayload = {
+    eventName: string;
+    eventData?: Record<string, unknown>;
+};
 
 onmessage = ({data: {eventData, eventName}}: MessageEvent<AmplitudePayload>) => {
-    logAmplitudeEvent(eventName, eventData).then();
+    logAmplitudeEvent({
+        origin: "sosialhjelpsoknad",
+        eventName,
+        eventData: {...eventData, skjemaId: "sosialhjelpsoknad"},
+    })
+        .catch((error) => {
+            logWarning(`Kunne ikke logge til Amplitude: ${error}`).then();
+        })
+        .then();
 };
