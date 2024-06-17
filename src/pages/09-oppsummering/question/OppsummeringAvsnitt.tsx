e664b37ad96c9eaf48e9f451d6eedb7882c16bc3
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-06-17 16:16:43 +0200

    B - Glemte en t() under refaktor av oppsummering

--- src/pages/09-oppsummering/question/OppsummeringAvsnitt.tsx
+++ src/pages/09-oppsummering/question/OppsummeringAvsnitt.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import {Heading} from "@navikt/ds-react";
 import {Avsnitt} from "../../../generated/model";
 import {OppsummeringSporsmal} from "./OppsummeringSporsmal";
+import {useTranslation} from "react-i18next";
 
-export const OppsummeringAvsnitt = ({avsnitt: {tittel, sporsmal}}: {avsnitt: Avsnitt}) => (
-    <div className={"mb-8"}>
-        <div className={"flex items-baseline gap-3"}>
-            <Heading level="3" size="small" spacing>
-                {tittel}
-            </Heading>
+export const OppsummeringAvsnitt = ({avsnitt: {tittel, sporsmal}}: {avsnitt: Avsnitt}) => {
+    const {t} = useTranslation();
+
+    return (
+        <div className={"mb-8"}>
+            <div className={"flex items-baseline gap-3"}>
+                <Heading level="3" size="small" spacing>
+                    {t(tittel)}
+                </Heading>
+            </div>
+            {sporsmal?.map((sporsmal) => <OppsummeringSporsmal sporsmal={sporsmal} key={sporsmal.tittel} />)}
         </div>
-        {sporsmal?.map((sporsmal) => <OppsummeringSporsmal sporsmal={sporsmal} key={sporsmal.tittel} />)}
-    </div>
-);
+    );
+};
