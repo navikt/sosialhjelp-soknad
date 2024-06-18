b9d31f82c946b79ea5f96c30a8dde89aca1ac293
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-06-18 20:44:41 +0200

    R - spre ut og enhetstest redirect-funksjoner

--- src/lib/api/auth/redirectToGotoSearchParameter.ts
+++ src/lib/api/auth/redirectToGotoSearchParameter.ts
@@ -1,12 +1,6 @@
 import {redirect} from "react-router-dom";
 import {basePath} from "../../config";
-
-const removeBasePath = (str: string | null, prefix: string) => {
-    if (!str) return null;
-    const isPrefixed = str.startsWith(prefix);
-    const path = isPrefixed ? str.substring(prefix.length) : str;
-    return path.length ? path : null;
-};
+import {removeBasePath} from "./removeBasePath";
 
 export const redirectToGotoSearchParameter = async () => {
     const gotoParam = new URLSearchParams(window.location.search).get("goto");
