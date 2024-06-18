58bc37c4ad245f982a7764584dbfabde0ec51a45
joeldlin <jorgen.elden.lindgren@nav.no>
2024-06-18 12:58:02 +0200

    b - går tilbake til versjonen som funker

--- src/lib/api/auth/buildRedirectUrl.ts
+++ src/lib/api/auth/buildRedirectUrl.ts
@@ -7,11 +7,14 @@ export interface LocationFragment {
 }
 
 export const buildRedirectUrl = ({origin, pathname, search}: LocationFragment) => {
-    const redirectUrl = new URL(`${origin}${pathname}`);
+    const redirectUrl =
+        pathname === linkPagePath ? new URL(`${origin}${linkPagePath}`) : new URL(`${origin}${pathname}`);
 
     // Hvis vi er på /link og har en goto-parameter, bevarer vi den.
     const goto = new URLSearchParams(search).get("goto");
-    if (pathname === linkPagePath && goto) redirectUrl.searchParams.set("goto", goto);
+    if (pathname === linkPagePath && goto) {
+        redirectUrl.searchParams.set("goto", goto);
+    }
 
     return redirectUrl.toString();
 };
