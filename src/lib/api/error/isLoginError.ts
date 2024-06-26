49229028bf7860af49edecbbeb57281304bb486d
joeldlin <59644512+joeldlin@users.noreply.github.com>
2024-06-26 14:22:51 +0200

    b - Det hoppet frem mye "Error: Missing loginUrl in 401 response" meldinger på slack, det kan være en litt hickup men for sikkerhet skyld endres const isLoginError (#1431)

--- src/lib/api/error/isLoginError.ts
+++ src/lib/api/error/isLoginError.ts
@@ -2,6 +2,7 @@ import {AxiosResponse} from "axios";
 import {UnauthorizedMelding} from "../../../generated/model";
 
 export const isLoginError = (response: AxiosResponse): response is AxiosResponse<UnauthorizedMelding> => {
-    if (!response.data?.loginUrl) throw new Error("Missing loginUrl in 401 response");
-    return response.status === 401;
+    const is401 = response.status === 401;
+    if (is401 && !response.data?.loginUrl) throw new Error("Missing loginUrl in 401 response");
+    return is401;
 };
