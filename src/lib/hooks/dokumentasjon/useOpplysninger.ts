7f463ffe3a04382004091afdae9dcb8d3e9fcc11
joeldlin <59644512+joeldlin@users.noreply.github.com>
2024-06-26 09:40:58 +0200

    Arbeid og utdanning vises to ganger (#1430)
    b - Steg 8 - "Arbeid og Utdanning" vises dobbelt opp, tilbakestiller derfor "Arbeid og utdanning" endringene på grunn av buggen, og det var det hurtigste måten å fikse det. Sjekk trello oppgaven https://trello.com/c/VInrdTbm/121-bug-i-steg-8-oppsummering-hent-fra-skatteetaten-video-lagt-til-illustrerer-bug for å se videoklipp om buggen.

--- src/lib/hooks/dokumentasjon/useOpplysninger.ts
+++ src/lib/hooks/dokumentasjon/useOpplysninger.ts
@@ -1,10 +1,9 @@
 import {useMemo} from "react";
-import {VedleggFrontendGruppe, VedleggFrontends} from "../../../generated/model";
+import {VedleggFrontends} from "../../../generated/model";
 import {Opplysning, opplysningSpec, vedleggGrupper} from "../../opplysninger";
 import {useBehandlingsId} from "../common/useBehandlingsId";
 import {useHentOkonomiskeOpplysninger} from "../../../generated/okonomiske-opplysninger-ressurs/okonomiske-opplysninger-ressurs";
 import {logError} from "../../log/loggerUtils";
-import {useSkatteetatenData} from "../data/useSkatteetatenData";
 
 export const flettOgSorter = ({okonomiskeOpplysninger, slettedeVedlegg}: VedleggFrontends): Opplysning[] => {
     const current = okonomiskeOpplysninger?.map((opplysning): Opplysning => ({...opplysning}));
@@ -18,7 +17,6 @@ export const useOpplysninger = () => {
     const behandlingsId = useBehandlingsId();
 
     const {data, isLoading, error} = useHentOkonomiskeOpplysninger(behandlingsId, {});
-    const {samtykke} = useSkatteetatenData();
 
     if (error) {
         logError(`Feil ved HentOkonomiskeOpplysninger: ${error}`);
@@ -29,16 +27,10 @@ export const useOpplysninger = () => {
     // Filtrer vekk tomme grupper, slik at vi kan bruke liste.length under for å mekke grønne linjer mellom ting
     const grupper = vedleggGrupper.filter((gruppeNavn) => sorterte.filter(({gruppe}) => gruppe === gruppeNavn).length);
 
-    // For å jobbe rundt at dokumentasjonskravet for arbeid|lonnsslipp forsvinner når samtykke er true,
-    // som fører til at arbeid-gruppa blir tom og dermed forsvinner. Men vi har hardkodet at dersom
-    // skatteetaten-samtykke er innvilget, skal vi vise det panelet i kontekst av arbeid-gruppa.
-    const grupperMedSuperstyggHack =
-        !grupper.includes("arbeid") && !samtykke ? grupper : (["arbeid", ...grupper] as VedleggFrontendGruppe[]);
-
     return {
         data,
         isLoading,
-        grupper: grupperMedSuperstyggHack,
+        grupper: grupper,
         sorterte,
         bekreftet: data?.isOkonomiskeOpplysningerBekreftet ?? undefined,
     };
