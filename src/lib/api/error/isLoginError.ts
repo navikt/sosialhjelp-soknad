86864c5ec38f6a5b1277e04d5a9b1be989b69ec1
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-07-04 11:40:35 +0200

    Fiks isLoginError.ts

--- src/lib/api/error/isLoginError.ts
+++ src/lib/api/error/isLoginError.ts
@@ -2,6 +2,9 @@ import {AxiosResponse} from "axios";
 import {UnauthorizedMelding} from "../../../generated/model";
 
 export const isLoginError = (response: AxiosResponse): response is AxiosResponse<UnauthorizedMelding> => {
+    if (response.status !== 401) return false;
+
     if (!response.data?.loginUrl) throw new Error("Missing loginUrl in 401 response");
-    return response.status === 401;
+
+    return true;
 };
