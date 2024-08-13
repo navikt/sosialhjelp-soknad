a7ca7e1830efce50ae7f518411f1d0d0961c3d21
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-08-13 20:18:20 +0200

    Etterhvert funker nok dette

--- src/lib/middleware/verifyOrRedirectToLogin.ts
+++ src/lib/middleware/verifyOrRedirectToLogin.ts
@@ -3,6 +3,7 @@ import digisosConfig from "../config.ts";
 import {BASE_PATH, LINK_PAGE_PATH} from "../constants.ts";
 import {getGotoParameter} from "../api/auth/getGotoParameter.ts";
 import {RequestCookies} from "next/dist/compiled/@edge-runtime/cookies";
+import {headers} from "next/headers";
 
 async function canaryRequest(url: string, cookies: RequestCookies): Promise<Response> {
     const cookie = Object.values(cookies.getAll())
@@ -21,6 +22,9 @@ export async function verifyOrRedirectToLogin({
     url,
     cookies,
 }: Pick<NextRequest, "url" | "cookies">): Promise<NextResponse> {
+    console.log(headers().get("host"));
+    console.log(headers().get("x-forwarded-host"));
+
     try {
         // FIXME: Find better way to verify session validity; this is relatively expensive
         // Because middleware runs on the Edge runtime, we can't use Axios here.
@@ -30,7 +34,6 @@ export async function verifyOrRedirectToLogin({
             return NextResponse.next();
         } else {
             const responseBody = await res.json();
-
             const nextUrl = new URL(url);
             const redirect = nextUrl.origin + LINK_PAGE_PATH;
             const goto = BASE_PATH + getGotoParameter(nextUrl);
