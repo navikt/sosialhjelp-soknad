73ad64e22ed21a9e31ecebf6e795c5c68ad5e6e7
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-08-20 12:34:23 +0200

    Se om dette funker i mock

--- src/app/informasjon/route.ts
+++ src/app/informasjon/route.ts
@@ -1,12 +1,17 @@
 import {NextRequest, NextResponse} from "next/server";
 import {BASE_PATH} from "../../lib/constants.ts";
 
-export async function GET({nextUrl}: NextRequest) {
+export async function GET({nextUrl, headers}: NextRequest) {
     // If there's an "axiosError" GET query parameter, we log an error.
-    if (nextUrl.searchParams.has("axiosError")) {
-        console.error("axiosError", nextUrl.searchParams.get("axiosError"));
-    }
+    // FIXME
+    // if (nextUrl.searchParams.has("axiosError")) {
+    //     console.error("axiosError", nextUrl.searchParams.get("axiosError"));
+    // }
 
-    nextUrl.pathname = BASE_PATH;
-    return NextResponse.redirect(nextUrl);
+    const url = nextUrl.clone();
+    url.pathname = BASE_PATH;
+    url.host = headers.get("x-forwarded-host") ?? headers.get("host")!;
+    url.port = headers.get("x-forwarded-port") ?? headers.get("port")!;
+    console.log(url);
+    return NextResponse.redirect(url);
 }
