5f2a8f3b2bb34ef547d285a2383e26c969b15732
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-09-03 14:47:21 +0200

    Fail-safe hvis server-side auth check feiler

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,15 @@
-import {type NextRequest} from "next/server";
+import {type NextRequest, NextResponse} from "next/server";
 import {verifyOrRedirectToLogin} from "./lib/middleware/verifyOrRedirectToLogin.ts";
 
 export async function middleware({url, cookies}: NextRequest) {
-    return await verifyOrRedirectToLogin({url, cookies});
+    try {
+        return await verifyOrRedirectToLogin({url, cookies});
+    } catch (e: any) {
+        // It's not a big deal if the above fails, because we do the same
+        // redirect on the client side. so we set this up fail-safe.
+        console.error("Harmless whoopsie-daisy in auth middleware:", e);
+        return NextResponse.next();
+    }
 }
 
 export const config = {
