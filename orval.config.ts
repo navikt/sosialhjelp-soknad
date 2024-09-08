0ca1c7e70c5da1a2d665238e79e4a3389802ccb3
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-09-08 16:50:25 +0200

    Fjerner ubrukt kode

--- orval.config.ts
+++ orval.config.ts
@@ -41,26 +41,27 @@ export default defineConfig({
             afterAllFilesWrite: "prettier --write",
         },
     },
-    "soknad-api-ssr": {
-        input: "./soknad-api.json",
-        output: {
-            mode: "tags-split",
-            target: "src/generated/server/axiosInstance.ts",
-            schemas: "src/generated/server/model",
-            client: "axios-functions",
-            override: {
-                mutator: {
-                    path: "./src/lib/api/axiosInstanceSsr.ts",
-                    name: "axiosInstance",
-                },
-                transformer: (operation) => ({...operation, operationName: `${operation.operationName}Ssr`}),
-            },
-            // Vi bruker ikke mocks enda, og avventer ny versjon av orval
-            // som støtter msw v2.
-            mock: false,
-        },
-        hooks: {
-            afterAllFilesWrite: "prettier --write",
-        },
-    },
+    // TODO: Finne løsning for API-bruk på server-side
+    // "soknad-api-ssr": {
+    //     input: "./soknad-api.json",
+    //     output: {
+    //         mode: "tags-split",
+    //         target: "src/generated/server/axiosInstance.ts",
+    //         schemas: "src/generated/server/model",
+    //         client: "axios-functions",
+    //         override: {
+    //             mutator: {
+    //                 path: "./src/lib/api/axiosInstanceSsr.ts",
+    //                 name: "axiosInstance",
+    //             },
+    //             transformer: (operation) => ({...operation, operationName: `${operation.operationName}Ssr`}),
+    //         },
+    //         // Vi bruker ikke mocks enda, og avventer ny versjon av orval
+    //         // som støtter msw v2.
+    //         mock: false,
+    //     },
+    //     hooks: {
+    //         afterAllFilesWrite: "prettier --write",
+    //     },
+    // },
 });
