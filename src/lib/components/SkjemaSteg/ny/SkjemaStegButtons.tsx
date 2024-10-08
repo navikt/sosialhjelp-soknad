165241815cea3dd074aef53976da8976a677b1c9
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-10-08 16:19:06 +0200

    Skiller ut avbryt-knapper/dialog

--- src/lib/components/SkjemaSteg/ny/SkjemaStegButtons.tsx
+++ src/lib/components/SkjemaSteg/ny/SkjemaStegButtons.tsx
@@ -1,17 +1,14 @@
 import * as React from "react";
+import {useState} from "react";
 import {Button, Loader} from "@navikt/ds-react";
 import {useTranslation} from "react-i18next";
 import {DigisosValidationError, KortSkjemaPage, SkjemaPage} from "./SkjemaSteg";
-import {useState} from "react";
-import digisosConfig from "../../../config";
-import {AvbrytSoknadModal} from "../../modals/AvbrytSoknadModal";
-import {NavEnhetInaktiv} from "../../../../sider/01-personalia/adresse/NavEnhetInaktiv";
-import {logAmplitudeEvent} from "../../../amplitude/Amplitude";
 import {DigisosLanguageKey} from "../../../i18n";
 import {ArrowRightIcon} from "@navikt/aksel-icons";
 import {scrollToTop} from "../../../utils";
 import {logWarning} from "../../../log/loggerUtils.ts";
 import {useSkjemaNavigation} from "../useSkjemaNavigation.ts";
+import {SkjemaStegCancelButtons} from "./SkjemaStegCancelButtons.tsx";
 
 interface SkjemaStegNavigasjonProps {
     loading?: boolean;
@@ -30,13 +27,17 @@ export const SkjemaStegButtons = ({
     page,
     onRequestNavigation,
 }: SkjemaStegNavigasjonProps) => {
-    const [avbrytModalOpen, setAvbrytModalOpen] = useState<boolean>(false);
-
     const {t} = useTranslation("skjema");
     const [sendSoknadPending, setSendSoknadPending] = useState<boolean>(false);
     const {gotoPage} = useSkjemaNavigation(page);
 
     const requestNavigation = async (page: number) => {
+        if (onConfirm) {
+            setSendSoknadPending(true);
+            await onConfirm();
+            setSendSoknadPending(false);
+        }
+
         try {
             if (onRequestNavigation !== undefined) await onRequestNavigation();
             gotoPage(page);
@@ -48,15 +49,10 @@ export const SkjemaStegButtons = ({
 
     return (
         <div>
-            {page !== 9 && <NavEnhetInaktiv />}
-            <AvbrytSoknadModal open={avbrytModalOpen} onClose={() => setAvbrytModalOpen(false)} />
             <div className={"!mt-12 md:!mt-16 lg:!mt-24 !mb-8 lg:!mb-16 space-x-3"}>
                 <Button
                     variant="secondary"
-                    id="gaa_tilbake_button"
-                    onClick={async () => {
-                        await requestNavigation(page - 1);
-                    }}
+                    onClick={async () => await requestNavigation(page - 1)}
                     disabled={loading || page <= 1}
                 >
                     {t("skjema.knapper.forrige")}
@@ -64,15 +60,7 @@ export const SkjemaStegButtons = ({
                 </Button>
                 <Button
                     variant="primary"
-                    id="gaa_videre_button"
-                    onClick={async () => {
-                        if (onConfirm) {
-                            setSendSoknadPending(true);
-                            await onConfirm();
-                            setSendSoknadPending(false);
-                        }
-                        return requestNavigation(page + 1);
-                    }}
+                    onClick={async () => await requestNavigation(page + 1)}
                     disabled={sendSoknadPending || loading}
                     icon={includeNextArrow && <ArrowRightIcon />}
                     iconPosition={"right"}
@@ -81,20 +69,7 @@ export const SkjemaStegButtons = ({
                     {(sendSoknadPending || loading) && <Loader className={"ml-2"} />}
                 </Button>
             </div>
-            <div className={"pb-8 lg:pb-16"}>
-                <Button
-                    variant="tertiary"
-                    onClick={async () => {
-                        await logAmplitudeEvent("Klikk på fortsett senere", {SoknadVersjon: "Standard"});
-                        window.location.href = digisosConfig.minSideURL;
-                    }}
-                >
-                    {t("avbryt.fortsettsenere")}
-                </Button>
-                <Button variant="tertiary" onClick={() => setAvbrytModalOpen(true)}>
-                    {t("avbryt.slett")}
-                </Button>
-            </div>
+            <SkjemaStegCancelButtons />
         </div>
     );
 };
