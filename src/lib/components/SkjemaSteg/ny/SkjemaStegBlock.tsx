aeba96fab3b300d5eb7626f74c77fe74aaf639c0
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-10-16 18:17:01 +0200

    Forenkler SkjemaStegBlock (ingen bruk for ref)

--- src/lib/components/SkjemaSteg/ny/SkjemaStegBlock.tsx
+++ src/lib/components/SkjemaSteg/ny/SkjemaStegBlock.tsx
@@ -1,24 +1,14 @@
 import * as React from "react";
 import {ReactNode} from "react";
 import cx from "classnames";
-import {Page} from "@navikt/ds-react";
 
-export const SkjemaStegBlock = React.forwardRef<
-    HTMLDivElement,
-    {
-        children: ReactNode;
-        className?: string;
-    }
->(({children, className}, ref) => (
-    <Page.Block width={"md"}>
-        <section
-            ref={ref}
-            className={cx(
-                "bg-white mx-auto rounded-2xl px-4 md:px-12 lg:px-24 pt-8 pb-8 mb-16 space-y-12 lg:space-y-24",
-                className
-            )}
-        >
-            {children}
-        </section>
-    </Page.Block>
-));
+export const SkjemaStegBlock = ({children, className}: {children: ReactNode; className?: string}) => (
+    <section
+        className={cx(
+            "bg-white mx-auto rounded-2xl px-4 md:px-12 lg:px-24 pt-8 pb-8 mb-16 space-y-12 lg:space-y-24",
+            className
+        )}
+    >
+        {children}
+    </section>
+);
