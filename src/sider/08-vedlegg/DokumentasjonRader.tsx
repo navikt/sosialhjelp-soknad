8503d0a5e4467d0b29ee387b00d11c06bb51f71a
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-10-22 12:07:36 +0200

    Henter DokumentasjonRader.tsx fra master
    Kompatibel med revidert useOpplysning

--- src/sider/08-vedlegg/DokumentasjonRader.tsx
+++ src/sider/08-vedlegg/DokumentasjonRader.tsx
@@ -3,42 +3,22 @@ import {OpplysningInputRad} from "./OpplysningInputRad";
 import {VedleggFrontend} from "../../generated/model";
 import {useOpplysning} from "../../lib/hooks/dokumentasjon/useOpplysning";
 import {useDokumentasjonTekster} from "../../lib/hooks/dokumentasjon/useDokumentasjonTekster";
-import {useEffect, useRef} from "react";
-import deepEqual from "deep-equal";
 
 export const DokumentasjonRader = ({opplysning}: {opplysning: VedleggFrontend}) => {
     const {
         multirow,
         inputs,
         form: {control},
-        rows: {entries, append, remove, update},
+        rows: {entries, append, remove},
     } = useOpplysning(opplysning);
 
     const {leggtil} = useDokumentasjonTekster(opplysning.type);
 
-    const previousEntriesRef = useRef(entries);
-
-    // Sync entries with opplysning.rader on component mount or when opplysning changes
-    useEffect(() => {
-        if (deepEqual(entries, previousEntriesRef.current)) return;
-        previousEntriesRef.current = entries;
-        console.log("dokumentasjonRader");
-        if (opplysning?.rader) {
-            opplysning.rader.forEach((rad, index) => {
-                if (entries[index]) {
-                    // Merge existing entry with rader values, prioritizing rader
-                    update(index, {...entries[index], ...rad});
-                } else {
-                    append(rad); // Append rader data to entries if no existing entry
-                }
-            });
-        }
-    }, [opplysning, append, update]);
-
     //TODO: multirow knappen skjules for lonnslipp|arbeid
     //TODO: dette på grunn av det kan skap forvirringer for søkeren på grunn av kor uoversiktlig det kan bli
+
     return (
-        <>
+        <form onSubmit={(e) => e.preventDefault()}>
             {entries.length > 0 && (
                 <ul>
                     {entries.map(({id}, index) => (
@@ -47,8 +27,8 @@ export const DokumentasjonRader = ({opplysning}: {opplysning: VedleggFrontend}) 
                             textKey={opplysning.type}
                             index={index}
                             control={control}
-                            fields={inputs || []}
-                            onDelete={index > 0 ? () => remove(index) : undefined}
+                            fields={inputs}
+                            onDelete={index > 0 ? remove : undefined}
                         />
                     ))}
                     {multirow && opplysning.type !== "lonnslipp|arbeid" && (
@@ -61,6 +41,6 @@ export const DokumentasjonRader = ({opplysning}: {opplysning: VedleggFrontend}) 
                     )}
                 </ul>
             )}
-        </>
+        </form>
     );
 };
