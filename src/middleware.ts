0705e3f94dba11c02bc2bcbe264ee8de3ef4bbd3
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-05 13:34:29 +0100

    Logg masse når preprod

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,33 @@ import {verifyOrRedirectToLogin} from "./lib/middleware/verifyOrRedirectToLogin.
 import {configureLogger} from "@navikt/next-logger";
 
 import {logger} from "@navikt/next-logger";
+import {getToken, parseIdportenToken, validateToken} from "@navikt/oasis";
 
 configureLogger({basePath: "/sosialhjelp/soknad", apiPath: "/sosialhjelp/soknad/api"});
 
-export async function middleware({url, cookies}: NextRequest) {
+export async function middleware({url, cookies, headers}: NextRequest) {
     if (process.env.NEXT_PUBLIC_DIGISOS_ENV === "preprod") {
         logger.warn("Running in preprod, skipping auth check");
+        logger.info("Auth header: ", headers.get("authorization"));
+        const token = getToken(headers);
+        if (!token) {
+            logger.warn("No token found in headers");
+            return NextResponse.next();
+        }
+        const validation = await validateToken(token);
+        if (!validation.ok) {
+            logger.warn("Token could not be validated");
+            return NextResponse.next();
+        }
+        const parsed = parseIdportenToken(token);
+
+        if (!parsed.ok) {
+            logger.warn("Token could not be parsed");
+            return NextResponse.next();
+        }
+
+        logger.info(JSON.stringify(parsed, null, 2));
+
         return NextResponse.next();
     }
     try {
