562c13f39b26f720cc4145ea465895656389a551
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-05 13:54:52 +0100

    Test proxying av API

---
+++ src/app/soknad-api/[...path]/route.ts
@@ -0,0 +1,24 @@
+import {proxyRouteHandler} from "@navikt/next-api-proxy";
+import {logger} from "@navikt/next-logger";
+
+export const GET = proxyMyBackend;
+export const POST = proxyMyBackend;
+export const PUT = proxyMyBackend;
+export const DELETE = proxyMyBackend;
+
+// Remember to export the HTTP verb you want Next to expose
+async function proxyMyBackend(request: Request, {params}: {params: Promise<{path: string[]}>}): Promise<Response> {
+    const path = `/sosialhjelp/soknad-api/${(await params).path.join("/")}`;
+    const hostname = "sosialhjelp-soknad-api.teamdigisos";
+    logger.info("Proxying request to sosialhjelp-soknad-api.teamdigisos");
+    logger.info("Authorization: " + request.headers.get("Authorization"));
+    logger.info("Path: " + path);
+
+    return proxyRouteHandler(request, {
+        hostname,
+        path,
+        bearerToken: `${request.headers.get("Authorization")?.split(" ")[1]}`,
+        // use https: false if you are going through service discovery
+        https: false,
+    });
+}
