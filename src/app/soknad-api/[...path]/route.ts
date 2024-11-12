072b5240b252c30dcbeebcc68f4f969632dbb989
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-12 13:33:32 +0100

    Wonderwall-støtte for KS- og husbanken-tokens (#1601)
    * Lager CRDer for Wonderwall og IDPorten
    * Legg på inbound accessPolicy
    * Dokumenter Wonderwall
    * Endrer navn på app fra sosialhjelp-soknad-preprod til sosialhjelp-soknad i tråd med øvrige apps i preprod

--- src/app/soknad-api/[...path]/route.ts
+++ src/app/soknad-api/[...path]/route.ts
@@ -5,12 +5,13 @@ export const GET = proxyMyBackend;
 export const POST = proxyMyBackend;
 export const PUT = proxyMyBackend;
 export const DELETE = proxyMyBackend;
+export const OPTIONS = proxyMyBackend;
 
 // Remember to export the HTTP verb you want Next to expose
 async function proxyMyBackend(request: Request, {params}: {params: Promise<{path: string[]}>}): Promise<Response> {
     const path = `/sosialhjelp/soknad-api/${(await params).path.join("/")}`;
     const hostname = "sosialhjelp-soknad-api.teamdigisos";
-    logger.info("Proxying request to sosialhjelp-soknad-api.teamdigisos");
+    logger.info(`Proxying request to ${hostname}`);
     logger.info("Authorization: " + request.headers.get("Authorization"));
     logger.info("Path: " + path);
 
