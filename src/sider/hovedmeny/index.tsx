ce88004837cde818b41bcb513285ba02ae657037
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-13 17:49:15 +0100

    Inliner forenklet komponent
    Denne komponenten er ikke kompleks nok lenger til å måtte være sin egen komponent

--- src/sider/hovedmeny/index.tsx
+++ src/sider/hovedmeny/index.tsx
@@ -6,18 +6,9 @@ import {EttersendDokuPanel} from "./paneler/EttersendDokuPanel";
 import {useTranslation} from "react-i18next";
 import {useGetSessionInfo} from "../../generated/informasjon-ressurs/informasjon-ressurs";
 import {ApplicationSpinner} from "../../lib/components/animasjoner/ApplicationSpinner.tsx";
-import {Heading, VStack} from "@navikt/ds-react";
-import {PabegyntSoknad} from "../../generated/model/pabegyntSoknad.ts";
+import {Heading, Link, VStack} from "@navikt/ds-react";
 import digisosConfig from "../../lib/config.ts";
 import DeveloperToolkit from "../../lib/components/appHeader/DeveloperToolkit.tsx";
-
-const PanelStack = ({open}: {open: PabegyntSoknad[]}) => (
-    <VStack gap={"5"}>
-        <NySoknadPanel defaultOpen={open?.length === 0} />
-        <PabegynteSoknaderPanel />
-        <EttersendDokuPanel />
-    </VStack>
-);
 
 export const Informasjon = () => {
     const {data: {userBlocked, open} = {userBlocked: false, open: []}, isPending} = useGetSessionInfo();
@@ -27,16 +18,30 @@ export const Informasjon = () => {
 
     return (
         <>
+            <Link href="#main-content" className="sr-only sr-only-focusable">
+                {t("hoppTilHovedinnhold")}
+            </Link>
             {digisosConfig.showDevPanel && <DeveloperToolkit />}
-            <div className="max-w-lg lg:max-w-3xl w-full mx-auto gap-6 max-lg:px-2 py-6 lg:gap-16 lg:py-16 flex flex-col grow">
+            <section
+                id="main-content"
+                className="max-w-lg lg:max-w-3xl w-full mx-auto gap-6 max-lg:px-2 py-6 lg:gap-16 lg:py-16 flex flex-col grow"
+            >
                 <title>{t("applikasjon.sidetittel.stringValue")}</title>
                 <VStack as="header" gap={"4"} className={"w-full"}>
                     <Heading level="1" size="small" className={"lg:!text-heading-xlarge !w-full !p-0 !text-left"}>
                         {t("skjema.tittel")}
                     </Heading>
                 </VStack>
-                {isPending ? <ApplicationSpinner /> : <PanelStack open={open} />}
-            </div>
+                {isPending ? (
+                    <ApplicationSpinner />
+                ) : (
+                    <VStack gap={"5"}>
+                        <NySoknadPanel defaultOpen={open?.length === 0} />
+                        <PabegynteSoknaderPanel />
+                        <EttersendDokuPanel />
+                    </VStack>
+                )}
+            </section>
         </>
     );
 };
