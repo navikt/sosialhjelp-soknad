f1808906507d84d78eb307600dfeabf72b40687d
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-14 16:24:51 +0100

    Pynte litt på proxy-koden

--- src/app/soknad-api/[...path]/route.ts
+++ src/app/soknad-api/[...path]/route.ts
@@ -1,24 +1,17 @@
 import {proxyRouteHandler} from "@navikt/next-api-proxy";
 import digisosConfig from "../../../lib/config.ts";
 
-const getRouteHandlerProxyTarget = (
-    headers: Headers,
-    requestPath: string[]
-): {
-    hostname: string;
-    path: string;
-    bearerToken: string;
-    https: boolean;
-} => {
+type RouteHandlerProxyTarget = {hostname: string; path: string; https: boolean; bearerToken?: string};
+type ProxyRequestContext = {params: Promise<{path: string[]}>};
+type ProxyRequestHandler = (request: Request, context: ProxyRequestContext) => Promise<Response>;
+
+const getRouteHandlerProxyTarget = (headers: Headers, requestPath: string[]): RouteHandlerProxyTarget => {
     if (!digisosConfig.proxy) throw new Error("Proxy not configured");
     const {hostname, basePath, https} = digisosConfig.proxy;
     const path = `${basePath}/${requestPath.join("/")}`;
     const bearerToken = `${headers.get("Authorization")?.split(" ")[1]}`;
     return {hostname, path, bearerToken, https};
 };
-
-type ProxyRequestContext = {params: Promise<{path: string[]}>};
-type ProxyRequestHandler = (request: Request, context: ProxyRequestContext) => Promise<Response>;
 
 const soknadApiProxy: ProxyRequestHandler = async (request, {params}): Promise<Response> =>
     proxyRouteHandler(request, getRouteHandlerProxyTarget(request.headers, (await params).path));
