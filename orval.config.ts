a1d0cedb23186cf2bd8999589fa5750d2a834843
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-19 12:53:41 +0100

    API-klientkode for SSR (#1618)
    * feature - generer SSR-klienter

--- orval.config.ts
+++ orval.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig} from "orval";
+import {pascal} from "@orval/core";
 
 export default defineConfig({
     "soknad-api-client-old": {
@@ -16,6 +17,26 @@ export default defineConfig({
             },
             // Vi bruker ikke mocks enda, og avventer ny versjon av orval
             // som støtter msw v2.
+            mock: false,
+        },
+        hooks: {
+            afterAllFilesWrite: "prettier --write",
+        },
+    },
+    "soknad-api-client-old-ssr": {
+        input: "./soknad-api-old.json",
+        output: {
+            mode: "single",
+            target: "src/generated/old-ssr",
+            schemas: "src/generated/old-ssr/model",
+            client: "fetch",
+            override: {
+                mutator: {
+                    path: "./src/lib/api/ssr/authenticatedFetch.ts",
+                    name: "authenticatedFetch",
+                },
+                operationName: ({operationId}) => ssrCamelCasePrefixer(operationId),
+            },
             mock: false,
         },
         hooks: {
@@ -43,6 +64,26 @@ export default defineConfig({
             afterAllFilesWrite: "prettier --write",
         },
     },
+    "soknad-api-client-new-ssr": {
+        input: "./soknad-api-new.json",
+        output: {
+            mode: "single",
+            target: "src/generated/new-ssr",
+            schemas: "src/generated/new-ssr/model",
+            client: "fetch",
+            override: {
+                mutator: {
+                    path: "./src/lib/api/ssr/authenticatedFetch.ts",
+                    name: "authenticatedFetch",
+                },
+                operationName: ({operationId}) => ssrCamelCasePrefixer(operationId),
+            },
+            mock: false,
+        },
+        hooks: {
+            afterAllFilesWrite: "prettier --write",
+        },
+    },
     driftsmeldinger: {
         input: "./driftsmelding-api.json",
         output: {
@@ -62,27 +103,23 @@ export default defineConfig({
             afterAllFilesWrite: "prettier --write",
         },
     },
-    // TODO: Finne løsning for API-bruk på server-side
-    // "soknad-api-ssr": {
-    //     input: "./soknad-api.json",
-    //     output: {
-    //         mode: "tags-split",
-    //         target: "src/generated/server/axiosInstance.ts",
-    //         schemas: "src/generated/server/model",
-    //         client: "axios-functions",
-    //         override: {
-    //             mutator: {
-    //                 path: "./src/lib/api/axiosInstanceSsr.ts",
-    //                 name: "axiosInstance",
-    //             },
-    //             transformer: (operation) => ({...operation, operationName: `${operation.operationName}Ssr`}),
-    //         },
-    //         // Vi bruker ikke mocks enda, og avventer ny versjon av orval
-    //         // som støtter msw v2.
-    //         mock: false,
-    //     },
-    //     hooks: {
-    //         afterAllFilesWrite: "prettier --write",
-    //     },
-    // },
 });
+
+/**
+ * Custom Orval function to prefix methods with "ssr" while retaining camelCase.
+ *
+ * operationIds are not strictly schematically mandated by openapi, but
+ * we can still take them for granted because we use springdoc to generate
+ * schemas for our Spring Boot backends, and springdoc - at least as configured
+ * in soknad-api at the time of writing - always generates an operationId.
+ *
+ * If we're not using those operations, we don't have to care if one pops up unnamed.
+ * So if the schema some day happens to include operations where operationId is missing,
+ * we generate eye-catchingly implausible names for them rather than breaking the build.
+ *
+ * Maybe when the SSR code is used on a majority of pages, we can prefer it's always flagged,
+ * and consider this an error instead.
+ */
+const ssrCamelCasePrefixer = (operationId: string | undefined) => {
+    return `ssr${pascal(operationId ?? "MISSING_OPERATION_ID_IN_SPEC")}`;
+};
