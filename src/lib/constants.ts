4583f5098d6fc2a0e312db14c60b1cc7acecb879
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-20 23:43:11 +0100

    lager en const for dekorator-språk-cookie-navn

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,6 @@
 export const BASE_PATH = "/sosialhjelp/soknad" as const;
 export const LINK_PAGE_PATH = `${BASE_PATH}/link` as const;
 export const ENABLE_DEBUG_I18N = false;
-export const DIGISOS_LANGUAGE_STORAGE_KEY = "digisos-language" as const;
+export const DECORATOR_LANG_COOKIE = "decorator-language" as const;
 export const XSRF_COOKIE_NAME = "XSRF-TOKEN-SOKNAD-API" as const;
 export const XSRF_HEADER_NAME = "X-XSRF-TOKEN" as const;
