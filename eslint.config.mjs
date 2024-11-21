066cf120cc031c4663795e3efc332982469ed222
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-21 01:05:50 +0100

    Skrur på eslint for next-core-web-vitals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import nextPlugin from "@next/eslint-plugin-next";
 import globals from "globals";
 
 export default tseslint.config(
@@ -12,16 +13,25 @@ export default tseslint.config(
             globals: {
                 ...globals.node,
             },
-        }
+        },
     },
-    {ignores: ['build/**/*', 'src/generated/**/*']},
+    {ignores: ["build/**/*", "src/generated/**/*"]},
     eslint.configs.recommended,
-    ...
-        tseslint.configs.recommended, {
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            "@next/next": nextPlugin,
+        },
+        rules: {
+            ...nextPlugin.configs.recommended.rules,
+            ...nextPlugin.configs["core-web-vitals"].rules,
+        },
+    },
+    {
         rules: {
             "@typescript-eslint/no-explicit-any": "off",
             "@typescript-eslint/no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
+            "@next/next/no-img-element": "off",
         },
     }
-)
-;
+);
