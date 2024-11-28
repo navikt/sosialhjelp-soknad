ed53f6c6112e5b434c5072f865bbcf3d1174969a
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-28 17:00:27 +0100

    Fjerner deprecated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,33 +5,21 @@ import tseslint from "typescript-eslint";
 import nextPlugin from "@next/eslint-plugin-next";
 import globals from "globals";
 
-export default tseslint.config(
-    // server.mjs er Node-serveren vår, så andre regler gjelder for den.
-    {
-        files: ["server.mjs"],
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+    ignores: ["build/**/*", "src/generated/**/*"],
+    plugins: {
+        "@next/next": nextPlugin,
+    },
+    rules: {
+        ...nextPlugin.configs.recommended.rules,
+        ...nextPlugin.configs["core-web-vitals"].rules,
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
+        "@next/next/no-img-element": "off",
+    },
+    languageOptions: {
+        globals: {
+            ...globals.node,
         },
     },
-    {ignores: ["build/**/*", "src/generated/**/*"]},
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        plugins: {
-            "@next/next": nextPlugin,
-        },
-        rules: {
-            ...nextPlugin.configs.recommended.rules,
-            ...nextPlugin.configs["core-web-vitals"].rules,
-        },
-    },
-    {
-        rules: {
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
-            "@next/next/no-img-element": "off",
-        },
-    }
-);
+});
