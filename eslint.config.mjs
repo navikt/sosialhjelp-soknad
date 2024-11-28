ea8c6ddbd5ad71115e44f05538275cb3f1a29247
Martin Almvik <martin.almvik@nav.no>
2024-11-28 10:29:37 +0100

    Revert diverse pipeline-relaterte commits

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,18 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import nextPlugin from "@next/eslint-plugin-next";
+import globals from "globals";
 
 export default tseslint.config(
+    // server.mjs er Node-serveren vår, så andre regler gjelder for den.
+    {
+        files: ["server.mjs"],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
+    },
     {ignores: ["build/**/*", "src/generated/**/*"]},
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
@@ -15,6 +25,10 @@ export default tseslint.config(
         rules: {
             ...nextPlugin.configs.recommended.rules,
             ...nextPlugin.configs["core-web-vitals"].rules,
+        },
+    },
+    {
+        rules: {
             "@typescript-eslint/no-explicit-any": "off",
             "@typescript-eslint/no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
             "@next/next/no-img-element": "off",
