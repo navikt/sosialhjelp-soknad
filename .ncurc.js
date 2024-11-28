be76477b7e6aff02f59c585590de02f2b8f7cf6d
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2024-11-28 17:05:10 +0100

    Fikser eslint config (og expect-errors)

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,5 @@
 // Konfig for npm-check-updates.
 // https://www.npmjs.com/package/npm-check-updates#config-file
-// eslint-disable-next-line no-undef
 module.exports = {
     upgrade: true,
     // Ikke foreslå oppdateringer for @types/node så lenge vi er pinned til 20.
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,21 +5,25 @@ import tseslint from "typescript-eslint";
 import nextPlugin from "@next/eslint-plugin-next";
 import globals from "globals";
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-    ignores: ["build/**/*", "src/generated/**/*"],
-    plugins: {
-        "@next/next": nextPlugin,
-    },
-    rules: {
-        ...nextPlugin.configs.recommended.rules,
-        ...nextPlugin.configs["core-web-vitals"].rules,
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
-        "@next/next/no-img-element": "off",
-    },
-    languageOptions: {
-        globals: {
-            ...globals.node,
+export default tseslint.config(
+    {ignores: ["build/**/*", "src/generated/**/*"]},
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            "@next/next": nextPlugin,
         },
-    },
-});
+        rules: {
+            ...nextPlugin.configs.recommended.rules,
+            ...nextPlugin.configs["core-web-vitals"].rules,
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
+            "@next/next/no-img-element": "off",
+        },
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
+    }
+);
