6d03b12d21cfce9a1b0d9451dd4d978aaa11cb87
joeldlin <jorgen.elden.lindgren@nav.no>
2025-03-11 09:52:29 +0100

    r - Må endre litt på hvordan renderingen av det søker skriver eller ikke skriver i saldo boksen i kort søknad. Og så er det i samhold med skissene

--- src/sider/09-oppsummering/question/OppsummeringSporsmal.tsx
+++ src/sider/09-oppsummering/question/OppsummeringSporsmal.tsx
@@ -9,8 +9,18 @@ export const OppsummeringSporsmal = ({sporsmal}: {sporsmal: Sporsmal}) => {
     const {tBackend} = useBackendTranslation();
     return (
         <div>
-            {sporsmal.tittel && <Label spacing>{tBackend(sporsmal.tittel)}</Label>}
-            {!sporsmal.erUtfylt && <Warning />}
+            {sporsmal.tittel &&
+            sporsmal.tittel === "utbetalinger.inntekt.skattbar.oppsummering_kort_konto_saldo_ingen" &&
+            !sporsmal.erUtfylt ? (
+                <div>{tBackend(sporsmal.tittel)}</div>
+            ) : (
+                sporsmal.tittel && <Label spacing>{tBackend(sporsmal.tittel)}</Label>
+            )}
+            {!(
+                sporsmal.tittel === "utbetalinger.inntekt.skattbar.oppsummering_kort_konto_saldo_ingen" &&
+                !sporsmal.erUtfylt
+            ) &&
+                !sporsmal.erUtfylt && <Warning />}
             <SporsmalFelt felt={sporsmal.felt} />
         </div>
     );
