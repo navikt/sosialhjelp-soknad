852cee843891156df941bd9337801ed8a2a672ce
Martin Almvik <martin.almvik@nav.no>
2025-03-20 17:15:11 +0100

    Gå over til ny controller på side 5

--- src/lib/hooks/data/useBosituasjon.tsx
+++ src/lib/hooks/data/useBosituasjon.tsx
@@ -1,25 +1,31 @@
-import {useHentBosituasjon, useUpdateBosituasjon} from "../../../generated/bosituasjon-ressurs/bosituasjon-ressurs";
 import {useBehandlingsId} from "../common/useBehandlingsId";
-import {BosituasjonFrontend, BosituasjonFrontendBotype} from "../../../generated/model";
 import {useQueryClient} from "@tanstack/react-query";
+import {
+    useGetBosituasjon,
+    useUpdateBosituasjon,
+} from "../../../generated/new/bosituasjon-controller/bosituasjon-controller.ts";
+import {BosituasjonDto, BosituasjonDtoBotype} from "../../../generated/new/model";
 
 export const useBosituasjon = () => {
     const behandlingsId = useBehandlingsId();
     const queryClient = useQueryClient();
-    const {data: bosituasjon, isLoading, isError, queryKey} = useHentBosituasjon(behandlingsId);
-    const {mutate} = useUpdateBosituasjon();
+    const {data, isLoading, isError, queryKey} = useGetBosituasjon(behandlingsId);
+    const {mutate, variables, isPending} = useUpdateBosituasjon({
+        mutation: {onSettled: () => queryClient.invalidateQueries({queryKey})},
+    });
 
-    const setBosituasjon = async (nyBosituasjon: Partial<BosituasjonFrontend>) => {
-        const data = {...(bosituasjon as BosituasjonFrontend), ...nyBosituasjon};
-        mutate({behandlingsId, data});
-        queryClient.setQueryData(queryKey, data);
+    const setBosituasjon = async (nyBosituasjon: Partial<BosituasjonDto>) => {
+        const input = {...data, ...nyBosituasjon};
+        mutate({soknadId: behandlingsId, data: input});
     };
 
-    const setBotype = (botype: BosituasjonFrontendBotype) => setBosituasjon({botype});
-    const {botype, antallPersoner} = bosituasjon || {};
+    const setBotype = (botype: BosituasjonDtoBotype) => setBosituasjon({botype});
+
+    const botype = isPending ? variables?.data?.botype : data?.botype;
+    const antallPersoner = isPending ? variables?.data?.antallPersoner : data?.antallPersoner;
 
     return {
-        bosituasjon,
+        data,
         botype,
         antallPersoner,
         setBosituasjon,
