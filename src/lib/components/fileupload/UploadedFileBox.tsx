b962b9d5f05ee0f97666546d0cae8582ebee6fa5
joeldlin <jorgen.elden.lindgren@nav.no>
2025-03-25 15:44:38 +0100

    f - kategori for opplastet dokument vises, fil ikon med circle vises også nu

--- src/lib/components/fileupload/UploadedFileBox.tsx
+++ src/lib/components/fileupload/UploadedFileBox.tsx
@@ -6,6 +6,23 @@ import {LinkButton} from "../LinkButton.tsx";
 import digisosConfig from "../../config.ts";
 import {BodyShort, Button} from "@navikt/ds-react";
 import {TrashIcon} from "@navikt/aksel-icons";
+import {useTranslation} from "react-i18next";
+import {FilIllustrasjon} from "../svg/illustrasjoner/FilIllustrasjon.tsx";
+
+const dokumentasjonsTypeTilTextKey: Record<string, string> = {
+    "barnebidrag|betaler": "begrunnelse.kategorier.kortKategorier.barnebidrag_b",
+    "barnebidrag|mottar": "begrunnelse.kategorier.kortKategorier.barnebidrag_m",
+    "faktura|barnehage": "begrunnelse.kategorier.kortKategorier.barnehage",
+    "faktura|sfo": "begrunnelse.kategorier.kortKategorier.barnehageSFO",
+    "husbanken|vedtak": "begrunnelse.kategorier.kortKategorier.bostotte",
+    "husleiekontrakt|husleiekontrakt": "begrunnelse.kategorier.kortKategorier.husleie",
+    "kontooversikt|annet": "begrunnelse.kategorier.kortKategorier.kontooversikt",
+    "lonnslipp|arbeid": "begrunnelse.kategorier.kortKategorier.lonnslipp",
+    "faktura|strom": "begrunnelse.kategorier.kortKategorier.stromOppvarming",
+    "student|vedtak": "begrunnelse.kategorier.kortKategorier.stipendLan",
+    "annet|annet": "begrunnelse.kategorier.kortKategorier.annet",
+    "kort|annet": "begrunnelse.kategorier.kortKategorier.kategoriValg",
+};
 
 export const UploadedFileBox = ({
     onDelete,
@@ -20,6 +37,11 @@ export const UploadedFileBox = ({
     const [showConfirmDelete, setShowConfirmDelete] = useState(false);
     const lastNedUrl = `opplastetVedlegg/${behandlingsId}/${dokumentId}/fil`;
 
+    const {t: tSkjema} = useTranslation("skjema");
+
+    const key = dokumentasjonsTypeTilTextKey[dokumentasjonsType]; // returns a string
+    const kategoriTekst = key ? tSkjema(key as any) : dokumentasjonsType;
+
     return (
         <li className="mt-4 flex gap-2 justify-between border-2 border-[var(--a-border-subtle)] border-solid rounded-md p-2">
             <div>
@@ -30,7 +52,8 @@ export const UploadedFileBox = ({
                         setShowConfirmDelete(false);
                     }}
                 />
-                <BodyShort>{dokumentasjonsType}</BodyShort>
+                <FilIllustrasjon />
+                <BodyShort>{kategoriTekst}</BodyShort>
                 <LinkButton onClick={() => window.open(digisosConfig.baseURL + lastNedUrl)}>{filename}</LinkButton>
             </div>
             <Button
