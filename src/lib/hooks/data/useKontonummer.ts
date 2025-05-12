2ecde2490daac0e0f2e80f52fea4d8d5567afd1f
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-05-12 12:34:50 +0200

    Oppdater til ny schema i PR

--- src/lib/hooks/data/useKontonummer.ts
+++ src/lib/hooks/data/useKontonummer.ts
@@ -4,15 +4,16 @@ import {
 } from "../../../generated/new/kontonummer-controller/kontonummer-controller.ts";
 import {useSoknadId} from "../common/useSoknadId.ts";
 import {useQueryClient} from "@tanstack/react-query";
-import {KontoInformasjonDto, UpdateKontoInformasjonBrukerBody} from "../../../generated/new/model";
 import {optimisticMutationHandlers} from "./optimisticMutationHandlers.ts";
 import {KontonummerFormValues} from "../../../sider/01-personalia/konto/KontonummerFormSchema.ts";
+import {KontoinformasjonResponse} from "../../../generated/new/model/kontoinformasjonResponse.ts";
+import {KontoinformasjonRequest} from "../../../generated/new/model/kontoinformasjonRequest.ts";
 
 export const useKontonummer = () => {
     const soknadId = useSoknadId();
     const {data: kontoinformasjon, isLoading, queryKey} = useGetKontonummer(soknadId);
     const queryClient = useQueryClient();
-    const mutationHandlers = optimisticMutationHandlers<KontoInformasjonDto, UpdateKontoInformasjonBrukerBody>(
+    const mutationHandlers = optimisticMutationHandlers<KontoinformasjonResponse, KontoinformasjonRequest>(
         queryClient,
         queryKey
     );
