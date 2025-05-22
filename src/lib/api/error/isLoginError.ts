d71e82df35f9071bd3527060b67ed0897df25388
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-05-22 12:53:41 +0200

    forenkler isLoginError

--- src/lib/api/error/isLoginError.ts
+++ src/lib/api/error/isLoginError.ts
@@ -1,8 +1,5 @@
 import {AxiosResponse} from "axios";
 import {UnauthorizedMelding} from "../../../generated/model";
 
-export const isLoginError = (response: AxiosResponse): response is AxiosResponse<UnauthorizedMelding> => {
-    if (response.status !== 401) return false;
-
-    return true;
-};
+export const isLoginError = (response: AxiosResponse): response is AxiosResponse<UnauthorizedMelding> =>
+    response.status === 401;
