3e4798eceeb0cf45c54000a1771c1017cb37eba4
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2025-05-22 22:58:20 +0200

    Flytter headings ut av Adresse og BasisPersonalia

--- src/sider/01-personalia/BasisPersonalia.tsx
+++ src/sider/01-personalia/BasisPersonalia.tsx
@@ -4,29 +4,24 @@ import {useSoknadId} from "../../lib/hooks/common/useSoknadId.ts";
 import {useAlgebraic} from "../../lib/hooks/common/useAlgebraic";
 import {useTranslation} from "react-i18next";
 import {formatFodselsnummer} from "@fremtind/jkl-formatters-util";
-import {BodyShort, Heading} from "@navikt/ds-react";
+import {BodyShort} from "@navikt/ds-react";
 import {useGetBasisPersonalia} from "../../generated/new/basis-personalia-controller/basis-personalia-controller.ts";
 
 export const BasisPersonalia = () => {
     const {expectOK} = useAlgebraic(useGetBasisPersonalia(useSoknadId()));
     const {t} = useTranslation("skjema");
     return expectOK(({navn, fodselsnummer, statsborgerskap = "Ukjent/statsløs"}) => (
-        <section aria-labelledby={"basispersonalia-heading"}>
-            <Heading id="basispersonalia-heading" level={"3"} size={"small"} spacing>
-                {t("kontakt.system.personalia.sporsmal")}
-            </Heading>
-            <Systeminfo>
-                <SysteminfoItem as="div" label={t("kontakt.system.personalia.navn")}>
-                    {navn?.fulltNavn}
-                </SysteminfoItem>
-                <SysteminfoItem as="div" label={t("kontakt.system.personalia.fnr")}>
-                    {formatFodselsnummer(fodselsnummer ?? "")}
-                </SysteminfoItem>
-                <SysteminfoItem as="div" label={t("kontakt.system.personalia.statsborgerskap")}>
-                    {statsborgerskap}
-                </SysteminfoItem>
-                <BodyShort className={"pt-3"}>{t("kontakt.system.personalia.infotekst.tekst")}</BodyShort>
-            </Systeminfo>
-        </section>
+        <Systeminfo>
+            <SysteminfoItem as="div" label={t("kontakt.system.personalia.navn")}>
+                {navn?.fulltNavn}
+            </SysteminfoItem>
+            <SysteminfoItem as="div" label={t("kontakt.system.personalia.fnr")}>
+                {formatFodselsnummer(fodselsnummer ?? "")}
+            </SysteminfoItem>
+            <SysteminfoItem as="div" label={t("kontakt.system.personalia.statsborgerskap")}>
+                {statsborgerskap}
+            </SysteminfoItem>
+            <BodyShort className={"pt-3"}>{t("kontakt.system.personalia.infotekst.tekst")}</BodyShort>
+        </Systeminfo>
     ));
 };
