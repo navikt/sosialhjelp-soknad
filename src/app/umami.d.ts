ba8f5e978832171d1c4de920ed7b75848e34ea36
joeldlin <jorgen.elden.lindgren@nav.no>
2025-06-05 13:46:02 +0200

    fikser steg måling

--- src/app/umami.d.ts
+++ src/app/umami.d.ts
@@ -3,8 +3,7 @@ export {};
 declare global {
     interface Window {
         umami: {
-            trackEvent: (eventName: (props) => any, eventData?: Record<string, any>) => void;
-            track: (eventName: string, eventData?: Record<string, any>) => void;
+            track: (eventName: string, eventData?: Record<string, unknown>) => void;
         };
     }
 }
