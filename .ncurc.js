20e92ba54281a899863cbc5e0a737ac5edf8286f
Martin Almvik <martin.almvik@nav.no>
2026-04-13 10:49:10 +0200

    Downgrade eslint
    Funker ikke med eslint-plugin-react

--- .ncurc.js
+++
@@ -1,8 +0,0 @@
-// Konfig for npm-check-updates.
-// https://www.npmjs.com/package/npm-check-updates#config-file
-module.exports = {
-    upgrade: true,
-    reject: [
-        /* evt pakker vi pinner her */
-    ],
-};
