884f9e0376e88783dd2956c867a09d702fe03a1e
Martin Almvik <martin.almvik@nav.no>
2026-06-22 12:06:18 +0200

    Fikse integrasjon mellom tailwind og ds

--- src/lib/components/SkjemaSteg/SkjemaStegBlock.tsx
+++ src/lib/components/SkjemaSteg/SkjemaStegBlock.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 import {ReactNode} from "react";
 import cx from "classnames";
+import {Box} from "@navikt/ds-react";
 
 export const SkjemaStegBlock = ({children, className}: {children: ReactNode; className?: string}) => (
-    <section
-        className={cx(
-            "bg-white rounded-2xl px-4 md:px-12 lg:px-24 pt-8 pb-8 mb-16 space-y-12 lg:space-y-24",
-            className
-        )}
+    <Box
+        as="section"
+        background="default"
+        className={cx("rounded-2xl px-4 md:px-12 lg:px-24 pt-8 pb-8 mb-16 space-y-12 lg:space-y-24", className)}
         role={"none"}
     >
         {children}
-    </section>
+    </Box>
 );
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,9 @@
+import navikt from "@navikt/ds-tailwind";
 /** @type {import('tailwindcss').Config} */
 
 export default {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    presets: [navikt],
     theme: {
         extend: {
             screens: {
